b482d7080f930dca94d6017d8e0c3522c99b600e
Dimitrie Stefanescu <didimitrie@gmail.com>
2020-06-14 00:32:09 +0100

    feat(api): starting a leaner api integration

---
+++ Speckle/Core2/EmptyClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Speckle.Models;
+using Speckle.Serialisation;
+using Speckle.Transports;
+
+namespace Speckle.Core2
+{
+
+  public class Stream
+  {
+    [JsonIgnore]
+    public ITransport LocalObjectTransport { get; set; }
+
+    [JsonIgnore]
+    public ITransport LocalStreamTransport { get; set; }
+
+    public Serializer Serializer { get; set; }
+
+    public Stream()
+    {
+      Serializer = new Serializer();
+      LocalObjectTransport = new DiskTransport();
+      LocalStreamTransport = new DiskTransport(scope: "Streams", splitPath: false);
+    }
+
+    public string PreviousCommitId { get; set; }
+
+    public async Task<string> PushLocal(List<Base> objects, string branchName = "master", string commitMessage = "")
+    {
+      var commit = new Commit { Objects = objects, CommitMessage = commitMessage };
+
+      Serializer.SerializeAndSave(commit, LocalObjectTransport);
+
+      PreviousCommitId = commit.hash;
+      return PreviousCommitId;
+    }
+
+    public async Task<List<Base>> PullLocal(string commitId)
+    {
+      var commit = (Commit) Serializer.DeserializeAndGet(LocalObjectTransport.GetObject(commitId), LocalObjectTransport);
+
+      return commit.Objects;
+    }
+
+  }
+
+  public class Commit : Base
+  {
+    [DetachProperty]
+    public List<Base> Objects { get; set; }
+
+    public string CommitMessage { get; set; }
+
+    public Commit() { }
+  }
+
+  public class Remote
+  {
+    public string ServerUrl { get; set; }
+
+    public string StreamId { get; set; }
+
+    public Remote() { }
+
+    public async Task Push(string commitId)
+    {
+
+    }
+  }
+
+
+}
---
+++ Tests/PushPull.cs
@@ -0,0 +1,85 @@
+﻿using Speckle.Models;
+using System.Collections.Generic;
+using Speckle.Core2;
+using System.Diagnostics;
+using System;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+
+namespace Tests
+{
+  [TestFixture]
+  public class PushPull
+  {
+
+    [SetUp]
+    public void SetUp() { }
+
+    [TearDown]
+    public void TearDown() { }
+
+    string commitId;
+
+    [Test]
+    public async Task PushPullLocal()
+    {
+      var myStream = new Speckle.Core2.Stream();
+
+      var objects = new List<Base>();
+
+      var numObjects = 100;
+      for (int i = 0; i < numObjects; i++)
+      {
+        if (i % 2 == 0)
+        {
+          objects.Add(new Point(i, i, i));
+        }
+        else
+        {
+          objects.Add(new Polyline { Points = new List<Point>() { new Point(i * 3, i * 3, i * 3), new Point(i / 2, i / 2, i / 2) } });
+        }
+      }
+
+      commitId = await myStream.PushLocal(objects);
+
+      var localObjects = await myStream.PullLocal(commitId);
+
+      Assert.AreEqual(100, localObjects.Count);
+      Assert.AreEqual(((Point)localObjects[0]).X, 0);
+    }
+
+    [Test]
+    public void StreamObjects()
+    {
+
+      var HttpClient = new HttpClient(new HttpClientHandler()
+      {
+        AutomaticDecompression = System.Net.DecompressionMethods.GZip,
+      }, true);
+
+      HttpClient.BaseAddress = new Uri(@"http://localhost:3000");
+
+
+      string[] inputFilePaths = Directory.GetFiles(@"/Users/Bender/.config/Speckle/Streams");
+      var memStream = new MemoryStream();
+      
+      //foreach (var file in inputFilePaths)
+      //{
+      //  using (var inp = File.OpenRead(file))
+      //  {
+      //    Debug.WriteLine(inp);
+      //    inp.CopyTo(memStream);
+      //  }
+      //}
+      //var SourceStream = File.Open(@" / Users/Bender/.config/Speckle/Streams/36dc3ba3-5bf5-4273-ba76-6bd6314b6b36", FileMode.Open);
+
+      //var test = HttpClient.PostAsync("/streaming", new StreamContent(memStream)).Result;
+
+      return;
+    }
+
+
+  }
+}
