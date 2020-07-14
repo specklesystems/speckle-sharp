b34fe56dd0ec9591baa0008c72a6de4b4ab7c7ca
Matteo Cominetti <matteo@cominetti.org>
2020-07-14 12:59:33 +0100

    feat(interfaces): moves ISpeckleConverter to separate file, adds a BatchConvert method and a few other props

--- Speckle/Kits/ISpeckleKit.cs
+++ Speckle/Kits/ISpeckleKit.cs
@@ -5,38 +5,6 @@ using System.Text;
 
 namespace Speckle.Kits
 {
-
-  public interface ISpeckleConverter
-  {
-    /// <summary>
-    /// Converts a native object to a Speckle one
-    /// </summary>
-    /// <param name="object">Native object to convert</param>
-    /// <returns></returns>
-    public Base ToSpeckle(object @object);
-
-    /// <summary>
-    /// Checks if it can onvert a native object to a Speckle one
-    /// </summary>
-    /// <param name="object">Native object to convert</param>
-    /// <returns></returns>
-    public bool CanConvertToSpeckle(object @object);
-
-    /// <summary>
-    /// Converts a Speckle object to a native one
-    /// </summary>
-    /// <param name="object">Speckle object to convert</param>
-    /// <returns></returns>
-    public object ToNative(Base @object);
-
-    /// <summary>
-    /// Checks if it can convert a Speckle object to a native one
-    /// </summary>
-    /// <param name="object">Speckle object to convert</param>
-    /// <returns></returns>
-    public bool CanConvertToNative(Base @object);
-  }
-
   public interface ISpeckleKit
   {
     /// <summary>
