4a236fad81d04a35fe551c5add66e057df6c0335
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-07-15 09:32:55 +0100

    feat(accounts): add accounts view and view model

---
+++ Accounts/AccountsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using SpeckleDesktopUI.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpeckleCore;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace SpeckleDesktopUI.Accounts
+{
+    class AccountsViewModel : BindableBase
+    {
+        public AccountsViewModel()
+        {
+            LoadAccounts();
+        }
+        private Account _defaultAccount;
+        private Account _selectedAccount;
+        private ObservableCollection<Account> _accountsList;
+        public ObservableCollection<Account> AccountsList
+        {
+            get => _accountsList;
+            set => SetProperty(ref _accountsList, value);
+        }
+        public Account DefaultAccount
+        {
+            get => _defaultAccount;
+            set => SetProperty(ref _defaultAccount, value);
+        }
+        public Account SelectedAccount
+        {
+            get => _selectedAccount;
+            set => SetProperty(ref _selectedAccount, value);
+        }
+
+        public void LoadAccounts()
+        {
+            AccountsList = new ObservableCollection<Account>(LocalContext.GetAllAccounts());
+            DefaultAccount = LocalContext.GetDefaultAccount();
+        }
+    }
+}
