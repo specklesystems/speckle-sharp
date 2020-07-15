01a84d348816ecb5fa6d33bf424e1d11c5d59391
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-07-15 15:02:12 +0100

    refactor(commands): simplify and clarify naming

--- Utils/RelayCommand.cs
+++ Utils/RelayCommand.cs
@@ -7,69 +7,20 @@ using System.Windows.Input;
 
 namespace SpeckleDesktopUI.Utils
 {
-    class RelayCommand : ICommand
-    {
-        Action _TargetExecuteMethod;
-        Func<bool> _TargetCanExecuteMethod;
-
-        public RelayCommand(Action executeMethod)
-        {
-            _TargetExecuteMethod = executeMethod;
-        }
-
-        public RelayCommand(Action executeMethod, Func<bool> canExecuteMethod)
-        {
-            _TargetExecuteMethod = executeMethod;
-            _TargetCanExecuteMethod = canExecuteMethod;
-        }
-
-        public void RaiseCanExecuteChanged()
-        {
-            CanExecuteChanged(this, EventArgs.Empty);
-        }
-        #region ICommand Members
-
-        bool ICommand.CanExecute(object parameter)
-        {
-            if (_TargetCanExecuteMethod != null)
-            {
-                return _TargetCanExecuteMethod();
-            }
-            if (_TargetExecuteMethod != null)
-            {
-                return true;
-            }
-            return false;
-        }
-
-        // Beware - should use weak references if command instance lifetime is longer than lifetime of UI objects that get hooked up to command
-        // Prism commands solve this in their implementation
-        public event EventHandler CanExecuteChanged = delegate { };
-
-        void ICommand.Execute(object parameter)
-        {
-            if (_TargetExecuteMethod != null)
-            {
-                _TargetExecuteMethod();
-            }
-        }
-        #endregion
-    }
-
     public class RelayCommand<T> : ICommand
     {
-        Action<T> _TargetExecuteMethod;
-        Func<T, bool> _TargetCanExecuteMethod;
+        Action<T> _TargetExecuteCommand;
+        Func<T, bool> _TargetCanExecuteCommand;
 
-        public RelayCommand(Action<T> executeMethod)
+        public RelayCommand(Action<T> executeCommand)
         {
-            _TargetExecuteMethod = executeMethod;
+            _TargetExecuteCommand = executeCommand;
         }
 
-        public RelayCommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        public RelayCommand(Action<T> executeCommand, Func<T, bool> canExecuteCommand)
         {
-            _TargetExecuteMethod = executeMethod;
-            _TargetCanExecuteMethod = canExecuteMethod;
+            _TargetExecuteCommand = executeCommand;
+            _TargetCanExecuteCommand = canExecuteCommand;
         }
 
         public void RaiseCanExecuteChanged()
@@ -80,12 +31,12 @@ namespace SpeckleDesktopUI.Utils
 
         bool ICommand.CanExecute(object parameter)
         {
-            if (_TargetCanExecuteMethod != null)
+            if (_TargetCanExecuteCommand != null)
             {
                 T tparm = (T)parameter;
-                return _TargetCanExecuteMethod(tparm);
+                return _TargetCanExecuteCommand(tparm);
             }
-            if (_TargetExecuteMethod != null)
+            if (_TargetExecuteCommand != null)
             {
                 return true;
             }
@@ -98,10 +49,7 @@ namespace SpeckleDesktopUI.Utils
 
         void ICommand.Execute(object parameter)
         {
-            if (_TargetExecuteMethod != null)
-            {
-                _TargetExecuteMethod((T)parameter);
-            }
+            _TargetExecuteCommand?.Invoke((T)parameter);
         }
         #endregion
     }
