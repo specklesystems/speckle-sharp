e90e6de283082b32571d3794c0286575bfceb71f
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-07-15 09:22:53 +0100

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpeckleDesktopUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                //if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
+
+        private void MenuToggleButton_OnClick(object sender, RoutedEventArgs e)
+            => NavDrawerListBox.Focus();
+
+        private void MenuDarkModeButton_Click(object sender, RoutedEventArgs e)
+            => ModifyTheme(DarkModeToggleButton.IsChecked == true);
+        private static void ModifyTheme(bool isDarkTheme)
+        {
+            PaletteHelper paletteHelper = new PaletteHelper();
+            ITheme theme = paletteHelper.GetTheme();
+
+            theme.SetBaseTheme(isDarkTheme ? Theme.Dark : Theme.Light);
+
+            paletteHelper.SetTheme(theme);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Speckle Desktop UI
+A desktop UI using WPF loosely based on the Revit plugin.
+
+Restructured to use MVVM design pattern and split into feature folders.
+
+### TODO
+- [ ] dynamically change styling within a template based on input source
+- [ ] get real stream data
+- [ ] remove / update commands
+- [ ] bloody build errors w it not finding the kits
