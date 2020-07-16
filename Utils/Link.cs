5fb22bd80e0a69879fa2363118ac8720b1779f8a
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-07-16 16:25:06 +0100

    feat(utils): link handling

---
+++ Utils/Link.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeckleDesktopUI.Utils
+{
+    public static class Link
+    {
+        public static void OpenInBrowser(string url)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+        }
+    }
+}
