312760734a1eb6f3d12bf09ea6b4f36e9b39aede
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-07-23 17:56:59 +0100

    feat: set up required pages
    created empty views and view models for Inbox, Feed, and Settings

---
+++ Feed/FeedView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpeckleDesktopUI.Feed
+{
+    /// <summary>
+    /// Interaction logic for FeedView.xaml
+    /// </summary>
+    public partial class FeedView : UserControl
+    {
+        public FeedView()
+        {
+            InitializeComponent();
+        }
+    }
+}
