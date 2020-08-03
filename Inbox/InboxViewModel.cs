4fd3b0e05b959ed7a2492eb7499a345d7b704dfe
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-08-03 18:11:22 +0100

    chore: hook up auxiliary pages and show header

--- Inbox/InboxViewModel.cs
+++ Inbox/InboxViewModel.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using Speckle.DesktopUI.Utils;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Speckle.DesktopUI.Inbox
 {
-    class InboxViewModel
+    class InboxViewModel : BindableBase
     {
+        public InboxViewModel()
+        {
+
+            RefreshInboxCommand = new RelayCommand<string>(OnRefreshInbox);
+        }
+
+        private ObservableCollection<object> _allNotifications;
+        private ObservableCollection<object> _filteredNotifications;
+        public ObservableCollection<object> AllNotifications
+        {
+            get => _allNotifications;
+            set => SetProperty(ref _allNotifications, value);
+        }
+        public ObservableCollection<object> FilteredNotifications
+        {
+            get => _filteredNotifications;
+            set => SetProperty(ref _filteredNotifications, value);
+        }
+
+        public RelayCommand<string> RefreshInboxCommand;
+        private void OnRefreshInbox(string arg)
+        {
+            // refresh AllNotifications
+        }
+
     }
 }
--- Settings/SettingsViewModel.cs
+++ Settings/SettingsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Speckle.DesktopUI.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Speckle.DesktopUI.Settings
 {
-    class SettingsViewModel
+    class SettingsViewModel : BindableBase
     {
     }
 }
