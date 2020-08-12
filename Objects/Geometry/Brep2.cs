ba591eb159e5cfd58b83e9a44512dd1c563d5005
Matteo Cominetti <matteo@cominetti.org>
2020-08-12 17:35:25 +0100

    chore: comments out wip code

--- Objects/Geometry/Brep2.cs
+++ Objects/Geometry/Brep2.cs
@@ -1,14 +1,15 @@
-﻿using Speckle.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using Speckle.Core.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Speckle.Objects.Geometry
-{
-  public class Brep2 : Base, IGeometry
-  {
-    public List<Point> vertices { get; set; }
-    public List<object> surfaces { get; set; }
-    public List<ICurve> edges { get; set; }
-  }
-}
+//namespace Speckle.Objects.Geometry
+//{
+//  //TODO: to finish
+//  public class Brep2 : Base, IGeometry
+//  {
+//    public List<Point> vertices { get; set; }
+//    public List<object> surfaces { get; set; }
+//    public List<ICurve> edges { get; set; }
+//  }
+//}
