76af0e2875de9d76ec82bebc74f05d060a734696
Matteo Cominetti <matteo@cominetti.org>
2020-08-12 17:30:42 +0100

    feat: adds a class that implements the ISpeckleKit interface

---
+++ Objects/ObjectsKit.cs
@@ -0,0 +1,21 @@
+﻿using Speckle.Core.Kits;
+using Speckle.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Speckle.Objects
+{
+  public class ObjectsKit : ISpeckleKit
+  {
+
+    public string Description => "Default Speckle Kit";
+    public string Name => "Objects";
+    public string Author => "Speckle";
+    public string WebsiteOrEmail => "https://speckle.systems";
+
+    public IEnumerable<Type> Types => Assembly.GetExecutingAssembly().GetTypes().Where(t => t.IsSubclassOf(typeof(Base)) && !t.IsAbstract);
+  }
+}
