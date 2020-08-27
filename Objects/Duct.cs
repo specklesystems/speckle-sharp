f4b6ee8bb3b12e2599241c8eb6c533bf1258d16c
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-08-27 13:30:27 +0100

    feat(mep): add speckle duct and revit duct

---
+++ Objects/Duct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Speckle.Objects
+{
+  public class Duct : Element
+  {
+    public double width { get; set; }
+    public double height { get; set; }
+    public double diameter { get; set; }
+    public double length { get; set; }
+    public Level refLevel { get; set; }
+    public double velocity { get; set; }
+
+    public Duct() { }
+  }
+}
