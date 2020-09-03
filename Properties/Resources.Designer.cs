8f4cf842b9e2122033e8f7c980cdb20c8bd05632
Matteo Cominetti <matteo@cominetti.org>
2020-09-03 22:09:12 +0100

    fix: moves resource dic to mainwindow

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DesktopUI.Properties {
+namespace Speckle.DesktopUI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DesktopUI.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DesktopUI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Speckle.DesktopUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
