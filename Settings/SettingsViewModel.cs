6cca72ad6e2d77cd86e6c19c09120991d9b9efde
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-04 17:29:44 +0100

    feat(settings): add default account info

--- Settings/SettingsViewModel.cs
+++ Settings/SettingsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Speckle.DesktopUI.Utils;
+using Speckle.DesktopUI.Accounts;
+using Speckle.Core.Credentials;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,24 @@ namespace Speckle.DesktopUI.Settings
 {
   class SettingsViewModel : BindableBase
   {
+    public SettingsViewModel()
+    {
+      DefaultAccount = _repo.GetDefault();
+      ManageAccountsCommand = new RelayCommand<string>(OnManageAccountsCommand);
+    }
+    private AccountsRepository _repo = new AccountsRepository();
+    private Account _defaultAccount;
+    public Account DefaultAccount
+    {
+      get => _defaultAccount;
+      set => SetProperty(ref _defaultAccount, value);
+    }
+
+    public RelayCommand<string> ManageAccountsCommand { get; set; }
+
+    private void OnManageAccountsCommand(string arg)
+    {
+
+    }
   }
 }
