1972d2d591b8ae5d87d0a5a4c0718a22c47fe169
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-08 10:47:07 +0100

    feat(settings): revamp
    - add account details to top of settings
    - button placeholder for launching manager
    - help links for docs, github repo, and forum
    - TODO add logging export here + any other settings

--- Settings/SettingsViewModel.cs
+++ Settings/SettingsViewModel.cs
@@ -15,6 +15,29 @@ namespace Speckle.DesktopUI.Settings
     {
       DefaultAccount = _repo.GetDefault();
       ManageAccountsCommand = new RelayCommand<string>(OnManageAccountsCommand);
+
+      HelpLinks = new List<HelpLink>() {
+        new HelpLink() {
+          name = "Docs",
+          description = "Browse through the Speckle documentation on our website",
+          url = "https://speckle.systems/docs/clients/revit/basics",
+          icon = "FileDocument"
+        },
+        new HelpLink()
+        {
+          name = "Github",
+          description = "Take a look at the source code or submit an issue in the repository",
+          url = "https://github.com/specklesystems/DesktopUI",
+          icon = "Github"
+        },
+        new HelpLink()
+        {
+          name = "Forum",
+          description = "Ask questions and join the discussion on our discourse forum",
+          url = "https://discourse.speckle.works/",
+          icon = "Forum"
+        }
+      };
     }
     private AccountsRepository _repo = new AccountsRepository();
     private Account _defaultAccount;
@@ -23,12 +46,20 @@ namespace Speckle.DesktopUI.Settings
       get => _defaultAccount;
       set => SetProperty(ref _defaultAccount, value);
     }
-
+    public List<HelpLink> HelpLinks { get; set; }
     public RelayCommand<string> ManageAccountsCommand { get; set; }
 
     private void OnManageAccountsCommand(string arg)
     {
+      //TODO open Manager app with speckle://
+    }
 
+    public class HelpLink
+    {
+      public string name { get; set; }
+      public string description { get; set; }
+      public string url { get; set; }
+      public string icon { get; set; }
     }
   }
 }
