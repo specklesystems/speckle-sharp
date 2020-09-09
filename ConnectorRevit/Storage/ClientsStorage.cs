3ed76a1684c8bc48c47774a2b634abe3e7593a5a
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-09 12:16:34 +0100

    feat(ui): bring over storage logic from 1.0

---
+++ ConnectorRevit/Storage/ClientsStorage.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.ExtensibleStorage;
+
+namespace Speckle.ConnectorRevit.Storage
+{
+  /// <summary>
+  /// Manages the serialisation of speckle clients in a revit document.
+  /// </summary>
+  public static class SpeckleClientsStorageManager
+  {
+    readonly static Guid ID = new Guid("{5D453471-1F20-44CE-B1D0-BBD2BDE4616A}");
+
+    /// <summary>
+    /// Returns the speckle clients present in the current document.
+    /// </summary>
+    /// <param name="doc"></param>
+    /// <returns></returns>
+    public static SpeckleClientsWrapper ReadClients(Document doc)
+    {
+      var speckleClientsEntity = GetSpeckleEntity(doc);
+      if (speckleClientsEntity == null || !speckleClientsEntity.IsValid()) return null;
+
+      var mySpeckleClients = new SpeckleClientsWrapper();
+      mySpeckleClients.SetClients(speckleClientsEntity.Get<IList<string>>("clients"));
+
+      return mySpeckleClients;
+    }
+
+    /// <summary>
+    /// Writes the clients to the current document.
+    /// </summary>
+    /// <param name="doc"></param>
+    /// <param name="wrap"></param>
+    public static void WriteClients(Document doc, SpeckleClientsWrapper wrap)
+    {
+      var ds = GetSettingsDataStorage(doc);
+
+      if (ds == null) ds = DataStorage.Create(doc);
+
+      Entity speckleClientsEntity = new Entity(SpeckleClientsSchema.GetSchema());
+
+      speckleClientsEntity.Set("clients", wrap.GetStringList() as IList<string>);
+
+      Entity idEntity = new Entity(DSUniqueSchemaClientsStorage.GetSchema());
+      idEntity.Set("Id", ID);
+
+      ds.SetEntity(idEntity);
+      ds.SetEntity(speckleClientsEntity);
+    }
+
+    private static DataStorage GetSettingsDataStorage(Document doc)
+    {
+      // Retrieve all data storages from project
+      FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+      var dataStorages = collector.OfClass(typeof(DataStorage));
+
+      // Find setting data storage
+      foreach (DataStorage dataStorage in dataStorages)
+      {
+        Entity settingIdEntity = dataStorage.GetEntity(DSUniqueSchemaClientsStorage.GetSchema());
+
+        if (!settingIdEntity.IsValid()) continue;
+
+        var id = settingIdEntity.Get<Guid>("Id");
+
+        if (!id.Equals(ID)) continue;
+
+        return dataStorage;
+      }
+      return null;
+    }
+
+    private static Entity GetSpeckleEntity(Document doc)
+    {
+      FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+      var dataStorages = collector.OfClass(typeof(DataStorage));
+      foreach (DataStorage dataStorage in dataStorages)
+      {
+        Entity settingEntity = dataStorage.GetEntity(SpeckleClientsSchema.GetSchema());
+        if (!settingEntity.IsValid()) continue;
+
+        return settingEntity;
+      }
+      return null;
+    }
+  }
+}
---
+++ ConnectorRevit/Storage/HelperClasses.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB.ExtensibleStorage;
+using Newtonsoft.Json;
+
+namespace Speckle.ConnectorRevit.Storage
+{
+  /// <summary>
+  /// Wrapper class to manage the storage of speckle clients.
+  /// </summary>
+  public class SpeckleClientsWrapper
+  {
+    public List<dynamic> clients { get; set; }
+
+    public SpeckleClientsWrapper() { clients = new List<dynamic>(); }
+
+    public List<string> GetStringList()
+    {
+      var myList = new List<string>();
+      foreach (dynamic el in clients)
+      {
+        myList.Add(JsonConvert.SerializeObject(el));
+      }
+      return myList;
+    }
+
+    public void SetClients(IList<string> stringList)
+    {
+      clients = stringList.Select(el => JsonConvert.DeserializeObject<dynamic>(el)).ToList();
+    }
+  }
+
+  /// <summary>
+  /// Revit schema of the SpeckleClientWrapper class. 
+  /// </summary>
+  public static class SpeckleClientsSchema
+  {
+    readonly static Guid schemaGuid = new Guid("{F29ABD4E-C2DA-4F6A-A301-C70F1C32128D}");
+
+    public static Schema GetSchema()
+    {
+      var schema = Schema.Lookup(schemaGuid);
+      if (schema != null) return schema;
+
+      var builder = new SchemaBuilder(schemaGuid);
+      builder.SetSchemaName("SpeckleClientStorage");
+      builder.AddArrayField("clients", typeof(string));
+
+      return builder.Finish();
+    }
+  }
+
+  /// <summary>
+  /// Unique schema for... something ¯\_(ツ)_/¯
+  /// </summary>
+  static class DSUniqueSchemaClientsStorage
+  {
+    static readonly Guid schemaGuid = new Guid("{174C7EEE-EC5E-4A3F-894A-C801871AEDB8}");
+
+    public static Schema GetSchema()
+    {
+      Schema schema = Schema.Lookup(schemaGuid);
+
+      if (schema != null) return schema;
+
+      SchemaBuilder schemaBuilder = new SchemaBuilder(schemaGuid);
+
+      schemaBuilder.SetSchemaName("DataStorageUniqueId");
+
+      schemaBuilder.AddSimpleField("Id", typeof(Guid));
+
+      return schemaBuilder.Finish();
+    }
+  }
+}
