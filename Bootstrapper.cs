2a63e40d0f5ef36bf2fa0cb77fac6ee489b8fa89
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-11 17:59:22 +0100

    refactor: stylet bootstrapper to start app

---
+++ Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Speckle.DesktopUI;
+using Stylet;
+using StyletIoC;
+
+namespace Speckle.DesktopUI
+{
+  public class Bootstrapper : Bootstrapper<RootViewModel>
+  {
+    private ConnectorBindings bindings;
+
+    // public Bootstrapper(ConnectorBindings appBindings = null)
+    // {
+    //   this.bindings = appBindings;
+    // }
+    protected override void ConfigureIoC(IStyletIoCBuilder builder)
+    {
+      base.ConfigureIoC(builder);
+
+      // if (bindings != null)
+      //   builder.Bind<ConnectorBindings>().ToInstance(bindings);
+    }
+  }
+}
