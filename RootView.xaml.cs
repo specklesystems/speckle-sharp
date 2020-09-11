4b5ed5841e57a7235d092764f113e3a7c530fabc
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-11 17:59:05 +0100

    refactor: create root view and viewmodel

---
+++ RootView.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace Speckle.DesktopUI
+{
+  /// <summary>
+  /// Interaction logic for RootView.xaml
+  /// </summary>
+  public partial class RootView : Window
+  {
+    public RootView()
+    {
+      if (Application.Current == null)
+      {
+        //if the app is null, eg revit, make one
+        new Application();
+        //make sure material design is loaded
+        var type = typeof(PaletteHelper);
+      }
+
+      //manually inject our main resource dic
+      //we can't put it in app.xml since this window can be loaded by another app
+      Application.Current.Resources.MergedDictionaries.Add(
+      Application.LoadComponent(
+        new Uri("SpeckleDesktopUI;component/Themes/Generic.xaml", UriKind.Relative)
+        ) as ResourceDictionary);
+
+      InitializeComponent();
+    }
+
+    private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+      //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+      var dependencyObject = Mouse.Captured as DependencyObject;
+      while (dependencyObject != null)
+      {
+        //if (dependencyObject is ScrollBar) return;
+        dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+      }
+
+      MenuToggleButton.IsChecked = false;
+    }
+
+    private void MenuToggleButton_OnClick(object sender, RoutedEventArgs e)
+        => NavDrawerListBox.Focus();
+
+    private void MenuDarkModeButton_Click(object sender, RoutedEventArgs e)
+        => ModifyTheme(DarkModeToggleButton.IsChecked == true);
+    private static void ModifyTheme(bool isDarkTheme)
+    {
+      PaletteHelper paletteHelper = new PaletteHelper();
+      ITheme theme = paletteHelper.GetTheme();
+
+      theme.SetBaseTheme(isDarkTheme ? Theme.Dark : Theme.Light);
+
+      paletteHelper.SetTheme(theme);
+    }
+  }
+}
+
---
+++ RootViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaterialDesignThemes.Wpf;
+using Speckle.DesktopUI.Feed;
+using Speckle.DesktopUI.Inbox;
+using Speckle.DesktopUI.Settings;
+using Speckle.DesktopUI.Streams;
+using Stylet;
+
+namespace Speckle.DesktopUI
+{
+  public class RootViewModel : Conductor<IScreen>.Collection.OneActive
+  {
+    private IWindowManager windowManager;
+    private ISnackbarMessageQueue _notifications = new SnackbarMessageQueue(TimeSpan.FromSeconds(5));
+    public RootViewModel(IWindowManager windowManager)
+    {
+      this.windowManager = windowManager;
+      LoadPages();
+    }
+    public ISnackbarMessageQueue Notifications
+    {
+      get => _notifications;
+      set => SetAndNotify(ref _notifications, value);
+    }
+
+    private void LoadPages()
+    {
+      var pages = new List<IScreen>
+      {
+        new StreamsHomeViewModel(),
+        new InboxViewModel(),
+        new FeedViewModel(),
+        new SettingsViewModel()
+      };
+
+      pages.ForEach(Items.Add);
+
+      this.ActiveItem = pages[0];
+    }
+  }
+}
