fe5a53f596cb66608e3e883739ae78b5ac966d29
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-14 11:23:19 +0100

    refactor(streams): streams and single stream pages

---
+++ Streams/AllStreamsViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Speckle.Core.Api;
+using Speckle.DesktopUI.Utils;
+using Stylet;
+
+namespace Speckle.DesktopUI.Streams
+{
+  public class AllStreamsViewModel : Screen
+  {
+    private IStreamViewModelFactory _streamViewModelFactory;
+    public AllStreamsViewModel(IStreamViewModelFactory streamViewModelFactory)
+    {
+      _repo = new StreamsRepository();
+      DisplayName = "Home";
+      SelectedSlide = 0;
+      _streamViewModelFactory = streamViewModelFactory;
+
+#if DEBUG
+      _allStreams = _repo.LoadTestStreams();
+#else
+      // do this properly
+#endif
+    }
+
+    private StreamsRepository _repo;
+    private ObservableCollection<Stream> _allStreams;
+    private Stream _selectedStream;
+    private Branch _selectedBranch;
+    private int _selectedSlide;
+
+    public void ShowStreamInfo(Stream stream)
+    {
+      var item = _streamViewModelFactory.CreateStreamViewModel();
+      item.Stream = stream;
+      // get master branch for now
+      // TODO allow user to select branch
+      item.Branch = _repo.GetMasterBranch(stream.branches.items);
+      var parent = (StreamsHomeViewModel)Parent;
+      parent.ActivateItem(item);
+    }
+    public ObservableCollection<Stream> AllStreams
+    {
+      get => _allStreams;
+      set => SetAndNotify(ref _allStreams, value);
+    }
+
+    public Stream SelectedStream
+    {
+      get => _selectedStream;
+      set => SetAndNotify(ref _selectedStream, value);
+    }
+
+    public Branch SelectedBranch
+    {
+      get => _selectedBranch;
+      set => SetAndNotify(ref _selectedBranch, value);
+    }
+
+    public int SelectedSlide
+    {
+      get => _selectedSlide;
+      set => SetAndNotify(ref _selectedSlide, value);
+    }
+
+    public void OpenHelpLink(string url)
+    {
+      Link.OpenInBrowser(url);
+    }
+  }
+
+  public interface IStreamViewModelFactory
+  {
+    StreamViewModel CreateStreamViewModel();
+  }
+}
---
+++ Streams/StreamViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Speckle.Core.Api;
+using Speckle.DesktopUI.Utils;
+using Stylet;
+
+namespace Speckle.DesktopUI.Streams
+{
+  public class StreamViewModel : Screen
+  {
+    public Stream Stream { get; set; }
+    public Branch Branch { get; set; }
+
+    public StreamViewModel()
+    {
+
+    }
+
+    public void OpenHelpLink(string url)
+    {
+      Link.OpenInBrowser(url);
+    }
+  }
+}
