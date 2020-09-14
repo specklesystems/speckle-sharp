e8af57a39bbe2a9313e438ac9c54bea60410110f
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-14 11:24:39 +0100

    refactor: add general methods to root

--- RootViewModel.cs
+++ RootViewModel.cs
@@ -1,25 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using MaterialDesignThemes.Wpf;
-using Speckle.DesktopUI.Feed;
-using Speckle.DesktopUI.Inbox;
-using Speckle.DesktopUI.Settings;
-using Speckle.DesktopUI.Streams;
+using Speckle.DesktopUI.Utils;
 using Stylet;
 
 namespace Speckle.DesktopUI
 {
   public class RootViewModel : Conductor<IScreen>.Collection.OneActive
   {
-    private IWindowManager windowManager;
+    private IWindowManager _windowManager;
+    private IViewModelFactory _viewModelFactory;
     private ISnackbarMessageQueue _notifications = new SnackbarMessageQueue(TimeSpan.FromSeconds(5));
-    public RootViewModel(IWindowManager windowManager)
+    public RootViewModel(IWindowManager windowManager, IViewModelFactory viewModelFactory)
     {
-      this.windowManager = windowManager;
+      _windowManager = windowManager;
+      _viewModelFactory = viewModelFactory;
       LoadPages();
     }
     public ISnackbarMessageQueue Notifications
@@ -32,15 +29,49 @@ namespace Speckle.DesktopUI
     {
       var pages = new List<IScreen>
       {
-        new StreamsHomeViewModel(),
-        new InboxViewModel(),
-        new FeedViewModel(),
-        new SettingsViewModel()
+        _viewModelFactory.CreateStreamsHomeViewModel(),
+        _viewModelFactory.CreateInboxViewModel(),
+        _viewModelFactory.CreateFeedViewModel(),
+        _viewModelFactory.CreateSettingsViewModel()
       };
 
       pages.ForEach(Items.Add);
 
-      this.ActiveItem = pages[0];
+      ActiveItem = pages[0];
+    }
+
+    public void OpenLink(string url)
+    {
+      Link.OpenInBrowser(url);
+    }
+
+    public void CopyStreamId(string text)
+    {
+      if (text != null)
+      {
+        CopyAndNotify(text, "Stream ID copied to clipboard!");
+      }
+    }
+
+    public void CopyAndNotify(string text, string message)
+    {
+      Clipboard.SetText(text);
+      Notifications.Enqueue(message);
+    }
+
+    private bool _darkMode;
+    public bool DarkMode
+    {
+      get => _darkMode;
+      set => SetAndNotify(ref _darkMode, value);
+    }
+
+    public void ToggleTheme(bool darkmode)
+    {
+      PaletteHelper paletteHelper = new PaletteHelper();
+      ITheme theme = paletteHelper.GetTheme();
+      theme.SetBaseTheme(darkmode ? Theme.Dark : Theme.Light);
+      DarkMode = darkmode;
     }
   }
 }
