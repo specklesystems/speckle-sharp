f4c562f7ccb74acbbd4ea79e7d03a4700d7d62d8
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-15 09:54:09 +0100

    feat(streams): stream added event
    to notify home page to refresh

--- Utils/Events.cs
+++ Utils/Events.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Speckle.Core.Api;
 
 namespace Speckle.DesktopUI.Utils
 {
   public class ShowNotificationEvent
   {
     public string Notification { get; set; }
+  }
+
+  public class StreamAddedEvent
+  {
+    public Stream NewStream { get; set; }
   }
 }
