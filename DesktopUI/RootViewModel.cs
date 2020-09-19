29d229387b049c926c6cbf8117dad25ba8efd53d
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-19 09:11:18 +0100

    feat(bindings): handle notif events from host

--- DesktopUI/RootViewModel.cs
+++ DesktopUI/RootViewModel.cs
@@ -8,75 +8,79 @@ using Stylet;
 
 namespace Speckle.DesktopUI
 {
-  public class RootViewModel : Conductor<IScreen>.Collection.OneActive, IHandle<ShowNotificationEvent>
-  {
-    private IWindowManager _windowManager;
-    private IViewModelFactory _viewModelFactory;
-    private ISnackbarMessageQueue _notifications = new SnackbarMessageQueue(TimeSpan.FromSeconds(5));
-    public ConnectorBindings Bindings;
-
-    public RootViewModel(
-      IWindowManager windowManager,
-      IEventAggregator events,
-      IViewModelFactory viewModelFactory,
-      ConnectorBindings bindings)
+  public class RootViewModel : Conductor<IScreen>.Collection.OneActive,
+    IHandle<ShowNotificationEvent>,
+    IHandle<ApplicationEvent>
     {
-      _windowManager = windowManager;
-      _viewModelFactory = viewModelFactory;
-      Bindings = bindings;
-      LoadPages();
-      events.Subscribe(this);
-    }
+      private IWindowManager _windowManager;
+      private IViewModelFactory _viewModelFactory;
+      private ISnackbarMessageQueue _notifications = new SnackbarMessageQueue(TimeSpan.FromSeconds(5));
+      public ConnectorBindings Bindings;
+      public string HostName => Bindings.GetApplicationHostName();
 
-    public ISnackbarMessageQueue Notifications
-    {
-      get => _notifications;
-      set => SetAndNotify(ref _notifications, value);
-    }
-
-    private void LoadPages()
-    {
-      var pages = new List<IScreen>
+      public RootViewModel(
+        IWindowManager windowManager,
+        IEventAggregator events,
+        IViewModelFactory viewModelFactory,
+        ConnectorBindings bindings)
       {
-        _viewModelFactory.CreateStreamsHomeViewModel(),
-        _viewModelFactory.CreateInboxViewModel(),
-        _viewModelFactory.CreateFeedViewModel(),
-        _viewModelFactory.CreateSettingsViewModel()
-      };
+        _windowManager = windowManager;
+        _viewModelFactory = viewModelFactory;
+        Bindings = bindings;
+        LoadPages();
+        events.Subscribe(this);
+      }
 
-      pages.ForEach(Items.Add);
+      public ISnackbarMessageQueue Notifications
+      {
+        get => _notifications;
+        set => SetAndNotify(ref _notifications, value);
+      }
 
-      ActiveItem = pages[0];
+      private void LoadPages()
+      {
+        var pages = new List<IScreen>
+        {
+          _viewModelFactory.CreateStreamsHomeViewModel(),
+          _viewModelFactory.CreateInboxViewModel(),
+          _viewModelFactory.CreateFeedViewModel(),
+          _viewModelFactory.CreateSettingsViewModel()
+        };
+
+        pages.ForEach(Items.Add);
+
+        ActiveItem = pages[0];
+      }
+
+      public void OpenLink(string url)
+      {
+        Link.OpenInBrowser(url);
+      }
+
+      private bool _darkMode;
+      public bool DarkMode
+      {
+        get => _darkMode;
+        set => SetAndNotify(ref _darkMode, value);
+      }
+
+      //
+      public void ToggleTheme(bool darkmode)
+      {
+        PaletteHelper paletteHelper = new PaletteHelper();
+        ITheme theme = paletteHelper.GetTheme();
+        theme.SetBaseTheme(darkmode ? Theme.Dark : Theme.Light);
+        DarkMode = darkmode;
+      }
+
+      public void Handle(ShowNotificationEvent message)
+      {
+        Notifications.Enqueue(message.Notification);
+      }
+
+      public void Handle(ApplicationEvent message)
+      {
+        Notifications.Enqueue($"App Event: {message.Type}");
+      }
     }
-
-    public void OpenLink(string url)
-    {
-      Link.OpenInBrowser(url);
-    }
-
-    private bool _darkMode;
-    public bool DarkMode
-    {
-      get => _darkMode;
-      set => SetAndNotify(ref _darkMode, value);
-    }
-
-    public void ToggleTheme(bool darkmode)
-    {
-      PaletteHelper paletteHelper = new PaletteHelper();
-      ITheme theme = paletteHelper.GetTheme();
-      theme.SetBaseTheme(darkmode ? Theme.Dark : Theme.Light);
-      DarkMode = darkmode;
-    }
-
-    public void Handle(ShowNotificationEvent message)
-    {
-      Notifications.Enqueue(message.Notification);
-    }
-
-    public string HostName
-    {
-      get => (Bindings is null) ? "Spockle" : Bindings.GetApplicationHostName();
-    }
-  }
 }
