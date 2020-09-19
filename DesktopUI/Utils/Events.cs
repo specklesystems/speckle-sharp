01befe17c0a212b28f6928334418407293493fb0
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-09-19 08:58:50 +0100

    feat(events): add selection events

--- DesktopUI/Utils/Events.cs
+++ DesktopUI/Utils/Events.cs
@@ -7,17 +7,39 @@ using Speckle.Core.Api;
 
 namespace Speckle.DesktopUI.Utils
 {
-  public class ShowNotificationEvent : EventBase
-  { }
+  public class EventBase
+  {
+    public string Notification { get; set; }
+    public dynamic DynamicInfo { get; set; }
+  }
+
+  public class ShowNotificationEvent : EventBase { }
 
   public class StreamAddedEvent : EventBase
   {
     public Stream NewStream { get; set; }
   }
 
-  public class EventBase
+  public class UpdateSelectionCountEvent : EventBase
   {
-    public string Notification { get; set; }
-    public dynamic DynamicInfo { get; set; }
+    public int SelectionCount { get; set; }
+  }
+
+  public class  UpdateSelectionEvent : EventBase
+  {
+    public List<string> ObjectIds { get; set; }
+  }
+
+  public class ApplicationEvent : EventBase
+  {
+    public enum EventType
+    {
+      ViewActivated, // is this Revit specific?
+      DocumentOpened,
+      DocumentClosed,
+      DocumentModified,
+      ApplicationIdling
+    }
+    public EventType Type { get; set; }
   }
 }
