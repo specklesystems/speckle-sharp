8654cb57fcd59cb92883ef9089d166a70b5f2252
Alan Rynne <alan@rynne.es>
2020-10-14 10:33:19 +0200

    feat(components): Implemented StreamDetails and StreamList

--- ConnectorGrashopper/Streams/StreamDetailsComponent.cs
+++ ConnectorGrashopper/Streams/StreamDetailsComponent.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ConnectorGrashopper.Extras;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Speckle.Core.Api;
+using Speckle.Core.Credentials;
 
 namespace ConnectorGrashopper.Streams
 {
@@ -20,17 +26,52 @@ namespace ConnectorGrashopper.Streams
             pManager.AddTextParameter("Stream ID", "ID", "Unique ID of the stream to be updated.", GH_ParamAccess.item);
             pManager.AddTextParameter("Name", "N", "Name of the stream.", GH_ParamAccess.item);
             pManager.AddTextParameter("Description", "D", "Description of the stream", GH_ParamAccess.item);
-            pManager.AddNumberParameter("Created At", "C", "Date of creation", GH_ParamAccess.item);
-            pManager.AddNumberParameter("Updated At", "U", "Date when it was last modified", GH_ParamAccess.item);
+            pManager.AddTextParameter("Created At", "C", "Date of creation", GH_ParamAccess.item);
+            pManager.AddTextParameter("Updated At", "U", "Date when it was last modified", GH_ParamAccess.item);
             pManager.AddBooleanParameter("Public", "P", "True if the stream is to be publicly available.", GH_ParamAccess.item);
             pManager.AddGenericParameter("Collaborators", "Cs", "Users that have collaborated in this stream", GH_ParamAccess.list);
             pManager.AddGenericParameter("Branches", "B", "List of branches for this stream", GH_ParamAccess.list);
-            pManager.AddGenericParameter("Account", "A", "Account to update stream.", GH_ParamAccess.item);
         }
 
+        private Stream stream;
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            throw new NotImplementedException();
+            GH_ObjectWrapper streamInput = null;
+            string accountId = null; 
+            if (stream == null)
+            {
+                Account account;
+                if (!DA.GetData(0, ref streamInput)) return;
+                var streamWrapper = (StreamWrapper) streamInput.Value;
+                
+                Task.Run(async () =>
+                {
+                    account = !DA.GetData(1, ref accountId) 
+                        ? AccountManager.GetDefaultAccount() 
+                        : AccountManager.GetAccounts().FirstOrDefault(a => a.id == accountId);
+                
+                    var client = new Client(account);
+                    stream = await client.StreamGet(streamWrapper.StreamId);
+                    Rhino.RhinoApp.InvokeOnUiThread((Action)delegate
+                    {
+                        ExpireSolution(true);
+                    });
+                });
+            }
+            else
+            {
+                DA.SetData(0, stream.id);
+                DA.SetData(1, stream.name);
+                DA.SetData(2, stream.description);
+                DA.SetData(3, stream.createdAt);
+                DA.SetData(4, stream.updatedAt);
+                DA.SetData(5, stream.isPublic);
+                DA.SetDataList(6, stream.collaborators);
+                DA.SetDataList(7, stream.branches.items);
+                stream = null;
+            }
+            
+
         }
     }
 }
--- ConnectorGrashopper/Streams/StreamListComponent.cs
+++ ConnectorGrashopper/Streams/StreamListComponent.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Grasshopper.Kernel;
+using Speckle.Core.Api;
+using Speckle.Core.Credentials;
+using Speckle.Core.Logging;
 
 namespace ConnectorGrashopper.Streams
 {
@@ -9,8 +15,8 @@ namespace ConnectorGrashopper.Streams
         public override Guid ComponentGuid => new Guid("BE790AF4-1834-495B-BE68-922B42FD53C7");
         protected override void RegisterInputParams(GH_InputParamManager pManager)
         {
-            pManager.AddGenericParameter("Account", "A", "Account to get streams from", GH_ParamAccess.item);
-            pManager.AddIntegerParameter("Limit", "L", "Max number of streams to fetch", GH_ParamAccess.item);
+            pManager.AddTextParameter("Account", "A", "Account to get streams from", GH_ParamAccess.item);
+            pManager.AddIntegerParameter("Limit", "L", "Max number of streams to fetch", GH_ParamAccess.item, 10);
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
@@ -19,9 +25,37 @@ namespace ConnectorGrashopper.Streams
                 GH_ParamAccess.list);
         }
 
+        private List<Stream> streams = null;
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            throw new NotImplementedException();
+            string accountId = null;
+            var limit = 10;
+            
+            if (streams == null)
+            {
+                if (!DA.GetData(0, ref accountId)) return;
+                if (!DA.GetData(1, ref limit)) return;
+
+                Task.Run(async () =>
+                {
+                    var account = AccountManager.GetAccounts().FirstOrDefault(a => a.id == accountId);
+                    Tracker.TrackEvent(Tracker.STREAM_LIST);
+                    var client = new Client(account);
+                    
+                    // Save the result
+                    streams = await client.StreamsGet(limit);
+                    
+                    Rhino.RhinoApp.InvokeOnUiThread((Action)delegate
+                    {
+                        ExpireSolution(true);
+                    });
+                });
+            }
+            else
+            {
+                DA.SetDataList(0, streams);
+                streams = null;
+            }
         }
     }
 }
