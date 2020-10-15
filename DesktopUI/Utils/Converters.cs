f8d45b3152d74cf0253f948ed4d0f34a58bf404a
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-10-15 20:57:13 +0100

    feat(ui): timeago converter for commits ⏰

--- DesktopUI/Utils/Converters.cs
+++ DesktopUI/Utils/Converters.cs
@@ -12,9 +12,27 @@ namespace Speckle.DesktopUI.Utils
   {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-      if (value != null && value is string)
+      if ( value != null && value is string )
       {
-        return ((string)value).ToUpper();
+        return ( ( string ) value ).ToUpper();
+      }
+
+      return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return null;
+    }
+  }
+
+  public class TimeAgoConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if ( value != null )
+      {
+        return Formatting.TimeAgo(( string ) value);
       }
 
       return value;
---
+++ DesktopUI/Utils/Helpers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Speckle.DesktopUI.Utils
+{
+  public static class Link
+  {
+    public static void OpenInBrowser(string url)
+    {
+      if ( !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ) return;
+
+      url = url.Replace("&", "^&");
+      Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") {CreateNoWindow = true});
+    }
+  }
+
+  public static class Formatting
+  {
+    public static string TimeAgo(string timestamp)
+    {
+      TimeSpan timeAgo;
+      try
+      {
+        timeAgo = DateTime.Now.Subtract(DateTime.Parse(timestamp));
+      }
+      catch ( FormatException e )
+      {
+        Debug.WriteLine("Could not parse the string to a DateTime");
+        return "";
+      }
+      if ( timeAgo.TotalSeconds < 60 )
+        return "less than a minute ago";
+      if ( timeAgo.TotalMinutes < 60 )
+        return $"about {timeAgo.Minutes} minute{PluralS(timeAgo.Minutes)} ago";
+      if ( timeAgo.TotalHours < 24 )
+        return $"about {timeAgo.Hours} hour{PluralS(timeAgo.Hours)} ago";
+      if ( timeAgo.TotalDays < 7 )
+        return $"about {timeAgo.Days} day{PluralS(timeAgo.Days)} ago";
+      if ( timeAgo.TotalDays < 30 )
+        return $"about {timeAgo.Days / 7} week{PluralS(timeAgo.Days / 7)} ago";
+      if ( timeAgo.TotalDays < 365 )
+        return $"about {timeAgo.Days / 30} month{PluralS(timeAgo.Days / 30)} ago";
+
+      return $"over {timeAgo.Days / 356} year{PluralS(timeAgo.Days / 356)} ago";
+    }
+
+    public static string PluralS(int num)
+    {
+      return num > 1 ? "s" : "";
+    }
+  }
+}
