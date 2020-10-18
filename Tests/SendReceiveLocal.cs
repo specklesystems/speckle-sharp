f9d575580ca889cd55bf6b2b5547328e8c2f419d
Dimitrie Stefanescu <didimitrie@gmail.com>
2020-10-18 16:33:07 +0100

    test(disk transport): removes test, final decision

--- Tests/SendReceiveLocal.cs
+++ Tests/SendReceiveLocal.cs
@@ -194,29 +194,29 @@ namespace Tests
       Assert.GreaterOrEqual(progress.Keys.Count, 1);
     }
 
-    [Test]
-    public async Task DiskTransportTest()
-    {
-      var myObject = new Base();
-      myObject["@items"] = new List<Base>();
-      myObject["test"] = "random";
+    //[Test]
+    //public async Task DiskTransportTest()
+    //{
+    //  var myObject = new Base();
+    //  myObject["@items"] = new List<Base>();
+    //  myObject["test"] = "random";
 
-      var rand = new Random();
+    //  var rand = new Random();
 
-      for (int i = 0; i < 100; i++)
-      {
-        ((List<Base>)myObject["@items"]).Add(new Point(i, i, i) { applicationId = i + "-___/---" });
-      }
+    //  for (int i = 0; i < 100; i++)
+    //  {
+    //    ((List<Base>)myObject["@items"]).Add(new Point(i, i, i) { applicationId = i + "-___/---" });
+    //  }
 
-      var dt = new DiskTransport.DiskTransport();
-      var id = await Operations.Send(myObject, new List<ITransport>() { dt }, false);
+    //  var dt = new DiskTransport.DiskTransport();
+    //  var id = await Operations.Send(myObject, new List<ITransport>() { dt }, false);
 
-      Assert.IsNotNull(id);
+    //  Assert.IsNotNull(id);
 
-      var rebase = await Operations.Receive(id, dt);
+    //  var rebase = await Operations.Receive(id, dt);
 
-      Assert.AreEqual(rebase.GetId(true), id);
-    }
+    //  Assert.AreEqual(rebase.GetId(true), id);
+    //}
 
   }
 }
