017c842da25366643fc531bda1a3f31e13538d72
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-10-23 10:50:53 +0100

    feat(filters): tab class to locate view

--- DesktopUI/Utils/SelectionFilter.cs
+++ DesktopUI/Utils/SelectionFilter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Stylet;
 
 namespace Speckle.DesktopUI.Utils
 {
@@ -18,7 +20,12 @@ namespace Speckle.DesktopUI.Utils
   {
     public string Name { get; set; }
     public string Icon { get; set; }
-    public string Type { get { return typeof(ElementsSelectionFilter).ToString(); } }
+
+    public string Type
+    {
+      get { return typeof(ElementsSelectionFilter).ToString(); }
+    }
+
     public List<string> Selection { get; set; } = new List<string>();
   }
 
@@ -26,7 +33,11 @@ namespace Speckle.DesktopUI.Utils
   {
     public string Name { get; set; }
     public string Icon { get; set; }
-    public string Type { get { return typeof(ListSelectionFilter).ToString(); } }
+
+    public string Type
+    {
+      get { return typeof(ListSelectionFilter).ToString(); }
+    }
 
     public List<string> Values { get; set; }
     public List<string> Selection { get; set; } = new List<string>();
@@ -36,7 +47,12 @@ namespace Speckle.DesktopUI.Utils
   {
     public string Name { get; set; }
     public string Icon { get; set; }
-    public string Type { get { return typeof(PropertySelectionFilter).ToString(); } }
+
+    public string Type
+    {
+      get { return typeof(PropertySelectionFilter).ToString(); }
+    }
+
     public List<string> Selection { get; set; } = new List<string>();
 
     public List<string> Values { get; set; }
@@ -45,5 +61,37 @@ namespace Speckle.DesktopUI.Utils
     public string PropertyValue { get; set; }
     public string PropertyOperator { get; set; }
     public bool HasCustomProperty { get; set; }
+  }
+
+  public class FilterTab
+  {
+    public string Name
+    {
+      get => Filter.Name;
+    }
+
+    public ISelectionFilter Filter { get; }
+
+    public object FilterView { get; private set; }
+
+    public FilterTab(ISelectionFilter filter)
+    {
+      Filter = filter;
+      LocateFilterView();
+    }
+
+    private void LocateFilterView()
+    {
+      var viewName = $"Speckle.DesktopUI.Streams.Dialogs.FilterViews.{Filter.Name}FilterView";
+      var type = Type.GetType(viewName);
+      try
+      {
+        FilterView = Activator.CreateInstance(type);
+      }
+      catch ( Exception e )
+      {
+        Debug.WriteLine(e);
+      }
+    }
   }
 }
