153026cf7aa5fda926297ff6968ab959097e28a3
Matteo Cominetti <matteo@cominetti.org>
2020-10-26 19:19:36 +0000

    feat: adds view stream node #10

---
+++ ConnectorDynamo/ConnectorDynamo/ViewNode/ViewViewCustomization.cs
@@ -0,0 +1,59 @@
+﻿using Dynamo.Configuration;
+using Dynamo.Controls;
+using Dynamo.Models;
+using Dynamo.Scheduler;
+using Dynamo.ViewModels;
+using Dynamo.Wpf;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Speckle.ConnectorDynamo.ViewNode
+{
+  public class ViewViewCustomization : INodeViewCustomization<View>
+  {
+
+    private DynamoViewModel dynamoViewModel;
+    private DispatcherSynchronizationContext syncContext;
+    private View viewNode;
+    private DynamoModel dynamoModel;
+
+    public void CustomizeView(View model, NodeView nodeView)
+    {
+      dynamoModel = nodeView.ViewModel.DynamoViewModel.Model;
+      dynamoViewModel = nodeView.ViewModel.DynamoViewModel;
+      syncContext = new DispatcherSynchronizationContext(nodeView.Dispatcher);
+      viewNode = model;
+
+      viewNode.OnRequestUpdates += UpdateNode;
+
+      var ui = new ViewUi();
+      nodeView.inputGrid.Children.Add(ui);
+
+      //bindings
+      ui.DataContext = model;
+      //ui.Loaded += model.AddedToDocument;
+      ui.ViewStreamButton.Click += ViewStreamButtonClick;
+    }
+
+
+    private void UpdateNode()
+    {
+      Task.Run(() =>
+      {
+        viewNode.UpdateNode(dynamoModel.EngineController);
+      });
+    }
+
+    private void ViewStreamButtonClick(object sender, RoutedEventArgs e)
+    {
+      Task.Run(() =>
+      {
+        viewNode.ViewStream();
+      });
+    }
+
+    public void Dispose() { }
+
+  }
+}
