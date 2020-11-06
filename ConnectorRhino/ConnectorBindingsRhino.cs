c776735dec2ac0a6d35bc91309846ab22f499d85
Dimitrie Stefanescu <didimitrie@gmail.com>
2020-11-06 11:29:54 +0000

    feat(rhino): inits the desktop ui 🎊

---
+++ ConnectorRhino/ConnectorBindingsRhino.cs
@@ -0,0 +1,123 @@
+﻿using Rhino;
+using Speckle.Core.Models;
+using Speckle.DesktopUI;
+using Speckle.DesktopUI.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeckleRhino
+{
+  class ConnectorBindingsRhino : ConnectorBindings
+  {
+
+    public RhinoDoc RhinoDoc { get; set; }
+
+    public ConnectorBindingsRhino(RhinoDoc rhinoDoc)
+    {
+      RhinoDoc = rhinoDoc;
+    }
+
+    public override void AddExistingStream(string args)
+    {
+      //throw new NotImplementedException();
+    }
+
+    public override void AddNewStream(StreamState state)
+    {
+      //throw new NotImplementedException();
+    }
+
+    public override void AddObjectsToClient(string args)
+    {
+      //throw new NotImplementedException();
+    }
+
+    public override void BakeStream(string args)
+    {
+      //throw new NotImplementedException();
+    }
+
+    public override string GetActiveViewName()
+    {
+      return RhinoDoc.Views.ActiveView.ActiveViewport.Name;
+    }
+
+    public override string GetApplicationHostName()
+    {
+      return Speckle.Core.Kits.Applications.Rhino;
+    }
+
+    public override string GetDocumentId()
+    {
+      return Utilities.hashString("X" + RhinoDoc?.Path + RhinoDoc?.Name, Utilities.HashingFuctions.MD5);
+    }
+
+    public override string GetDocumentLocation()
+    {
+      return RhinoDoc?.Path;
+    }
+
+    public override List<StreamState> GetFileContext()
+    {
+      return new List<StreamState>();
+    }
+
+    public override string GetFileName()
+    {
+      return RhinoDoc?.Name;
+    }
+
+    public override List<string> GetObjectsInView()
+    {
+      return new List<string>();
+    }
+
+    public override List<string> GetSelectedObjects()
+    {
+      return new List<string>();
+    }
+
+    public override List<ISelectionFilter> GetSelectionFilters()
+    {
+      return new List<ISelectionFilter>();
+      //throw new NotImplementedException();
+    }
+
+    public override Task<StreamState> ReceiveStream(StreamState state)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void RemoveObjectsFromClient(string args)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void RemoveSelectionFromClient(string args)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void RemoveStream(string args)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void SelectClientObjects(string args)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override Task<StreamState> SendStream(StreamState state)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void UpdateStream(StreamState state)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ ConnectorRhino/ConnectorRhinoCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Rhino;
+using Rhino.Commands;
+using RhinoWindows;
+using Rhino.Input.Custom;
+using Rhino.PlugIns;
+using Rhino.UI;
+using Speckle.DesktopUI;
+using System.Drawing;
+using System.Windows;
+
+namespace SpeckleRhino
+{
+  public class RhinoConnector : PlugIn
+  {
+  }
+
+  public class SpeckleCommand : Command
+  {
+    public static SpeckleCommand Instance { get; set; }
+
+    public override string EnglishName => "Speckle";
+
+    public SpeckleCommand()
+    {
+    }
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      var bootstrapper = new Bootstrapper()
+      {
+        Bindings = new ConnectorBindingsRhino(doc)
+      };
+
+      if (Application.Current == null) new Application();
+
+      var app = Application.Current;
+
+      var rhApp = Rhino.RhinoApp.MainWindowHandle();
+      bootstrapper.Setup(Application.Current);
+      bootstrapper.Start(new string[] { });
+
+      var helper = new System.Windows.Interop.WindowInteropHelper(bootstrapper.Application.MainWindow);
+      helper.Owner = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+
+      return Result.Success;
+    }
+  }
+
+}
