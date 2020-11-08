93e8abe5c55fb9ed4472179bd6374a060812087f
Dimitrie Stefanescu <didimitrie@gmail.com>
2020-11-08 11:39:33 +0000

    feat(rhino): send implemented

--- ConnectorRhino/ConnectorBindingsRhino.cs
+++ ConnectorRhino/ConnectorBindingsRhino.cs
@@ -1,9 +1,15 @@
 ﻿using Newtonsoft.Json;
 using Rhino;
+using Rhino.DocObjects;
+using Speckle.Core.Api;
+using Speckle.Core.Kits;
 using Speckle.Core.Models;
+using Speckle.Core.Transports;
 using Speckle.DesktopUI;
 using Speckle.DesktopUI.Utils;
+using Stylet;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,7 +28,7 @@ namespace SpeckleRhino
     {
       Rhino.RhinoDoc.EndOpenDocument += RhinoDoc_EndOpenDocument;
 
-      SelectionTimer = new Timer(500) { AutoReset = true, Enabled = true };
+      SelectionTimer = new Timer(1000) { AutoReset = true, Enabled = true };
       SelectionTimer.Elapsed += SelectionTimer_Elapsed;
       SelectionTimer.Start();
     }
@@ -42,7 +48,11 @@ namespace SpeckleRhino
 
     private void RhinoDoc_EndOpenDocument(object sender, DocumentOpenEventArgs e)
     {
-      if (e.Merge) return; // copy pastes, imports, etc.
+      if (e.Merge)
+      {
+        return; // prevents triggering this on copy pastes, imports, etc.
+      }
+
       if (e.Document == null)
       {
         return;
@@ -105,7 +115,7 @@ namespace SpeckleRhino
 
     public override string GetDocumentId()
     {
-      return Utilities.hashString("X" + Doc?.Path + Doc?.Name, Utilities.HashingFuctions.MD5);
+      return Speckle.Core.Models.Utilities.hashString("X" + Doc?.Path + Doc?.Name, Speckle.Core.Models.Utilities.HashingFuctions.MD5);
     }
 
     public override string GetDocumentLocation()
@@ -139,7 +149,7 @@ namespace SpeckleRhino
       return new List<ISelectionFilter>()
       {
          new ElementsSelectionFilter { Name = "Selection", Icon = "Mouse", Selection = GetSelectedObjects()},
-         new ListSelectionFilter { Name = "Layers", Icon = "FilterList", Values = layers, Selection = new List<string>(){ } },
+         new ListSelectionFilter { Name = "Category", Icon = "FilterList", Values = layers, Selection = new List<string>(){ } },
          new ListSelectionFilter { Name = "Layers", Icon = "FilterList", Values = layers }
       };
     }
@@ -171,9 +181,96 @@ namespace SpeckleRhino
       throw new NotImplementedException();
     }
 
-    public override Task<StreamState> SendStream(StreamState state)
+    public override async Task<StreamState> SendStream(StreamState state)
     {
-      return Task.Run(() => state);
+      var kit = KitManager.GetDefaultKit();
+      var converter = kit.LoadConverter(Applications.Rhino);
+
+      var rhObjects = new List<RhinoObject>();
+      var baseObjects = new List<Base>();
+      var layerNames = new HashSet<string>();
+
+      var commitObj = new Base();
+
+      var units = LengthUnits.GetUnitsFromString(Doc.GetUnitSystemName(true, false, false, false));
+      commitObj["units"] = units;
+
+      int objCount = 0;
+
+      foreach (var placeholder in state.Placeholders)
+      {
+        if (state.CancellationToken.IsCancellationRequested)
+        {
+          return null;
+        }
+
+        var obj = Doc.Objects.FindId(new Guid(placeholder.applicationId));
+        if (obj == null)
+        {
+          continue;
+        }
+
+        var converted = converter.ConvertToSpeckle(obj.Geometry);
+        if (converted == null)
+        {
+          continue;
+        }
+
+        foreach (var key in obj.Attributes.GetUserStrings().AllKeys)
+        {
+          converted[key] = obj.Attributes.GetUserString(key);
+        }
+
+        var layerName = Doc.Layers[obj.Attributes.LayerIndex].Name;
+
+        if (commitObj[$"@{layerName}"] == null)
+        {
+          commitObj[$"@{layerName}"] = new List<Base>();
+        }
+
+        ((List<Base>)commitObj[$"@{layerName}"]).Add(converted);
+        objCount++;
+      }
+
+      if (state.CancellationToken.IsCancellationRequested)
+      {
+        return null;
+      }
+
+      Execute.PostToUIThread(() => state.Progress.Maximum = objCount);
+
+      var streamId = state.Stream.id;
+      var client = state.Client;
+
+      var transports = new List<ITransport>() { new ServerTransport(client.Account, streamId) };
+
+      var hasErrors = false;
+      var commitObjId = await Operations.Send(
+        commitObj,
+        transports,
+        onProgressAction: dict => UpdateProgress(dict, state.Progress),
+        onErrorAction: (err, exception) => { hasErrors = true; /* TODO: a wee bit nicer handling here; plus request cancellation! */ }
+        );
+
+      if (hasErrors) return null;
+
+      var res = await client.CommitCreate(new CommitCreateInput()
+      {
+        streamId = streamId,
+        objectId = commitObjId,
+        branchName = "main",
+        message = $"Added {objCount} elements from Rhino. "
+      });
+
+      state.Stream = await client.StreamGet(streamId);
+      //state.Placeholders = new List<Base>(); 
+      // ask izzy: confused re the demarcation between state.objects, state.placeholders, etc. seems like
+      // the above clears the set selection of a stream. 
+      UpdateStream(state);
+      
+      RaiseNotification($"{objCount} objects sent to Speckle 🦏 + 🚀");
+
+      return state;
     }
 
     public override void UpdateStream(StreamState state)
@@ -181,7 +278,7 @@ namespace SpeckleRhino
       var filter = state.Filter;
       var objects = new List<Base>();
 
-      switch(state.Filter)
+      switch (state.Filter)
       {
         case ElementsSelectionFilter selFilter:
           objects = selFilter.Selection.Select(id => new Base { applicationId = id }).ToList();
@@ -191,6 +288,16 @@ namespace SpeckleRhino
       }
       state.Placeholders = objects;
       Doc.Strings.SetString("speckle", state.Stream.id, JsonConvert.SerializeObject(state));
+    }
+
+    private void UpdateProgress(ConcurrentDictionary<string, int> dict, ProgressReport progress)
+    {
+      if (progress == null)
+      {
+        return;
+      }
+
+      Execute.PostToUIThread(() => progress.Value = dict.Values.Last());
     }
   }
 }
