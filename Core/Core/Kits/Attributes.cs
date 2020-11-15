563c481c2515cae46beaeba993edf6862c84ec9a
Matteo Cominetti <matteo@cominetti.org>
2020-11-15 21:01:48 +0000

    feat(core): scaffolds schema builder attriburtes

---
+++ Core/Core/Kits/Attributes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Speckle.Core.Kits
+{
+  [AttributeUsage(AttributeTargets.Class)]
+  public class SchemaBuilderAttribute : Attribute
+  {
+    private string _description;
+
+    public virtual string Description
+    {
+      get { return _description; }
+    }
+
+    public SchemaBuilderAttribute(string description)
+    {
+      _description = description;
+
+    }
+  }
+
+  [AttributeUsage(AttributeTargets.All)]
+  public class SchemaBuilderIgnoreAttribute : Attribute
+  {
+  }
+}
