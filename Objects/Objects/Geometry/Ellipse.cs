4b3554081e637b2f2a6e2204cb5e2df901b12adb
Alan Rynne <alan@speckle.systems>
2020-11-19 14:38:16 +0100

    feat(objects): Added support for elliptical arcs in Revit.

--- Objects/Objects/Geometry/Ellipse.cs
+++ Objects/Objects/Geometry/Ellipse.cs
@@ -12,6 +12,7 @@ namespace Objects.Geometry
     public double? secondRadius { get; set; }
     public Plane plane { get; set; }
     public Interval domain { get; set; }
+    public Interval trimDomain { get; set; }
 
     public Box boundingBox { get; set; }
     public Point center { get; set; }
@@ -21,14 +22,21 @@ namespace Objects.Geometry
 
     public Ellipse()
     {
-
     }
 
     public Ellipse(Plane plane, double radius1, double radius2, string applicationId = null)
+      : this(plane, radius1, radius2, new Interval(0, 2 * Math.PI), null)
+    {
+    }
+
+    public Ellipse(Plane plane, double radius1, double radius2, Interval domain, Interval trimDomain,
+      string applicationId = null)
     {
       this.plane = plane;
       this.firstRadius = radius1;
       this.secondRadius = radius2;
+      this.domain = domain;
+      this.trimDomain = trimDomain;
       this.applicationId = applicationId;
     }
   }
