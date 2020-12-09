d3a7da517af1d0e753d4feada8210d607087795a
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-12-09 18:28:35 +0000

    feat(converters): robot prof pic converter

--- DesktopUI/DesktopUI/Utils/Converters.cs
+++ DesktopUI/DesktopUI/Utils/Converters.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using Speckle.Core.Api;
 
 namespace Speckle.DesktopUI.Utils
 {
@@ -52,6 +53,35 @@ namespace Speckle.DesktopUI.Utils
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
       return value != null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+  public class NullAvatarToRobotConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if ( value.GetType() == typeof(User) )
+      {
+        var user = ( User ) value;
+        if ( String.IsNullOrEmpty(user.avatar) )
+          return $"https://robohash.org/{user.id}";
+        return user.avatar;
+      }
+
+      if ( value.GetType() == typeof(Collaborator) )
+      {
+        var collab = ( Collaborator ) value;
+        if ( String.IsNullOrEmpty(collab.avatar) )
+          return $"https://robohash.org/{collab.id}";
+        return collab.avatar;
+      }
+      else
+        throw new InvalidOperationException("Unrecognised type given to robot converter");
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
