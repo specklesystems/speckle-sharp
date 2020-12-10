4d30ec3bbd663bb3fd50db49ea4771bce6cc0f84
izzy lyseggen <izzy.lyseggen@gmail.com>
2020-12-10 20:49:10 +0000

    feat(collabs): multiselection coming together

--- DesktopUI/DesktopUI/Streams/Dialogs/ShareStreamDialogViewModel.cs
+++ DesktopUI/DesktopUI/Streams/Dialogs/ShareStreamDialogViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
 using MaterialDesignThemes.Wpf;
 using Speckle.Core.Api;
 using Speckle.DesktopUI.Utils;
@@ -50,6 +51,14 @@ namespace Speckle.DesktopUI.Streams
       set => SetAndNotify(ref _userSearchResults, value);
     }
 
+    private BindableCollection<User> _selectedUsers = new BindableCollection<User>();
+
+    public BindableCollection<User> SelectedUsers
+    {
+      get => _selectedUsers;
+      set => SetAndNotify(ref _selectedUsers, value);
+    }
+
     private User _selectedUser;
 
     public User SelectedUser
@@ -74,6 +83,16 @@ namespace Speckle.DesktopUI.Streams
       set => SetAndNotify(ref _shareLink, value);
     }
 
+    private bool _dropdownState = false;
+
+    public bool DropdownState
+    {
+      get => _dropdownState;
+      set => SetAndNotify(ref _dropdownState, value);
+    }
+
+    private bool sourceChanged = false;
+
     public async void SearchForUsers()
     {
       if ( UserQuery.Length <= 2 )
@@ -82,12 +101,51 @@ namespace Speckle.DesktopUI.Streams
       try
       {
         var users = await StreamState.Client.UserSearch(UserQuery);
+        DropdownState = sourceChanged = true;
         UserSearchResults = new BindableCollection<User>(users);
       }
       catch ( Exception e )
       {
         // search prob returned no results
         UserSearchResults?.Clear();
+      }
+    }
+
+    public void ToggleDropdown()
+    {
+      DropdownState = !DropdownState;
+    }
+
+    public void UserSelectionChanged(ListBox sender, SelectionChangedEventArgs e)
+    {
+      // we're only allowing adding items by click, 
+      //so if changed items is more than 1, something is sus
+      if ( e.AddedItems.Count == 1 )
+      {
+        var added = ( User ) e.AddedItems[ 0 ];
+        if ( !SelectedUsers.Any(s => s.id == added.id) )
+          SelectedUsers.Add(( User ) e.AddedItems[ 0 ]);
+      }
+
+      // avoid removing all selected users when new search is made
+      if ( sourceChanged )
+      {
+        sourceChanged = !sourceChanged;
+
+        var selected = sender.Items.Cast<User>().Where(sel => SelectedUsers.Any(u => sel.id == u.id));
+        foreach ( var s in selected )
+        {
+          sender.SelectedItems.Add(s);
+        }
+
+        return;
+      }
+
+      if ( e.RemovedItems.Count == 1 )
+      {
+        var removed = ( User ) e.RemovedItems[ 0 ];
+        var toRemove = SelectedUsers.FirstOrDefault(s => s.id == removed.id);
+        SelectedUsers.Remove(toRemove);
       }
     }
 
