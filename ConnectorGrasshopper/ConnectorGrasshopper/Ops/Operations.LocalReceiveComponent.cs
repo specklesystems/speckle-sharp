ae7bedcdc35154dac4865ed6d1c0c9894e323c09
Alan Rynne <alan@speckle.systems>
2020-12-23 14:29:45 +0100

    feat(gh): Local send/receive components

---
+++ ConnectorGrasshopper/ConnectorGrasshopper/Ops/Operations.LocalReceiveComponent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using ConnectorGrasshopper.Extras;
+using ConnectorGrasshopper.Objects;
+using Grasshopper.Kernel;
+using GrasshopperAsyncComponent;
+using Speckle.Core.Api;
+
+namespace ConnectorGrasshopper.Ops
+{
+  public class LocalReceiveComponent : SelectKitComponentBase
+  {
+    public LocalReceiveComponent() : base("Local Receive", "LR",
+      "Receives data locally, without the need of a Speckle Server. NOTE: updates will not be automatically received.",
+      "Speckle 2", "   Send/Receive")
+    {
+    }
+
+    protected override void RegisterInputParams(GH_InputParamManager pManager)
+    {
+      pManager.AddTextParameter("localDataId", "id", "ID of the data to receive", GH_ParamAccess.item);
+    }
+
+    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+    {
+      pManager.AddGenericParameter("Data", "D", "Data received.", GH_ParamAccess.tree);
+    }
+
+    protected override void SolveInstance(IGH_DataAccess DA)
+    {
+      var localDataId = "";
+      if (!DA.GetData(0, ref localDataId)) return;
+
+      var @base = Operations.Receive(localDataId).Result;
+      if (Converter.CanConvertToNative(@base))
+      {
+        var data = Converter.ConvertToNative(@base);
+
+        DA.SetData(0, Utilities.TryConvertItemToNative(data, Converter));
+      }
+      else if (@base.GetDynamicMembers().Count() == 1)
+      {
+        var treeBuilder = new TreeBuilder(Converter);
+        var tree = treeBuilder.Build(@base[@base.GetDynamicMembers().ElementAt(0)]);
+
+        DA.SetDataTree(0, tree);
+      }
+      else
+      {
+        DA.SetData(0, new GH_SpeckleBase(@base));
+      }
+    }
+
+    public override Guid ComponentGuid => new Guid("D690C3C5-B18A-4A7B-B6FC-A4FFF9A55046");
+  }
+}
---
+++ ConnectorGrasshopper/ConnectorGrasshopper/Ops/Operations.SendLocalComponent.cs
@@ -0,0 +1,59 @@
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Speckle.Core.Models;
+using System;
+using System.Threading.Tasks;
+using ConnectorGrasshopper.Objects;
+using Grasshopper.Kernel.Data;
+using Speckle.Core.Api;
+using Utilities = ConnectorGrasshopper.Extras.Utilities;
+
+namespace ConnectorGrasshopper.Ops
+{
+  public class SendLocalComponent : SelectKitComponentBase
+  {
+    public SendLocalComponent() : base("Local Send", "LS", "Sends data locally, without the need of a Speckle server", "Speckle 2", "   Send/Receive")
+    {
+    }
+
+    public override Guid ComponentGuid => new Guid("6E03AC48-B9E9-48D4-886F-1197F71E4ED2");
+    
+    protected override void RegisterInputParams(GH_InputParamManager pManager)
+    {
+      pManager.AddGenericParameter("Data", "D", "Data to send.", GH_ParamAccess.tree);
+    }
+
+    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+    {
+      pManager.AddGenericParameter("localDataId", "id", "ID of the local data sent.", GH_ParamAccess.item);
+    }
+
+    private bool hasSentObject;
+    private string sentObjectId;
+    protected override void SolveInstance(IGH_DataAccess DA)
+    {
+      if (!hasSentObject)
+      {
+        GH_Structure<IGH_Goo> data;
+        if (!DA.GetDataTree(0, out data)) return;
+      
+        Task.Run(() =>
+        {
+          Converter.SetContextDocument(Rhino.RhinoDoc.ActiveDoc);
+          var converted = Utilities.DataTreeToNestedLists(data, Converter);
+          var ObjectToSend = new Base();
+          ObjectToSend["@data"] = converted;
+          sentObjectId = Operations.Send(ObjectToSend).Result;
+          hasSentObject = true;
+          Rhino.RhinoApp.InvokeOnUiThread((Action) delegate { ExpireSolution(true); });
+        });
+      }
+      else
+      {
+        DA.SetData(0, sentObjectId);
+        sentObjectId = null;
+        hasSentObject = false;
+      }
+    }
+  }
+}
