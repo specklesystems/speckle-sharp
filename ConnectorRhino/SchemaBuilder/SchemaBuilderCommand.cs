733401abb832f8ef31b8149989aa75b4fbc8f393
clairekuang <kuang.claire@gmail.com>
2021-01-14 04:45:57 +0000

    refactor(Rhino): created separate BuiltElements converter
    - using public ConvertToSpeckleBE
    - improved SpeckleSchema command

--- ConnectorRhino/SchemaBuilder/SchemaBuilderCommand.cs
+++ ConnectorRhino/SchemaBuilder/SchemaBuilderCommand.cs
@@ -7,56 +7,116 @@ using System.Threading.Tasks;
 using Rhino;
 using Rhino.DocObjects;
 using Rhino.Commands;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using Rhino.UI;
 using Rhino.PlugIns;
 
 namespace SpeckleRhino
 {
-    public class SchemaBuilderCommand : Command
+  public class SchemaBuilderCommand : Command
+  {
+    // speckle user string for custom schemas
+    // TODO: address consistency weak point, since this string needs to match exactly in ConverterRhinoGH.Geometry.cs!!!
+    // TODO: Add a remove schema command option!
+    string SpeckleSchemaKey = "SpeckleSchema";
+
+    RhinoDoc ActiveDoc = null;
+
+    public SchemaBuilderCommand()
     {
-        // speckle user string for custom schemas
-        // TODO: address consistency weak point, since this string needs to match exactly in ConverterRhinoGH.Geometry.cs!!!
-        string SpeckleUSKey = "SpeckleSchema";
-
-        RhinoDoc ActiveDoc = null;
-
-        public SchemaBuilderCommand()
-        {
-            Instance = this;
-        }
-
-        public static SchemaBuilderCommand Instance
-        {
-            get; private set;
-        }
-
-        public override string EnglishName
-        {
-            get { return "SpeckleApplySchema"; }
-        }
-
-        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
-        {
-            ActiveDoc = doc;
-
-            // get selection doc objects
-            var selectionObjs = doc.Objects.GetSelectedObjects(false, false);
-            SchemaObjectFilter schemaFilter = new SchemaObjectFilter(selectionObjs.ToList(), doc);
-
-            // process the schema dictionary to apply a User Attribute Text field to each doc object
-            var schemaDictionary = schemaFilter.SchemaDictionary;
-            foreach (string schema in schemaDictionary.Keys)
-            {
-                foreach (RhinoObject obj in schemaDictionary[schema])
-                {
-                    obj.Attributes.SetUserString(SpeckleUSKey, schema);
-                }
-            }
-
-            return Result.Success;
-        }
-        protected void CommandTask()
-        {
-            
-        }
+      Instance = this;
     }
+
+    public static SchemaBuilderCommand Instance
+    {
+      get; private set;
+    }
+
+    public override string EnglishName
+    {
+      get { return "SpeckleSchema"; }
+    }
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      ActiveDoc = doc;
+
+      const ObjectType geometryFilter = ObjectType.Surface | ObjectType.PolysrfFilter;
+      OptionToggle toggle = new OptionToggle(true, "Remove", "Apply");
+
+      GetObject go = new GetObject();
+      go.SetCommandPrompt("Select surfaces or polysurfaces");
+      go.GeometryFilter = geometryFilter;
+      go.AddOptionToggle("BuiltElements", ref toggle);
+      go.GroupSelect = true;
+      go.SubObjectSelect = false;
+      go.EnableClearObjectsOnEntry(false);
+      go.EnableUnselectObjectsOnExit(false);
+      go.DeselectAllBeforePostSelect = false;
+
+      bool hasPreselected = false;
+
+      for (; ; )
+      {
+        GetResult res = go.GetMultiple(1, 0);
+
+        if (res == GetResult.Option)
+        {
+          go.EnablePreSelect(false, true);
+          continue;
+        }
+
+        else if (res != GetResult.Object)
+          return Result.Cancel;
+
+        if (go.ObjectsWerePreselected)
+        {
+          hasPreselected = true;
+          go.EnablePreSelect(false, true);
+          continue;
+        }
+
+        break;
+      }
+
+      if (hasPreselected)
+      {
+        // Normally, pre-selected objects will remain selected, when a
+        // command finishes, and post-selected objects will be unselected.
+        // Thus it is possible to have a combination of pre-selected and
+        // post-selected. So, to make sure everything "looks the same"
+        // after command execution, unselect everything before finishing command
+
+        for (int i = 0; i < go.ObjectCount; i++)
+        {
+          RhinoObject rhinoObject = go.Object(i).Object();
+          if (null != rhinoObject)
+            rhinoObject.Select(false);
+        }
+        doc.Views.Redraw();
+      }
+
+      List<RhinoObject> commandObjs = go.Objects().Select(o => o.Object()).ToList();
+      if (toggle.CurrentValue == true)
+        ApplySchemas(commandObjs);
+      else
+        DeleteSchemas(commandObjs);
+      return Result.Success;
+    }
+
+    protected void DeleteSchemas(List<RhinoObject> objs)
+    {
+      foreach (RhinoObject obj in objs)
+        obj.Attributes.DeleteUserString(SpeckleSchemaKey);
+    }
+    protected void ApplySchemas(List<RhinoObject> objs)
+    {
+      SchemaObjectFilter schemaFilter = new SchemaObjectFilter(objs, ActiveDoc);
+      var schemaDictionary = schemaFilter.SchemaDictionary;
+      foreach (string schema in schemaDictionary.Keys)
+        foreach (RhinoObject obj in schemaDictionary[schema])
+          obj.Attributes.SetUserString(SpeckleSchemaKey, schema);
+    }
+  }
 }
--- ConnectorRhino/SchemaBuilder/SchemaObjectFilter.cs
+++ ConnectorRhino/SchemaBuilder/SchemaObjectFilter.cs
@@ -12,215 +12,181 @@ using System.Collections;
 
 namespace SpeckleRhino
 {
-    /// <summary>
-    /// Handles QA/QC for determining whether RhinoObjects can be converted to supported Speckle schemas
-    /// </summary>
-    public class SchemaObjectFilter
+  /// <summary>
+  /// Handles QA/QC for determining whether RhinoObjects can be converted to supported Speckle schemas
+  /// </summary>
+  public class SchemaObjectFilter
+  {
+    enum SupportedSchema { Floor, Wall, Roof, Ceiling };
+
+    #region Properties
+    private Rhino.RhinoDoc Doc;
+    public Dictionary<string, List<RhinoObject>> SchemaDictionary = new Dictionary<string, List<RhinoObject>>();
+
+    // internal vars for processing doc objects
+    private Dictionary<SupportedSchema, List<RhinoObject>> filterDictionary = new Dictionary<SupportedSchema, List<RhinoObject>>();
+    private List<RhinoObject> objsToBeFiltered;
+    #endregion
+
+    #region Constructors
+    public SchemaObjectFilter (List<RhinoObject> docObjects, Rhino.RhinoDoc doc)
     {
-        private List<string> SupportedSchemas = new List<string>() { "Floor", "Wall", "Roof", "Ceiling" };
+      Doc = doc;
 
-        #region Properties
-        private Rhino.RhinoDoc Doc;
-        public Dictionary<string, List<RhinoObject>> SchemaDictionary = new Dictionary<string, List<RhinoObject>>();
+      foreach (SupportedSchema schema in Enum.GetValues(typeof(SupportedSchema)))
+      {
+        filterDictionary.Add(schema, new List<RhinoObject>());
+        SchemaDictionary.Add(schema.ToString(), new List<RhinoObject>());
+      }
+      objsToBeFiltered = docObjects;
 
-        // internal vars for processing doc objects
-        private Dictionary<string, List<RhinoObject>> filterDictionary = new Dictionary<string, List<RhinoObject>>();
-        private List<RhinoObject> objsToBeFiltered;
-        #endregion
-
-        #region Constructors
-        public SchemaObjectFilter (List<RhinoObject> docObjects, Rhino.RhinoDoc doc)
-        {
-            Doc = doc;
-
-            foreach (string SupportedSchema in SupportedSchemas)
-            {
-                filterDictionary.Add(SupportedSchema, new List<RhinoObject>());
-                SchemaDictionary.Add(SupportedSchema, new List<RhinoObject>());
-            }
-            objsToBeFiltered = docObjects;
-
-            ApplyNamingFilter();
-            ApplyGeomFilter();
-        }
-        #endregion
-
-        #region Internal Methods
-        // check layer path and object name for all supported schemas
-        private void ApplyNamingFilter()
-        {
-            for (int j = objsToBeFiltered.Count - 1; j >= 0; j--)
-            {
-                bool applied = false; 
-
-                RhinoObject obj = objsToBeFiltered[j];
-                string objName = obj.Attributes.Name;
-                string objPath = Doc.Layers[obj.Attributes.LayerIndex].FullPath;
-
-                if (objName != null)
-                {
-                    foreach (string schema in SupportedSchemas)
-                    {
-                        if (objName.Contains(schema))
-                        {
-                            // add to filter dic and remove from filter list
-                            filterDictionary[schema].Add(obj);
-                            objsToBeFiltered.RemoveAt(j);
-                            applied = true;
-                            break;
-                        }
-                    }
-                }
-                if (!applied)
-                {
-                    foreach (string schema in SupportedSchemas)
-                    {
-                        if (objPath.Contains(schema))
-                        {
-                            // add to filter dic and remove from filter list
-                            filterDictionary[schema].Add(obj);
-                            objsToBeFiltered.RemoveAt(j);
-                            applied = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        private void ApplyGeomFilter()
-        {
-            // process the filter dictionary first and add all viable geom to the output dict
-            foreach (string schema in filterDictionary.Keys)
-            {
-                foreach (RhinoObject obj in filterDictionary[schema])
-                {
-                    if (IsViableObject(schema,obj))
-                    {
-                        SchemaDictionary[schema].Add(obj);
-                    }
-                }
-            }
-
-            // process any srf objs remaining in the objstobefiltered list
-            //ObjectEnumeratorSettings settings = new ObjectEnumeratorSettings();
-            //settings.ObjectTypeFilter = ObjectType.Surface | ObjectType.Brep | ObjectType.PolysrfFilter;
-            //List<RhinoObject> rhObjs = Doc.Objects.GetObjectList(settings).Where(o => objsToBeFiltered.Contains(o)).ToList();
-            ProcessSurfaceObjects(objsToBeFiltered);
-        }
-
-        // this will output a dictionary with supported schemas for surface objects
-        private void ProcessSurfaceObjects(List<RhinoObject> objs)
-        {
-            string floor = "Floor";
-            string wall = "Wall";
-
-            // try to select all xy planar srf: default to floor if so
-            foreach (RhinoObject obj in objs)
-            {
-                Brep brp = obj.Geometry as Brep;
-                if (brp.Surfaces.Count == 1)
-                {
-                    // test as floor first and then wall if this is a single face brp
-                    if (IsViableObject(floor, obj))
-                    {
-                        SchemaDictionary[floor].Add(obj);
-                    }
-                    else if (IsViableObject(wall, obj))
-                    {
-                        SchemaDictionary[wall].Add(obj);
-                    }
-                }
-                else
-                {
-                    // if multi surface, test if it may be a wall
-                    if (IsViableObject(wall, obj))
-                    {
-                        SchemaDictionary[wall].Add(obj);
-                    }
-                }
-            }
-        }
-
-        private bool IsViableObject(string schema, RhinoObject obj)
-        {
-            switch (schema)
-            {
-                case "Floor":
-                case "Ceiling":
-                case "Roof":
-                    // assumes xy planar single surface
-                    try
-                    {
-                        Brep brp = obj.Geometry as Brep;
-                        if (brp.Surfaces.Count > 1) { return false; }
-                        if (IsPlanar(brp.Surfaces.First(), out bool singleH, out bool singleV))
-                        {
-                            if (singleH)
-                            {
-                                return true;
-                            }
-                        }
-                    }
-                    catch
-                    {
-                        return false;
-                    }
-                    break;
-                case "Wall":
-                    // assumes all vertical planar surfaces
-                    try
-                    {
-                        Brep brp = obj.Geometry as Brep;
-                        bool allV = true;
-                        foreach (Surface srf in brp.Surfaces)
-                        {
-                            if (IsPlanar(srf, out bool isH, out bool isV))
-                            {
-                                if (!isV)
-                                {
-                                    allV = false; break;
-                                }
-                            }
-                        }
-                        if (allV) return true;
-                    }
-                    catch
-                    {
-                        return false;
-                    }
-                    break;
-                default:
-                    return false;
-            }
-            return false;
-        }
-
-        private bool IsPlanar(Surface srf, out bool isHorizontal, out bool isVertical)
-        {
-            isHorizontal = false;
-            isVertical = false;
-            Plane p = new Plane();
-
-            if (srf.TryGetPlane(out p))
-            {
-                Vector3d normal = p.Normal;
-                if (normal.Unitize())
-                {
-                    if (Math.Abs(normal.Z) == 1)
-                    {
-                        isHorizontal = true;
-                    }
-                    else if (normal.Z == 0)
-                    {
-                        isVertical = true;
-                    }
-                    return true;
-                }
-            }
-
-            return false;
-        }
-        #endregion
+      ApplyNamingFilter();
+      ApplyGeomFilter();
     }
+    #endregion
+
+    #region Internal Methods
+    // check layer path and object name for all supported schemas
+    private void ApplyNamingFilter()
+    {
+      for (int j = objsToBeFiltered.Count - 1; j >= 0; j--)
+      {
+        bool applied = false; 
+
+        RhinoObject obj = objsToBeFiltered[j];
+        string objName = obj.Attributes.Name;
+        string objPath = Doc.Layers[obj.Attributes.LayerIndex].FullPath;
+
+        if (objName != null)
+        {
+          foreach (SupportedSchema schema in Enum.GetValues(typeof(SupportedSchema)))
+          {
+            if (objName.Contains(schema.ToString()))
+            {
+                // add to filter dic and remove from filter list
+                filterDictionary[schema].Add(obj);
+                objsToBeFiltered.RemoveAt(j);
+                applied = true;
+                break;
+            }
+          }
+        }
+        if (!applied)
+        {
+          foreach (SupportedSchema schema in Enum.GetValues(typeof(SupportedSchema)))
+          {
+            if (objPath.Contains(schema.ToString()))
+            {
+                // add to filter dic and remove from filter list
+                filterDictionary[schema].Add(obj);
+                objsToBeFiltered.RemoveAt(j);
+                applied = true;
+                break;
+            }
+          }
+        }
+      }
+    }
+
+    private void ApplyGeomFilter()
+    {
+      // process the filter dictionary first and add all viable geom to the output dict
+      foreach (SupportedSchema schema in filterDictionary.Keys)
+        foreach (RhinoObject obj in filterDictionary[schema])
+          if (IsViableObject(schema,obj))
+            SchemaDictionary[schema.ToString()].Add(obj);
+      ProcessSurfaceObjects(objsToBeFiltered);
+    }
+
+    // this will output a dictionary with supported schemas for surface objects
+    private void ProcessSurfaceObjects(List<RhinoObject> objs)
+    {
+      foreach (RhinoObject obj in objs)
+      {
+        Brep brp = obj.Geometry as Brep;
+        if (brp.Surfaces.Count == 1) // test as floor first and then wall if this is a single face brp
+        {
+          if (IsViableObject(SupportedSchema.Floor, obj))
+            SchemaDictionary[SupportedSchema.Floor.ToString()].Add(obj);
+          else if (IsViableObject(SupportedSchema.Wall, obj))
+            SchemaDictionary[SupportedSchema.Wall.ToString()].Add(obj);
+        }
+        else // if multi surface, test if it may be a wall
+        {
+          if (IsViableObject(SupportedSchema.Wall, obj))
+            SchemaDictionary[SupportedSchema.Wall.ToString()].Add(obj);
+        }
+      }
+    }
+
+    private bool IsViableObject(SupportedSchema schema, RhinoObject obj)
+    {
+      switch (schema)
+      {
+        case SupportedSchema.Floor:
+        case SupportedSchema.Ceiling:
+        case SupportedSchema.Roof:
+          // assumes xy planar single surface
+          try
+          {
+            Brep brp = obj.Geometry as Brep;
+            if (brp.Surfaces.Count > 1) { return false; }
+            if (IsPlanar(brp.Surfaces.First(), out bool singleH, out bool singleV))
+              if (singleH)
+                  return true;
+          }
+          catch
+          {
+              return false;
+          }
+          break;
+        case SupportedSchema.Wall:
+          // assumes all vertical planar surfaces
+          try
+          {
+            Brep brp = obj.Geometry as Brep;
+            bool allV = true;
+            foreach (Surface srf in brp.Surfaces)
+            {
+              if (IsPlanar(srf, out bool isH, out bool isV))
+                if (!isV)
+                    allV = false; break;
+            }
+            if (allV) 
+              return true;
+          }
+          catch
+          {
+              return false;
+          }
+          break;
+        default:
+          return false;
+      }
+      return false;
+    }
+
+    private bool IsPlanar(Surface srf, out bool isHorizontal, out bool isVertical)
+    {
+      isHorizontal = false;
+      isVertical = false;
+      Plane p = new Plane();
+
+      if (srf.TryGetPlane(out p))
+      {
+        Vector3d normal = p.Normal;
+        if (normal.Unitize())
+        {
+          if (Math.Abs(normal.Z) == 1)
+              isHorizontal = true;
+          else if (normal.Z == 0)
+              isVertical = true;
+          return true;
+        }
+      }
+
+      return false;
+    }
+    #endregion
+  }
 }
