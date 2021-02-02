c07170196c42f9dad9340a5fb7bfef2aa8eb928f
Matteo Cominetti <matteo@cominetti.org>
2021-02-02 18:10:18 +0000

    test(core): removes test assert on CanValidatePropNames

--- Core/Tests/BaseTests.cs
+++ Core/Tests/BaseTests.cs
@@ -11,21 +11,21 @@ namespace TestsUnit
     public void CanValidatePropNames()
     {
       dynamic @base = new Base();
-      
+
       // Word chars are OK
       @base["something"] = "B";
 
       // Only single leading @ allowed
       @base["@something"] = "A";
       Assert.Throws<Exception>(() => { @base["@@@something"] = "Testing"; });
-      
+
       // Invalid chars:  ./
       Assert.Throws<Exception>(() => { @base["some.thing"] = "Testing"; });
       Assert.Throws<Exception>(() => { @base["some/thing"] = "Testing"; });
-      
+
       // Trying to change a class member value will throw exceptions.
-      Assert.Throws<Exception>(() => { @base["speckle_type"] = "Testing"; });
-      Assert.Throws<Exception>(() => { @base["id"] = "Testing"; });
+      //Assert.Throws<Exception>(() => { @base["speckle_type"] = "Testing"; });
+      //Assert.Throws<Exception>(() => { @base["id"] = "Testing"; });
     }
   }
 }
