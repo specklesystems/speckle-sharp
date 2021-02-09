d56cb3acb5486a4108d00d26c234e4285e6b4469
clairekuang <kuang.claire@gmail.com>
2021-02-09 23:59:58 +0000

    minor name fixes omg

--- ConnectorAutocadCivil/ConnectorAutocad2021/Properties/AssemblyInfo.cs
+++ ConnectorAutocadCivil/ConnectorAutocad2021/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConnectorAutoCAD2021")]
+[assembly: AssemblyTitle("ConnectorAutocad2021")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConnectorAutoCAD2021")]
+[assembly: AssemblyProduct("ConnectorAutocad2021")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
