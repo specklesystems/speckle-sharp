ad0ce3d5bbd512b7619968d88b0ad67492c03b6b
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-03-02 10:49:35 +0000

    refactor(dyn): remove excess imports

--- ConnectorDynamo/ConnectorDynamoFunctions/BatchConverter.cs
+++ ConnectorDynamo/ConnectorDynamoFunctions/BatchConverter.cs
@@ -1,15 +1,10 @@
 ﻿using Autodesk.DesignScript.Runtime;
-using Newtonsoft.Json;
-using ProtoCore.Lang;
 using Speckle.Core.Kits;
 using Speckle.Core.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Speckle.ConnectorDynamo.Functions
 {
