71bcb303e4d3bf65215573bb0c52e558c35499bb
izzy lyseggen <izzy.lyseggen@gmail.com>
2021-03-09 11:07:48 +0000

    fix(revit): swallow unresolvable errors

--- ConnectorRevit/ConnectorRevit/Revit/ErrorEater.cs
+++ ConnectorRevit/ConnectorRevit/Revit/ErrorEater.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Autodesk.Revit.DB;
+using Speckle.ConnectorRevit.Entry;
+using Speckle.ConnectorRevit.UI;
 using Speckle.Core.Kits;
 using Speckle.Core.Models;
 
@@ -10,16 +14,20 @@ namespace ConnectorRevit.Revit
   public class ErrorEater : IFailuresPreprocessor
   {
     private ISpeckleConverter _converter;
+
     public ErrorEater(ISpeckleConverter converter)
     {
       _converter = converter;
     }
+
     public FailureProcessingResult PreprocessFailures(FailuresAccessor failuresAccessor)
     {
       IList<FailureMessageAccessor> failList = new List<FailureMessageAccessor>();
+      var criticalFails = 0;
+      var failedElements = new List<ElementId>();
       // Inside event handler, get all warnings
       failList = failuresAccessor.GetFailureMessages();
-      foreach (FailureMessageAccessor failure in failList)
+      foreach ( FailureMessageAccessor failure in failList )
       {
         // check FailureDefinitionIds against ones that you want to dismiss, 
         //FailureDefinitionId failID = failure.GetFailureDefinitionId();
@@ -27,9 +35,23 @@ namespace ConnectorRevit.Revit
         //if (failID == BuiltInFailures.RoomFailures.RoomNotEnclosed)
         //{
         var t = failure.GetDescriptionText();
-        var r = failure.GetDefaultResolutionCaption();
+        _converter.ConversionErrors.Add(new Error {message = t, details = ""});
 
-        _converter.ConversionErrors.Add(new Error { message = t, details = "" });
+        var s = failure.GetSeverity();
+        if ( s == FailureSeverity.Warning ) continue;
+        try
+        {
+          failuresAccessor.ResolveFailure(failure);
+        }
+        catch ( Exception e )
+        {
+          // currently, the whole commit is rolled back. this should be investigated further at a later date
+          // to properly proceed with commit
+          failedElements.AddRange(failure.GetFailingElementIds());
+          _converter.ConversionErrors.Clear();
+          _converter.ConversionErrors.Add(new Error { message = "Objects failed to bake due to fatal error: " + t, details = "Fatal Error"});
+          return FailureProcessingResult.ProceedWithCommit;
+        }
       }
 
       failuresAccessor.DeleteAllWarnings();
