4d842e51d73b572cfa4f847b5792547e5741ffed
Matteo Cominetti <matteo@speckle.systems>
2021-03-17 20:58:37 +0000

    Ci (#362)
    * ci: moves core tests into same job
    
    * ci: integration tests to use new server

--- Core/IntegrationTests/Api.cs
+++ Core/IntegrationTests/Api.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Speckle.Core.Api;
@@ -9,10 +6,6 @@ using Speckle.Core.Credentials;
 using Speckle.Core.Models;
 using Speckle.Core.Transports;
 using Tests;
-using System.Net;
-using System.Collections.Specialized;
-using System.Text;
-using Newtonsoft.Json;
 
 ////////////////////////////////////////////////////////////////////////////
 /// NOTE:                                                                ///
@@ -39,7 +32,7 @@ namespace TestsIntegration
     [OneTimeSetUp]
     public void Setup()
     {
-      testServer = new ServerInfo { url = "https://testing.speckle.dev", name = "TestServer" };
+      testServer = new ServerInfo { url = "https://latest.speckle.dev", name = "TestServer" };
 
       firstUserAccount = Fixtures.SeedUser(testServer);
       secondUserAccount = Fixtures.SeedUser(testServer);
@@ -233,7 +226,7 @@ namespace TestsIntegration
     public async Task StreamGetCommits()
     {
       var res = await myClient.StreamGetCommits(streamId);
-      
+
       Assert.NotNull(res);
       Assert.AreEqual(commitId, res[0].id);
     }
