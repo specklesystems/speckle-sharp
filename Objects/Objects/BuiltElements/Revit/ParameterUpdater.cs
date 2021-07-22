3d9a8ae4cfbba3ac161d8375620dd75d29535d95
Matteo Cominetti <matteo@speckle.systems>
2021-07-22 19:24:03 +0200

    feat: improves param setting from excel

--- Objects/Objects/BuiltElements/Revit/ParameterUpdater.cs
+++ Objects/Objects/BuiltElements/Revit/ParameterUpdater.cs
@@ -9,14 +9,14 @@ namespace Objects.BuiltElements.Revit
 {
   public class ParameterUpdater : Base
   {
-    public string revitId { get; set; }
+    public string elementId { get; set; }
     public Base parameters { get; set; }
 
 
     [SchemaInfo("ParameterUpdater", "Updates parameters on a Revit element by id", "Revit", "Families")]
     public ParameterUpdater([SchemaParamInfo("A Revit ElementId or UniqueId")] string id, List<Parameter> parameters)
     {
-      this.revitId = id;
+      this.elementId = id;
       this.parameters = parameters.ToBase();
     }
 
