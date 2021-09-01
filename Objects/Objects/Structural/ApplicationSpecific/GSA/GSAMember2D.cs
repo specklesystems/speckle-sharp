c1b9673b6e9f7408ac0a62203716aa0fa0199f1c
Jenessa Man <jenessa.man@arup.com>
2021-08-31 21:21:38 -0400

    Additional updates per discussion with NB, GT and DDK

--- Objects/Objects/Structural/ApplicationSpecific/GSA/GSAMember2D.cs
+++ Objects/Objects/Structural/ApplicationSpecific/GSA/GSAMember2D.cs
@@ -6,10 +6,11 @@ using Objects.Geometry;
 using Objects.Structural.Geometry;
 using Objects.Structural.Properties;
 
-namespace Objects.Structural.GSA.Geometry //GSA.Geometry?
+namespace Objects.Structural.GSA.Geometry 
 {
     public class GSAMember2D : Element2D
     {
+        public List<List<Node>> voids { get; set; } = new List<List<Node>>();
         public int nativeId { get; set; }
         public int group { get; set; }
         public string colour { get; set; }
@@ -17,7 +18,7 @@ namespace Objects.Structural.GSA.Geometry //GSA.Geometry?
         public bool isDummy { get; set; }
         public bool intersectsWithOthers { get; set; }
         public double targetMeshSize { get; set; }
-        //public Result results { get; set; }
+
         public GSAMember2D() { }
 
         [SchemaInfo("GSAMember2D", "Creates a Speckle structural 2D member for GSA")]
@@ -25,6 +26,18 @@ namespace Objects.Structural.GSA.Geometry //GSA.Geometry?
         {
             this.baseMesh = baseMesh;
             this.property = property;
+            this.type = type;
+            this.offset = offset;
+            this.orientationAngle = orientationAngle;
+        }
+
+        [SchemaInfo("GSAMember2D", "Creates a Speckle structural 2D member for GSA", "GSA", "Geometry")]
+        public GSAMember2D([SchemaParamInfo("An ordered list of nodes which represents the perimeter of a member (ie. order should based on valid polyline)")] List<Node> perimeter, 
+            [SchemaParamInfo("A list of ordered lists of nodes representing the voids within a member (ie. order should be based on valid polyline)")] List<List<Node>> voids, 
+            ElementType2D type, double offset = 0, double orientationAngle = 0)
+        {
+            this.topology = perimeter; //needs to be ordered properly (to match valid polyline order)
+            this.voids = voids; //needs to be ordered properly (to match valid polyline order)
             this.type = type;
             this.offset = offset;
             this.orientationAngle = orientationAngle;
