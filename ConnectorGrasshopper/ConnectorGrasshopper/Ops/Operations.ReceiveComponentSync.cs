c4765614783dff3be6d7e2fc2d34b0d447c14f05
spsarras <psarras.st@gmail.com>
2021-09-02 17:42:51 +0100

    using the SelectKitTaskCapableComponent

--- ConnectorGrasshopper/ConnectorGrasshopper/Ops/Operations.ReceiveComponentSync.cs
+++ ConnectorGrasshopper/ConnectorGrasshopper/Ops/Operations.ReceiveComponentSync.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ConnectorGrasshopper.Extras;
+using ConnectorGrasshopper.Objects;
 using ConnectorGrasshopper.Properties;
 using GH_IO.Serialization;
 using Grasshopper.Kernel;
@@ -21,19 +22,19 @@ using Speckle.Core.Transports;
 
 namespace ConnectorGrasshopper.Ops
 {
-  public class ReceiveSync : GH_TaskCapableComponent<Speckle.Core.Models.Base>
+  public class ReceiveSync : SelectKitTaskCapableComponentBase<Speckle.Core.Models.Base>
   {
     CancellationTokenSource source;
     const int delay = 100000;
     public ISpeckleKit Kit;
-    public ISpeckleConverter Converter;
+    //public ISpeckleConverter Converter;
     public TaskCreationOptions? TaskCreationOptions { get; set; } = null;
     public StreamWrapper StreamWrapper { get; set; }
     private Client ApiClient { get; set; }
     public string ReceivedCommitId { get; set; }
     public string InputType { get; set; }
     public bool AutoReceive { get; set; }
-    public bool ConvertToNative { get; set; } = true;
+    //public bool ConvertToNative { get; set; } = true;
     public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.obscure;
 
     public override void DocumentContextChanged(GH_Document document, GH_DocumentContext context)
@@ -101,16 +102,6 @@ namespace ConnectorGrasshopper.Ops
       if (StreamWrapper.Type == StreamWrapperType.Branch && e.branchName != StreamWrapper.BranchName) return;
       HandleNewCommit();
     }
-    public void SetConverterFromKit(string kitName)
-    {
-      if (kitName == Kit?.Name) return;
-
-      Kit = KitManager.Kits.FirstOrDefault(k => k.Name == kitName);
-      Converter = Kit.LoadConverter(Applications.Rhino);
-
-      Message = $"Using the {Kit.Name} Converter";
-      ExpireSolution(true);
-    }
 
     /// <summary>
     /// Initializes a new instance of the Operations class.
@@ -126,12 +117,6 @@ namespace ConnectorGrasshopper.Ops
       Menu_AppendSeparator(menu);
       Menu_AppendItem(menu, "Select the converter you want to use:");
       var kits = KitManager.GetKitsWithConvertersForApp(Applications.Rhino);
-
-      Menu_AppendItem(menu, $"{nameof(ConvertToNative)}", (s, e) =>
-      {
-        ConvertToNative = !ConvertToNative;
-        ExpireSolution(true);
-      }, true, ConvertToNative);
 
       foreach (var kit in kits)
         Menu_AppendItem(menu, $"{kit.Name} ({kit.Description})", (s, e) =>
@@ -175,7 +160,7 @@ namespace ConnectorGrasshopper.Ops
       writer.SetString("KitName", Kit.Name);
       var streamUrl = StreamWrapper != null ? StreamWrapper.ToString() : "";
       writer.SetString("StreamWrapper", streamUrl);
-      writer.SetBoolean(nameof(ConvertToNative), ConvertToNative);
+      //writer.SetBoolean(nameof(ConvertToNative), ConvertToNative);
       return base.Write(writer);
     }
 
@@ -210,7 +195,7 @@ namespace ConnectorGrasshopper.Ops
       else
         SetDefaultKitAndConverter();
 
-      ConvertToNative = reader.GetBoolean(nameof(ConvertToNative));
+      //ConvertToNative = reader.GetBoolean(nameof(ConvertToNative));
 
       return base.Read(reader);
     }
@@ -264,11 +249,6 @@ namespace ConnectorGrasshopper.Ops
       {
         var task = Task.Run(async () =>
         {
-          if (StreamWrapper == null)
-          {
-            AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Select a Kit");
-            return null;
-          }
           var acc = await StreamWrapper?.GetAccount();
           var client = new Client(acc);
           var remoteTransport = new ServerTransport(acc, StreamWrapper?.StreamId);
@@ -319,39 +299,28 @@ namespace ConnectorGrasshopper.Ops
         ReceivedObjectId = ReceivedObject.id;
 
         //the active document may have changed
-        Converter.SetContextDocument(RhinoDoc.ActiveDoc);
+        Converter?.SetContextDocument(RhinoDoc.ActiveDoc);
+        GH_Structure<IGH_Goo> data;
 
-        dynamic data = null;
-        // case 1: it's an item that has a direct conversion method, eg a point
-        var canConvert = Converter.CanConvertToNative(ReceivedObject);
-
-        if (canConvert && !ConvertToNative)
+        if (Converter != null && Converter.CanConvertToNative(ReceivedObject))
         {
-          data = Extras.Utilities.TryConvertItemToNative(ReceivedObject, Converter);
-          DA.SetData(0, data);
-          return;
+          var converted = Converter.ConvertToNative(ReceivedObject);
+          data = new GH_Structure<IGH_Goo>();
+          data.Append(Extras.Utilities.TryConvertItemToNative(converted, Converter));
         }
-        else if (!canConvert)
+        else if (ReceivedObject.GetDynamicMembers().Count() == 1)
         {
-          var members = ReceivedObject.GetDynamicMembers();
-
-          if (members.Count() == 1)
-          {
-            var treeBuilder = new TreeBuilder(Converter) { ConvertToNative = ConvertToNative };
-            var tree = treeBuilder.Build(ReceivedObject[members.ElementAt(0)]);
-
-            DA.SetDataTree(0, tree);
-            return;
-          }
+          var treeBuilder = new TreeBuilder(Converter);
+          var tree = treeBuilder.Build(ReceivedObject[ReceivedObject.GetDynamicMembers().ElementAt(0)]);
+          data = tree;
         }
         else
         {
-          data = ReceivedObject;
-          DA.SetData(0, data);
-          return;
+          data = new GH_Structure<IGH_Goo>();
+          data.Append(new GH_SpeckleBase(ReceivedObject));
         }
 
-
+        DA.SetDataTree(0, data);
       }
     }
 
