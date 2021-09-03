7beef535167a45ab9c22d4ad519ea0b1b28a1776
Jenessa Man <jenessa.man@arup.com>
2021-09-03 16:55:48 -0400

    Add section profile classes, make updates per continued discussion with GT

--- Objects/Objects/Structural/ApplicationSpecific/GSA/GSAProperties.cs
+++ Objects/Objects/Structural/ApplicationSpecific/GSA/GSAProperties.cs
@@ -11,15 +11,15 @@ namespace Objects.Structural.GSA.Properties
     public class GSAProperty1D : Property1D
     {
         public int nativeId { get; set; }
-        public Material designMaterial { get; set; } 
-        public double cost { get; set; }
-        public double additionalMass { get; set; } 
-        public int poolRef { get; set; }
+        public Material designMaterial { get; set; }
+        public double additionalMass { get; set; }
+        public double? cost { get; set; }        
+        public int? poolRef { get; set; }
         public string colour { get; set; }
         public GSAProperty1D() { }
 
         [SchemaInfo("GSAProperty1D", "Creates a Speckle structural 1D element property for GSA", "GSA", "Properties")]
-        public GSAProperty1D(int nativeId, string name, Material material, string grade, SectionProfile profile, double cost = 0, double additionalMass = 0, int poolRef = 0)
+        public GSAProperty1D(int nativeId, string name, Material material, string grade, SectionProfile profile, double cost = 0, double additionalMass = 0)
         {
             this.nativeId = nativeId;
             this.name = name;
@@ -27,9 +27,7 @@ namespace Objects.Structural.GSA.Properties
             this.grade = grade;
             this.profile = profile;
             this.nativeId = nativeId;
-            this.cost = cost;
             this.additionalMass = additionalMass;
-            this.poolRef = poolRef;
         }
     }
 
