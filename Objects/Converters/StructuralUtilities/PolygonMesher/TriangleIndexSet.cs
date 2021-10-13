c1f7c7ff73741c2c87e3ae3eae6f95b4a23b86b7
Jenessa Man <jenessa.man@arup.com>
2021-10-13 18:31:57 -0400

    Add v1 polygon mesher for member meshes

---
+++ Objects/Converters/StructuralUtilities/PolygonMesher/TriangleIndexSet.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace StructuralUtilities.PolygonMesher
+{
+  internal class TriangleIndexSet : IndexSet
+  {
+    public TriangleIndexSet(int index1, int index2, int index3) : base(new List<int>() { index1, index2, index3 })
+    {
+    }
+  }
+}
