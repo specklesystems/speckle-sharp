abc7e8374e71f29482484d88a229bff6c30a7d92
Reynold Chan <reynold.ch@gmail.com>
2021-10-13 22:01:50 -0400

    added concrete sections (rectangular, Tee, circular)

--- Objects/Converters/ConverterETABS/ConverterETABSShared/Partial Classes/Properties/Convert1DProperty.cs
+++ Objects/Converters/ConverterETABS/ConverterETABSShared/Partial Classes/Properties/Convert1DProperty.cs
@@ -13,9 +13,40 @@ namespace Objects.Converter.ETABS
     {
         public object Property1DToNative(Property1D property1D)
         {
-            SectionProfile.Catalogue sectionProfile = (SectionProfile.Catalogue)property1D.profile;
-            Model.PropFrame.ImportProp(property1D.name, property1D.material.name, sectionProfile.catalogueName + ".xml", sectionProfile.sectionName);
-            return property1D.name;
+            var catalogue = new SectionProfile.Catalogue();
+            if (property1D.profile.GetType().Equals(catalogue.GetType()))
+            {
+                SectionProfile.Catalogue sectionProfile = (SectionProfile.Catalogue)property1D.profile;
+                Model.PropFrame.ImportProp(property1D.name, property1D.material.name, sectionProfile.catalogueName + ".xml", sectionProfile.sectionName);
+                return property1D.name;
+            }
+            var rectangle = new SectionProfile.Rectangular();
+            if (property1D.profile.GetType().Equals(rectangle.GetType()))
+            {
+                SectionProfile.Rectangular sectionProfile = (SectionProfile.Rectangular)property1D.profile;
+                Model.PropFrame.SetRectangle(property1D.name, property1D.material.name, sectionProfile.depth, sectionProfile.width);
+                return property1D.name;
+            }
+
+            var circular = new SectionProfile.Circular();
+            if (property1D.profile.GetType().Equals(circular.GetType()))
+            {
+                SectionProfile.Circular sectionProfile = (SectionProfile.Circular)property1D.profile;
+                Model.PropFrame.SetCircle(property1D.name, property1D.material.name, sectionProfile.radius * 2);
+                return property1D.name;
+            }
+
+            var T = new SectionProfile.Tee();
+            if (property1D.profile.GetType().Equals(T.GetType()))
+            {
+                SectionProfile.Tee sectionProfile = (SectionProfile.Tee)property1D.profile;
+                Model.PropFrame.SetConcreteTee(property1D.name, property1D.material.name, sectionProfile.depth, sectionProfile.width, sectionProfile.flangeThickness, sectionProfile.webThickness, sectionProfile.webThickness, false);
+                return property1D.name;
+            }
+
+;
+            return null;
+
         }
         public  Property1D Property1DToSpeckle(string name)
         {
@@ -27,6 +58,7 @@ namespace Objects.Converter.ETABS
             speckleStructProperty1D.profile = SectionToSpeckle(name);
             return speckleStructProperty1D;
         }
+
 
     }
 }
--- Objects/Converters/ConverterETABS/ConverterETABSShared/Partial Classes/Properties/ConvertSectionProfile.cs
+++ Objects/Converters/ConverterETABS/ConverterETABSShared/Partial Classes/Properties/ConvertSectionProfile.cs
@@ -15,20 +15,55 @@ namespace Objects.Converter.ETABS
             string catalogue = "";
             string matProp = "";
             string sectionPropertyName = "";
-            Model.PropFrame.GetNameInPropFile(property, ref sectionPropertyName, ref catalogue, ref matProp, ref propType);
-            string[] arrayCatPath =  catalogue.Split('\\');
-            catalogue = arrayCatPath.Last();
-            arrayCatPath = catalogue.Split('.');
-            catalogue = arrayCatPath[0];
-            switch (propType)
+            int s = Model.PropFrame.GetNameInPropFile(property, ref sectionPropertyName, ref catalogue, ref matProp, ref propType);
+            if(s == 0)
             {
-                case eFramePropType.SD:
-                    speckleSectionProfile = new SectionProfile.Explicit();
-                    break;
-                default:
-                    speckleSectionProfile = new SectionProfile.Catalogue(property, catalogue, propType.ToString(), sectionPropertyName);
-                    break;
+                string[] arrayCatPath = catalogue.Split('\\');
+                catalogue = arrayCatPath.Last();
+                arrayCatPath = catalogue.Split('.');
+                catalogue = arrayCatPath[0];
+                switch (propType)
+                {
+                    case eFramePropType.SD:
+                        speckleSectionProfile = new SectionProfile.Explicit();
+                        break;
+                    default:
+                        speckleSectionProfile = new SectionProfile.Catalogue(property, catalogue, propType.ToString(), sectionPropertyName);
+                        break;
+                }
             }
+
+            double T3 = 0;
+            double T2 = 0;
+            int color = 0;
+            string notes = "";
+            string GUID = "";
+            string FileName = "";
+            s = Model.PropFrame.GetRectangle(property, ref FileName, ref matProp, ref T3, ref T2, ref color, ref notes, ref GUID);
+
+            if (s == 0)
+            {
+                speckleSectionProfile = new SectionProfile.Rectangular(property,T3,T2);
+            }
+
+            double Tf = 0;
+            double TwF = 0;
+            double Twt = 0;
+            bool mirrorAbout3 = false;
+            s = Model.PropFrame.GetConcreteTee(property, ref FileName, ref matProp, ref T3, ref T2, ref Tf, ref TwF, ref Twt, ref mirrorAbout3, ref color, ref notes, ref GUID);
+            if (s == 0)
+            {
+                speckleSectionProfile = new SectionProfile.Tee(property,T3,T2,TwF,Tf);
+            }
+
+            s = Model.PropFrame.GetCircle(Name, ref FileName, ref matProp, ref T3, ref color, ref notes, ref GUID);
+            if (s == 0)
+            {
+                speckleSectionProfile = new SectionProfile.Circular(property, T3 / 2);
+            }
+            
+
+
 
 
             return speckleSectionProfile;
