bb7854c7f6bff0ddb45d09d79c65ff2980a6491f
Reynold Chan <reynold.ch@gmail.com>
2021-11-11 02:20:22 -0500

    result global

--- ConnectorETABS/ConverterETABSTests/UnitTest1.cs
+++ ConnectorETABS/ConverterETABSTests/UnitTest1.cs
@@ -1,17 +1,22 @@
 using NUnit.Framework;
-using ETABSv1;
+using Speckle.Core.Kits;
+//using ETABSv1;
 
 namespace ConverterETABSTests
 {
     public class Tests
     {
+        //cSapModel Model { get; set; }
         [SetUp]
         public void Setup()
         {
-            ETABSv1.cOAPI myETABSObject;
-            ETABSv1.cHelper myHelper = new ETABSv1.Helper();
-
-            myETABSObject = myHelper.CreateObjectProgID("CSI.ETABS.API.ETABSObject");
+            var kits = KitManager.GetDefaultKit();
+            var converter = kits.LoadConverter(Applications.ETABSv18);
+            //cOAPI myETABSObject;
+            //cHelper myHelper = new Helper();
+            //myETABSObject = myHelper.CreateObjectProgID("CSI.ETABS.API.ETABSObject");
+            //Model = myETABSObject.SapModel;
+            //converter.SetContextDocument(Model);
         }
 
         [Test]
