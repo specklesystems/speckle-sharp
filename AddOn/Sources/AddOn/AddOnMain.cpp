53846525602c92261a944c5d7496091f5a6bac53
neszojabe <94619277+neszojabe@users.noreply.github.com>
2021-11-19 09:39:55 +0200

    Start Archicad connector app from AddOn
    Search the Archicad connector next to the AddOn (./ConnectorArchicad/ConnectorArchicad.exe) and starts it in a separate process.

--- AddOn/Sources/AddOn/AddOnMain.cpp
+++ AddOn/Sources/AddOn/AddOnMain.cpp
@@ -5,34 +5,76 @@
 #include "Process.hpp"
 #include "ResourceIds.hpp"
 
-static const GSResID AddOnInfoID			= ID_ADDON_INFO;
-	static const Int32 AddOnNameID			= 1;
-	static const Int32 AddOnDescriptionID	= 2;
+static const GSResID AddOnInfoID 			= ID_ADDON_INFO;
+static const Int32 AddOnNameID 				= 1;
+static const Int32 AddOnDescriptionID 		= 2;
 
-static const short AddOnMenuID				= ID_ADDON_MENU;
-	static const Int32 AddOnCommandID		= 1;
+static const short AddOnMenuID 				= ID_ADDON_MENU;
+static const Int32 AddOnCommandID 			= 1;
 
-static GSErrCode MenuCommandHandler (const API_MenuParams *menuParams)
+
+static GS::UniString GetPlatformSpecificExecutable ()
+{
+#if defined (macintosh)
+	return "";
+#endif
+#if defined (WINDOWS)
+
+	static const char* FolderName = "ConnectorArchicad";
+	static const char* FileName = "ConnectorArchicad.exe";
+
+	IO::Location ownFileLoc;
+	const auto err = ACAPI_GetOwnLocation (&ownFileLoc);
+	if (err != NoError) {
+		return "";
+	}
+
+	ownFileLoc.DeleteLastLocalName ();
+	ownFileLoc.AppendToLocal (IO::Name (FolderName));
+	ownFileLoc.AppendToLocal (IO::Name (FileName));
+
+	GS::UniString executableStr;
+	ownFileLoc.ToPath (&executableStr);
+
+	return executableStr;
+
+#endif
+}
+
+static GS::Array<GS::UniString> GetExecutableArguments ()
+{
+	UShort portNumber;
+	const auto err = ACAPI_Goodies (APIAny_GetHttpConnectionPortID, &portNumber);
+
+	if (err != NoError) {
+		throw GS::IllegalArgumentException ();
+	}
+
+	return GS::Array<GS::UniString> { GS::ValueToUniString (portNumber) };
+}
+
+static GSErrCode MenuCommandHandler (const API_MenuParams* menuParams)
 {
 	switch (menuParams->menuItemRef.menuResID) {
-		case AddOnMenuID:
-			switch (menuParams->menuItemRef.itemIndex) {
-				case AddOnCommandID:
-					{
-#if defined (macintosh)
-						GS::UniString command = "";
-						GS::Array<GS::UniString> arguments;
-						try {
-							GS::Process process = GS::Process::Create (command, arguments);
-						} catch (GS::Exception&) {
-							DG::ErrorAlert ("Error", "Can't start Speckle UI", "OK");
-						}
-#endif
-					}
-					break;
+	case AddOnMenuID:
+		switch (menuParams->menuItemRef.itemIndex) {
+		case AddOnCommandID:
+		{
+			try {
+				const GS::UniString command = GetPlatformSpecificExecutable ();
+				const GS::Array<GS::UniString> arguments = GetExecutableArguments ();
+
+				GS::Process process = GS::Process::Create (command, arguments);
 			}
-			break;
+			catch (GS::Exception&) {
+				DG::ErrorAlert ("Error", "Can't start Speckle UI", "OK");
+			}
+		}
+		break;
 	}
+	break;
+	}
+
 	return NoError;
 }
 
