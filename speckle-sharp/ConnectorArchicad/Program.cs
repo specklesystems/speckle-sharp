c7533221fc39e44a3cfbbdc40e94e1d10eb666c7
neszojabe <94619277+neszojabe@users.noreply.github.com>
2021-11-19 08:03:13 +0200

    Basic Connector for Archicad
    This executable will be started from the Archicad Addon. Uses the dummy connector bindings (later will be replaced)

---
+++ speckle-sharp/ConnectorArchicad/Program.cs
@@ -0,0 +1,39 @@
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+using System.IO;
+
+
+namespace Archicad.Launcher
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            BuildAvaloniaApp ().StartWithClassicDesktopLifetime (args, Avalonia.Controls.ShutdownMode.OnMainWindowClose);
+        }
+
+        public static AppBuilder BuildAvaloniaApp ()
+        {
+            return AppBuilder.Configure<DesktopUI2.App> ()
+                .UsePlatformDetect ()
+                .With (new X11PlatformOptions { UseGpu = false })
+                .With (new MacOSPlatformOptions { ShowInDock = false })
+                .With (new AvaloniaNativePlatformOptions { AvaloniaNativeLibraryPath = GetAvaloniaNativeLibraryPath () })
+                .With (new SkiaOptions { MaxGpuResourceSizeBytes = 8096000 })
+                .With (new Win32PlatformOptions { AllowEglInitialization = true, EnableMultitouch = false })
+                .LogToTrace ()
+                .UseReactiveUI ();
+        }
+
+        private static string GetAvaloniaNativeLibraryPath ()
+		{
+			string path = Path.GetDirectoryName (typeof (DesktopUI2.App).Assembly.Location);
+            if (path is null)
+			{
+                return null;
+			}
+
+            return Path.Combine (path, "Native", "libAvalonia.Native.OSX.dylib");
+        }
+  }
+}
