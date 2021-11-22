695eb661f30b139c54f57da5133e2a60db92abd8
Hudumannaia <kezdiors@gmail.com>
2021-11-22 18:26:46 +0200

    Format/indenting and cmake workaround

--- AddOn/Sources/AddOn/Commands/GetElementTypes.cpp
+++ AddOn/Sources/AddOn/Commands/GetElementTypes.cpp
@@ -44,23 +44,25 @@ API_AddOnCommandExecutionPolicy GetElementTypes::GetExecutionPolicy () const
 	return API_AddOnCommandExecutionPolicy::ScheduleForExecutionOnMainThread; 
 }
 
-static const GS::HashTable<API_ElemTypeID, GS::UniString> supportedElementTypes{	{API_ZombieElemID,""},
-																					{API_WallID,"Wall"},
-																					{API_ColumnID,"Column"},
-																					{API_BeamID,"Beam"},
-																					{API_WindowID,"Window"},
-																					{API_DoorID,"Door"},
-																					{API_ObjectID,"Object"},
-																					{API_LampID,"Lamp"},
-																					{API_SlabID,"Slab"},
-																					{API_RoofID,"Roof"},
-																					{API_MeshID,"Mesh"},
-																					{API_CurtainWallID,"CurtainWall"},
-																					{API_ShellID,"Shell"},
-																					{API_SkylightID,"Skylight"},
-																					{API_MorphID,"Morph"},
-																					{API_StairID,"Stair"},
-																					{API_RailingID,"Railing"}
+static const GS::HashTable<API_ElemTypeID, GS::UniString> supportedElementTypes
+{
+	{ API_ZombieElemID,					"ZombieElem"},
+	{ API_WallID,						"Wall"},
+	{ API_ColumnID,						"Column"},
+	{ API_BeamID,						"Beam"},
+	{ API_WindowID,						"Window"},
+	{ API_DoorID,						"Door"},
+	{ API_ObjectID,						"Object"},
+	{ API_LampID,						"Lamp"},
+	{ API_SlabID,						"Slab"},
+	{ API_RoofID,						"Roof"},
+	{ API_MeshID,						"Mesh"},
+	{ API_CurtainWallID,				"CurtainWall"},
+	{ API_ShellID,						"Shell"},
+	{ API_SkylightID,					"Skylight"},
+	{ API_MorphID,						"Morph"},
+	{ API_StairID,						"Stair"},
+	{ API_RailingID,					"Railing"}
 };
 
 
@@ -75,7 +77,7 @@ GS::ObjectState GetElementTypes::Execute (const GS::ObjectState& parameters, GS:
 
 	const auto& listAdder = retVal.AddList<GS::UniString> (ElementTypesFieldName);
 	for (const API_Guid& guid : elementGuids) {
-		listAdder (supportedElementTypes[Utility::GetElementType (guid)]);
+		listAdder (supportedElementTypes.Get (Utility::GetElementType (guid)));
 	}
 
 	return retVal;
