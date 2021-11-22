8a55841121f79cd725dd50cecd92c5c3101c1162
JR-Morgan <45512892+JR-Morgan@users.noreply.github.com>
2021-11-22 15:30:27 +0000

    Addded tests for DateTime, Color, and Guid (de)serilisation

--- Core/Tests/SerializationTests.cs
+++ Core/Tests/SerializationTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
 using NUnit.Framework;
 using Speckle.Core.Api;
 using Speckle.Core.Models;
@@ -221,5 +224,57 @@ namespace Tests
       Assert.AreEqual(isCorrect, true);
     }
 
+
+    private class DateMock : Base
+    {
+      public DateTime TestField { get; set; }
+    }
+    [Test]
+    public void DateSerilisation()
+    {
+      // TODO
+      var date = new DateTime(2020, 1, 14);
+      var mockBase = new DateMock{ TestField = date};
+
+      var result = Operations.Serialize(mockBase);
+      var test = (DateMock)Operations.Deserialize(result);
+
+      Assert.AreEqual(date, test.TestField);
+    }
+    
+    private class GUIDMock : Base
+    {
+      public Guid TestField { get; set; }
+    }
+    [Test]
+    public void GuidSerilisation()
+    {
+      // TODO
+      var guid = Guid.NewGuid();
+      var mockBase = new GUIDMock{ TestField = guid};
+
+      var result = Operations.Serialize(mockBase);
+      var test = (GUIDMock)Operations.Deserialize(result);
+
+      Assert.AreEqual(guid, test.TestField);
+    }
+    
+    private class ColorMock : Base
+    {
+      public Color TestField { get; set; }
+    }
+    [Test]
+    public void ColorSerilisation()
+    {
+      // TODO
+      var color = Color.FromArgb(255, 4, 126, 251);
+      var mockBase = new ColorMock{ TestField = color};
+
+      var result = Operations.Serialize(mockBase);
+      var test = (ColorMock)Operations.Deserialize(result);
+
+      Assert.AreEqual(color, test.TestField);
+    }
+    
   }
 }
