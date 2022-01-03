e8d63bbf351d88491889ee8cd5a00a4e5274c6db
Claire Kuang <kuang.claire@gmail.com>
2022-01-03 08:17:21 +0000

    added dui2 stream state manager

--- ConnectorMicroStation/ConnectorMicroStationOpenShared/Storage/StreamStateManager2.cs
+++ ConnectorMicroStation/ConnectorMicroStationOpenShared/Storage/StreamStateManager2.cs
@@ -1,104 +1,112 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Bentley.DgnPlatformNET.DgnEC;
-//using Bentley.MstnPlatformNET;
-//using Bentley.DgnPlatformNET;
-//using Bentley.ECObjects.Schema;
-//using Bentley.ECObjects;
-//using Bentley.ECObjects.Instance;
-//using Bentley.EC.Persistence.Query;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bentley.DgnPlatformNET.DgnEC;
+using Bentley.MstnPlatformNET;
+using Bentley.DgnPlatformNET;
+using Bentley.ECObjects.Schema;
+using Bentley.ECObjects;
+using Bentley.ECObjects.Instance;
+using Bentley.EC.Persistence.Query;
 
-//using Speckle.Newtonsoft.Json;
-//using Speckle.DesktopUI.Utils;
+using Speckle.Newtonsoft.Json;
+using DesktopUI2.Models;
 
-//using DesktopUI2.Models;
+namespace Speckle.ConnectorMicroStationOpenRoads.Storage
+{
+  public static class StreamStateManager2
+  {
+    static readonly string schemaName = "StreamStateWrapper";
+    static readonly string className = "StreamStates";
+    static readonly string propertyName = "StreamData";
 
-//namespace Speckle.ConnectorMicroStationOpenRoads.Storage
-//{
-//  public static class StreamStateManager2
-//  {
-//    static readonly string schemaName = "StreamStateWrapper";
-//    static readonly string className = "StreamState";
+    /// <summary>
+    /// Returns all the speckle stream states present in the custom schema (schema is attached to file).
+    /// </summary>
+    /// <param name="schema"></param>
+    /// <returns></returns>
+    public static List<StreamState> ReadState(ECSchema schema)
+    {
+      DgnFile File = Session.Instance.GetActiveDgnFile();
+      DgnECManager Manager = DgnECManager.Manager;
 
-//    /// <summary>
-//    /// Returns all the speckle stream states present in the custom schema (schema is attached to file).
-//    /// </summary>
-//    /// <param name="schema"></param>
-//    /// <returns></returns>
-//    public static List<DesktopUI2.Models.StreamState> ReadState(ECSchema schema)
-//    {
-//      DgnFile File = Session.Instance.GetActiveDgnFile();
-//      DgnECManager Manager = DgnECManager.Manager;
+      try
+      {
+        ECQuery readWidget = new ECQuery(schema.GetClass(className));
+        readWidget.SelectClause.SelectAllProperties = true;
 
-//      try
-//      {
-//        ECQuery readWidget = new ECQuery(schema.GetClass(className));
-//        readWidget.SelectClause.SelectAllProperties = true;
+        FindInstancesScope scope = FindInstancesScope.CreateScope(File, new FindInstancesScopeOption(DgnECHostType.All));
 
-//        FindInstancesScope scope = FindInstancesScope.CreateScope(File, new FindInstancesScopeOption(DgnECHostType.All));
+        var states = new List<StreamState>();
+        using (DgnECInstanceCollection ecInstances = Manager.FindInstances(scope, readWidget))
+        {
+          var streamStatesInstance = ecInstances.First();
+          if (streamStatesInstance != null)
+          {
+            var str = streamStatesInstance[propertyName].StringValue;
+            states = JsonConvert.DeserializeObject<List<StreamState>>(str);
+          }
+        }
 
-//        var states = new List<DesktopUI2.Models.StreamState>();
-//        using (DgnECInstanceCollection ecInstances = Manager.FindInstances(scope, readWidget))
-//        {
-//          foreach (IDgnECInstance instance in ecInstances)
-//          {
-//            var id = instance["Id"].StringValue;
-//            var streamStateData = instance["StreamData"].StringValue;
-//            var state = JsonConvert.DeserializeObject<DesktopUI2.Models.StreamState>(streamStateData);
-//            states.Add(state);
-//          }
-//        }
+        return states;
+      }
+      catch (Exception e)
+      {
+        return new List<StreamState>();
+      }
+    }
 
-//        return states;
-//      }
-//      catch (Exception e)
-//      {
-//        return new List<DesktopUI2.Models.StreamState>();
-//      }
-//    }
+    /// <summary>
+    /// Writes the stream states to the current schema.
+    /// </summary>
+    /// <param name="streamStates"></param>
+    public static void WriteStreamStateList(DgnFile File, List<StreamState> streamStates)
+    {
+      DgnECManager Manager = DgnECManager.Manager;
 
-//    /// <summary>
-//    /// Writes the stream states to the current schema.
-//    /// </summary>
-//    /// <param name="streamStates"></param>
-//    public static void WriteStreamStateList(DgnFile File, List<DesktopUI2.Models.StreamState> streamStates)
-//    {
-//      DgnECManager Manager = DgnECManager.Manager;
+      FindInstancesScope scope = FindInstancesScope.CreateScope(File, new FindInstancesScopeOption(DgnECHostType.All));
+      IECSchema schema = Manager.LocateSchemaInScope(scope, schemaName, 1, 0, SchemaMatchType.Latest);
 
-//      FindInstancesScope scope = FindInstancesScope.CreateScope(File, new FindInstancesScopeOption(DgnECHostType.All));
-//      IECSchema schema = Manager.LocateSchemaInScope(scope, schemaName, 1, 0, SchemaMatchType.Latest);
+      if (schema == null)
+      {
+        schema = CreateSchema(File);
+      }
 
-//      if (schema == null)
-//      {
-//        schema = StreamStateListSchema.GetSchema();
-//      }
+      IECClass ecClass = schema.GetClass(className);
 
-//      IECClass ecClass = schema.GetClass(className);
+      ECQuery readWidget = new ECQuery(ecClass);
+      readWidget.SelectClause.SelectAllProperties = true;
 
-//      ECQuery readWidget = new ECQuery(ecClass);
-//      readWidget.SelectClause.SelectAllProperties = true;
+      using (DgnECInstanceCollection instances = Manager.FindInstances(scope, readWidget))
+      {
+        foreach (IDgnECInstance instance in instances)
+          instance.Delete();
+      }
 
-//      using (DgnECInstanceCollection instances = Manager.FindInstances(scope, readWidget))
-//      {
-//        foreach (IDgnECInstance instance in instances)
-//          instance.Delete();
-//      }
+      DgnECInstanceEnabler instanceEnabler = Manager.ObtainInstanceEnabler(File, ecClass);
 
-//      DgnECInstanceEnabler instanceEnabler = Manager.ObtainInstanceEnabler(File, ecClass);
+      var data = JsonConvert.SerializeObject(streamStates) as string;
+      StandaloneECDInstance _instance = instanceEnabler.SharedWipInstance;
+      _instance.SetAsString(propertyName, data);
+      instanceEnabler.CreateInstanceOnFile(File, _instance);
+    }
 
-//      foreach (var streamState in streamStates)
-//      {
-//        var data = JsonConvert.SerializeObject(streamState) as string;
-//        StandaloneECDInstance instance = instanceEnabler.SharedWipInstance;
+    private static ECSchema CreateSchema(DgnFile File)
+    {
+      ECSchema newSchema = new ECSchema(schemaName, 1, 0, schemaName);
+      ECClass streamStateClass = new ECClass(className);
+      ECProperty streamDataProp = new ECProperty(propertyName, ECObjects.StringType);
+      streamStateClass.Add(streamDataProp);
+      newSchema.AddClass(streamStateClass);
 
-//        instance.SetAsString("Id", streamState.StreamId);
-//        instance.SetAsString("StreamData", data);
+      var status = DgnECManager.Manager.ImportSchema(newSchema, File, new ImportSchemaOptions());
 
-//        instanceEnabler.CreateInstanceOnFile(File, instance);
-//      }
-//    }
-//  }
-//}
+      if (status != SchemaImportStatus.Success)
+        return null;
+
+      return newSchema;
+    }
+  }
+}
