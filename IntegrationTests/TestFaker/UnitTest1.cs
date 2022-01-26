8f178bc915c506fbeed7071f5397194636577a66
Gergő Jedlicska <gergo@jedlicska.com>
2022-01-26 20:39:25 +0100

    rhino tester poc code

---
+++ IntegrationTests/TestFaker/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace TestFaker;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
--- IntegrationTests/TestRunner/Program.cs
+++ IntegrationTests/TestRunner/Program.cs
@@ -8,20 +8,26 @@ Console.WriteLine("Setting up the connection to the test server");
 //1. set up the test server with an account, set default account to test acc just in case
 //1/a. set user account
 //1/b. create new stream, share the stream with a given list of users, for dev purposes
-var settings = await ServerSettings.Initialize();
+// var settings = await ServerSettings.Initialize();
 
 
-//2. sync execution steps, we need to send all the data before we can receive
-Console.WriteLine("Configuring send operations");
-var executor = new TestExecutor(settings);
+// //2. sync execution steps, we need to send all the data before we can receive
+// Console.WriteLine("Configuring send operations");
+// var executor = new TestExecutor(settings);
 
-await executor.Setup();
-var sendResults = (await executor.Send()).ToList();
+// await executor.Setup();
+// var sendResults = (await executor.Send()).ToList();
 
-Console.WriteLine("Send operations completed");
+// Console.WriteLine("Send operations completed");
 
-Console.WriteLine("Tests finished.");
-Console.WriteLine("Tear down test environment? [Y]/[n]");
-var res = Console.ReadLine();
-if (res != "n") executor.Teardown();
+// Console.WriteLine("Tests finished.");
+// Console.WriteLine("Tear down test environment? [Y]/[n]");
+// var res = Console.ReadLine();
+// if (res != "n") executor.Teardown();
 
+var rhinoSender = new RhinoSender();
+
+var res = await rhinoSender.Send();
+
+
+Console.Write("Done");
