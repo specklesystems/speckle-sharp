4b13516760e00a416a823caad5107c348d228881
Reynold Chan <reynold.ch@gmail.com>
2022-02-16 13:01:24 +0100

    Tekla grasshopper components

--- Objects/Objects/BuiltElements/TeklaStructures/Bolts.cs
+++ Objects/Objects/BuiltElements/TeklaStructures/Bolts.cs
@@ -14,7 +14,7 @@ namespace Objects.BuiltElements.TeklaStructures
     public string boltStandard { get; set; }
     public double cutLength { get; set; }
     public List<Point> coordinates { get; set; }
-        public List<string> boltedPartsIds { get; set; } = new List<string>(); // First guid is PartToBeBolted, second guid is PartToBoltTo, any others are OtherPartsToBolt
+    public List<string> boltedPartsIds { get; set; } = new List<string>(); // First guid is PartToBeBolted, second guid is PartToBoltTo, any others are OtherPartsToBolt
     public Bolts() { }
 
   }
--- Objects/Objects/BuiltElements/TeklaStructures/TeklaContourPlate.cs
+++ Objects/Objects/BuiltElements/TeklaStructures/TeklaContourPlate.cs
@@ -8,36 +8,49 @@ using Objects.Structural.Properties.Profiles;
 
 namespace Objects.BuiltElements.TeklaStructures
 {
-    public class TeklaContourPlate : Area
+  public class TeklaContourPlate : Area
+  {
+    [DetachProperty]
+    public SectionProfile profile { get; set; }
+    [DetachProperty]
+    public Material material { get; set; }
+    public string finish { get; set; }
+    public string classNumber { get; set; }
+    public TeklaPosition position { get; set; }
+
+    [DetachProperty]
+    public Mesh displayMesh { get; set; }
+
+    [DetachProperty]
+    public Base rebars { get; set; }
+
+    public string units { get; set; }
+
+    [SchemaInfo("ContourPlate", "Creates a TeklaStructures contour plate.", "Tekla", "Structure")]
+    public TeklaContourPlate(SectionProfile profile, Polyline outline, string finish, string classNumber, string units, Material material = null ,TeklaPosition position = null , Base rebars = null)
     {
-        [DetachProperty]
-        public SectionProfile profile { get; set; }
-        [DetachProperty]
-        public Material material { get; set; }
-        public string finish { get; set; }
-        public string classNumber { get; set; }
-        public TeklaPosition position { get; set; }
-
-        [DetachProperty]
-        public Mesh displayMesh { get; set; }
-
-        [DetachProperty]
-        public Base rebars { get; set; }
-
-        public string units { get; set; }
-
-        [SchemaInfo("ContourPlate", "Creates a TeklaStructures contour plate.", "Tekla", "Structure")]
-        public TeklaContourPlate() { }
+      this.profile = profile;
+      this.outline = outline;
+      this.material = material;
+      this.finish = finish;
+      this.classNumber = classNumber;
+      this.position = position;
+      this.rebars = rebars;
+      this.units = units;
     }
-    public class TeklaContourPoint : Point
-    {
-        public TeklaChamferType chamferType { get; set; }
-        public double xDim { get; set; }
-        public double yDim { get; set; }
-        public double dz1 { get; set; }
-        public double dz2 { get; set; }
 
-        public TeklaContourPoint() { }
-        public TeklaContourPoint(Point point) { }
-    }
+  
+    public TeklaContourPlate() { }
+  }
+  public class TeklaContourPoint : Point
+  {
+    public TeklaChamferType chamferType { get; set; }
+    public double xDim { get; set; }
+    public double yDim { get; set; }
+    public double dz1 { get; set; }
+    public double dz2 { get; set; }
+
+    public TeklaContourPoint() { }
+    public TeklaContourPoint(Point point) { }
+  }
 }
