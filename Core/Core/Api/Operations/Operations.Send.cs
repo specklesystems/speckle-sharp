b812cdd2224418bd977e5c097ebcd8e0d7e93284
Gergő Jedlicska <gergo@jedlicska.com>
2022-02-17 18:35:23 +0100

    (core) rever mandatory operations send on error action

--- Core/Core/Api/Operations/Operations.Send.cs
+++ Core/Core/Api/Operations/Operations.Send.cs
@@ -27,15 +27,15 @@ namespace Speckle.Core.Api
     /// <param name="onProgressAction">Action that gets triggered on every progress tick (keeps track of all transports).</param>
     /// <param name="onErrorAction">Use this to capture and handle any errors from within the transports.</param>
     /// <returns>The id (hash) of the object.</returns>
-    public static Task<string> Send(Base @object, List<ITransport> transports = null, bool useDefaultCache = true, Action<ConcurrentDictionary<string, int>> onProgressAction = null, Action<string, Exception> onErrorAction = null, bool disposeTransports = false, SerializerVersion serializerVersion = SerializerVersion.V2)
+    public static Task<string> Send(Base @object, Action<string, Exception> onErrorAction, List<ITransport> transports = null, bool useDefaultCache = true, Action<ConcurrentDictionary<string, int>> onProgressAction = null, bool disposeTransports = false, SerializerVersion serializerVersion = SerializerVersion.V2)
     {
       return Send(
         @object,
         CancellationToken.None,
+        onErrorAction,
         transports,
         useDefaultCache,
         onProgressAction,
-        onErrorAction,
         disposeTransports,
         serializerVersion
       );
@@ -51,7 +51,8 @@ namespace Speckle.Core.Api
     /// <param name="onProgressAction">Action that gets triggered on every progress tick (keeps track of all transports).</param>
     /// <param name="onErrorAction">Use this to capture and handle any errors from within the transports.</param>
     /// <returns>The id (hash) of the object.</returns>
-    public static async Task<string> Send(Base @object, CancellationToken cancellationToken, Action<string, Exception> onErrorActionusing System.Net.Http;, List<ITransport> transports = null, bool useDefaultCache = true, Action<ConcurrentDictionary<string, int>> onProgressAction = null, bool disposeTransports = false, SerializerVersion serializerVersion = SerializerVersion.V2)
+
+    public static async Task<string> Send(Base @object, CancellationToken cancellationToken, List<ITransport> transports = null, bool useDefaultCache = true, Action<ConcurrentDictionary<string, int>> onProgressAction = null, Action<string, Exception> onErrorAction = null, bool disposeTransports = false, SerializerVersion serializerVersion = SerializerVersion.V2)
     {
       Log.AddBreadcrumb("Send");
 
