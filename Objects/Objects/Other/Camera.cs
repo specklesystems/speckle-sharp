96f151d841da8aff920345495dcea678b2709d0c
izzy lyseggen <izzy.lyseggen@gmail.com>
2022-03-08 18:08:15 +0000

    feat(objects): 🎥 camera and views wip

---
+++ Objects/Objects/Other/Camera.cs
@@ -0,0 +1,18 @@
+﻿namespace Objects.Other
+{
+  public class Camera : View
+  {
+    public SensorFit sensorFit { get; set; } = SensorFit.Auto;
+    public double sensorHeight { get; set; } = 24;
+    public double sensorWidth { get; set; } = 36;
+
+    // (mayb for l8r) shift X and shift Y
+  }
+
+  public enum SensorFit
+  {
+    Auto,
+    Horizontal,
+    Vertical
+  }
+}
---
+++ Objects/Objects/Other/View.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+
+using Objects.Geometry;
+using Speckle.Core.Kits;
+using Speckle.Core.Models;
+using Speckle.Newtonsoft.Json;
+
+namespace Objects.Other
+{
+  public class View : Base
+  {
+    // TODO: do we want sensible defaults for everything?
+    public string name { get; set; }
+    public string units { get; set; } = Units.Millimeters;
+    public ViewType viewType { get; set; } = ViewType.Perspective;
+    public Transform transform { get; set; } = new Transform();
+    public double fov { get; set; }
+    public double? fovX { get; set; }
+    public double? fovY { get; set; }
+    public double? orthoScale { get; set; }
+    public double aspectX { get; set; }
+    public double aspectY { get; set; }
+
+    // TODO: convenience methods
+    // lens (computed)
+    // target point
+    // forward direction
+    // up direction
+    [JsonIgnore] public Point cameraPosition => transform.ApplyToPoint(new Point(0, 0, 0, units));
+
+    public View() { }
+  }
+
+  public enum ViewType
+  {
+    Perspective,
+    Orthogonal,
+    Panoramic
+  }
+}
