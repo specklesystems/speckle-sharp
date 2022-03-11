5fa8d644c1cff56dd9236b1147065443583fd0ba
Alan Rynne <alan@speckle.systems>
2022-03-11 14:23:59 +0100

    A bit more fleshed up!

--- IntegrationTests/TestRunner/Refactored.cs
+++ IntegrationTests/TestRunner/Refactored.cs
@@ -1,6 +1,8 @@
 
+using Sentry.Protocol;
 using Speckle.Core.Api;
 using Speckle.Core.Credentials;
+using Speckle.Core.Kits;
 
 [Flags]
 public enum SpeckleTransportOperations 
@@ -45,6 +47,12 @@ public class TestServerInfo
 [Serializable]
 public class SourceFileDescriptor
 {
+    public SourceFileDescriptor(string filePath, string application)
+    {
+        FilePath = filePath;
+        Application = application;
+    }
+
     public string FilePath { get; }
     public string Application { get; set; }
 }
@@ -56,7 +64,32 @@ public class AutotestConfig
 
     public static async Task<AutotestConfig> CreateAutotestConfig(Config config)
     {
-        //TODO create registry from config
+        var temp = new Config();
+        temp.ValidSendReceivePairs = new List<(string sender, string receiver)>
+        {
+            (Applications.Rhino7, Applications.Grasshopper),
+            (Applications.Revit2021, Applications.Rhino7)
+        };
+        
+        temp.PipelineDescriptors = new List<List<string>>
+        {
+            new() { "Rhino", "Revit", "Grasshopper" },
+            new() { "Revit", "Grasshopper", "Revit" },
+        };
+        temp.FileDescriptors = new List<SourceFileDescriptor>
+        {
+            new("fakepath.3dm", Applications.Rhino7),
+            new("fakepath.rvt", Applications.Revit2022),
+            new("fakepath.gh", Applications.Grasshopper)
+        };
+
+        var autoTestConfig = new AutotestConfig();
+        
+        
+        
+        
+
+        return autoTestConfig;
     } 
 }
 
@@ -73,10 +106,34 @@ public class Config
 
 public interface ISourceFileRegistry
 {
-    public Dictionary<string, IEnumerable<SourceFileDescriptor>> SourceFileDescriptors { get; }
+    public Dictionary<string, List<SourceFileDescriptor>> SourceFileDescriptors { get; }
 }
 
+public class SourceFileRegistry : ISourceFileRegistry
+{
+    public Dictionary<string, List<SourceFileDescriptor>> SourceFileDescriptors { get; set; }
 
+    public static Task<SourceFileRegistry> CreateFileRegistry(List<SourceFileDescriptor> fileDescriptors)
+    {
+        var registry = new SourceFileRegistry();
+        var sourceFileDescriptors = new Dictionary<string, List<SourceFileDescriptor>>();
+        
+        fileDescriptors.ForEach(descriptor =>
+        {
+            // Validation may be needed on the descriptor instead.
+            if (!File.Exists(descriptor.FilePath))
+                return;
+            
+            
+            if (!sourceFileDescriptors.ContainsKey(descriptor.Application))
+                sourceFileDescriptors[descriptor.Application] = new List<SourceFileDescriptor>();
+            sourceFileDescriptors[descriptor.Application].Add(descriptor);
+        });
+        registry.SourceFileDescriptors = sourceFileDescriptors;
+
+        return Task.Run(() => registry);
+    }
+}
 
 public class TestExecutor
 {
