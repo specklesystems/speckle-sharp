2013a3c15c91cbc13d94105b77f06e380bee4bab
Matteo Cominetti <matteo@cominetti.org>
2022-03-14 20:21:10 +0000

    feat(dui): fiddles and fixes

---
+++ DesktopUI2/DesktopUI2/Views/Converters/StringOpacityValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia.Data.Converters;
+using System;
+
+namespace DesktopUI2.Views.Converters
+{
+
+  public class StringOpacityValueConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      if (value == null || String.IsNullOrEmpty(value.ToString()))
+        return 0;
+      return 1;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      throw new NotSupportedException();
+    }
+  }
+}
+
