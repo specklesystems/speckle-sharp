af53b07aa7120b498354e87aeca00115301a246c
Reynold Chan <reynold.ch@gmail.com>
2022-03-30 01:31:35 -0400

    Squashed commit of the following: Feat Tekla structures missing parameters + non dependent plugin
    commit f69f54c94783e24609a977e3bb0473e09ca1fe65
    Author: Reynold Chan <reynold.ch@gmail.com>
    Date:   Wed Mar 30 01:27:39 2022 -0400
    
        Missing paramters for Tekla Structures
    
    commit 524f974117dcfadeed20cfc701089011ae147df4
    Author: eestrado <92844367+eestrado@users.noreply.github.com>
    Date:   Mon Mar 21 14:47:51 2022 +0100
    
        Change to non dependent plugin (#1102)
    
    commit 43ff1b34d627410346be5e57a3b4848b5b4bb845
    Author: Reynold Chan <reynold.ch@gmail.com>
    Date:   Mon Mar 7 17:08:05 2022 -0500
    
        Tekla Beam to Revit

--- ConnectorTeklaStructures/ConnectorTeklaStructures2020/MainForm.Designer.cs
+++ ConnectorTeklaStructures/ConnectorTeklaStructures2020/MainForm.Designer.cs
@@ -29,10 +29,21 @@ namespace Speckle.ConnectorTeklaStructures
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "MainForm";
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.structuresExtender.SetAttributeName(this, null);
+            this.structuresExtender.SetAttributeTypeName(this, null);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.structuresExtender.SetBindPropertyName(this, null);
+            this.ClientSize = new System.Drawing.Size(480, 197);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+
     }
 
     #endregion
