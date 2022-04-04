cc0630dbb0740a9185ef5037570b72c6558caa60
Matteo Cominetti <matteo@cominetti.org>
2022-04-04 12:41:32 +0100

    feat(revit): moves addin to its own tab, updates icons

--- ConnectorRevit/ConnectorRevit/UI/ConnectorBindingsRevit2/ConnectorBindingsRevit2.Events.cs
+++ ConnectorRevit/ConnectorRevit/UI/ConnectorBindingsRevit2/ConnectorBindingsRevit2.Events.cs
@@ -110,33 +110,47 @@ namespace Speckle.ConnectorRevit.UI
     //checks whether to refresh the stream list in case the user changes active view and selects a different document
     private void RevitApp_ViewActivated(object sender, Autodesk.Revit.UI.Events.ViewActivatedEventArgs e)
     {
+      try
+      {
 
-      if (e.Document == null || e.Document.IsFamilyDocument || e.PreviousActiveView == null || GetDocHash(e.Document) == GetDocHash(e.PreviousActiveView.Document))
-        return;
+        if (e.Document == null || e.Document.IsFamilyDocument || e.PreviousActiveView == null || GetDocHash(e.Document) == GetDocHash(e.PreviousActiveView.Document))
+          return;
 
-      var streams = GetStreamsInFile();
-      UpdateSavedStreams(streams);
+        var streams = GetStreamsInFile();
+        UpdateSavedStreams(streams);
 
-      MainWindowViewModel.GoHome();
+        MainWindowViewModel.GoHome();
+      }
+      catch (Exception ex)
+      {
+
+      }
 
     }
 
     private void Application_DocumentClosed(object sender, Autodesk.Revit.DB.Events.DocumentClosedEventArgs e)
     {
-      // the DocumentClosed event is triggered AFTER ViewActivated
-      // is both doc A and B are open and B is closed, this would result in wiping the list of streams retrieved for A
-      // only proceed if it's the last document open (the current is null)
-      if (CurrentDoc != null)
-        return;
+      try
+      {
+        // the DocumentClosed event is triggered AFTER ViewActivated
+        // is both doc A and B are open and B is closed, this would result in wiping the list of streams retrieved for A
+        // only proceed if it's the last document open (the current is null)
+        if (CurrentDoc != null)
+          return;
 
-      if (SpeckleRevitCommand2.MainWindow != null)
-        SpeckleRevitCommand2.MainWindow.Hide();
+        if (SpeckleRevitCommand2.MainWindow != null)
+          SpeckleRevitCommand2.MainWindow.Hide();
 
-      //clear saved streams if closig a doc
-      if (UpdateSavedStreams != null)
-        UpdateSavedStreams(new List<StreamState>());
+        //clear saved streams if closig a doc
+        if (UpdateSavedStreams != null)
+          UpdateSavedStreams(new List<StreamState>());
 
-      MainWindowViewModel.GoHome();
+        MainWindowViewModel.GoHome();
+      }
+      catch (Exception ex)
+      {
+
+      }
     }
 
     // this method is triggered when there are changes in the active document
