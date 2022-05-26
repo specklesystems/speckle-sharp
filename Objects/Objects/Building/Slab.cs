deeae71fac873eb8c48628c4ce1d6ed48792bb43
izzy lyseggen <izzy.lyseggen@gmail.com>
2022-05-26 18:18:03 +0100

    feat(objects): wip slab (?) class

---
+++ Objects/Objects/Building/Slab.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Objects.Geometry;
+using Speckle.Core.Models;
+
+namespace Objects.Building
+{
+  // TODO: think of better name for this
+  public class Slab : OutlineBasedElement
+  {
+    public SlabType slabType { get; set; } = SlabType.Slab;
+
+    [DetachProperty] public List<ICurve> voids { get; set; } = new List<ICurve>();
+
+    public double slope { get; set; }
+  }
+
+  public enum SlabType
+  {
+    Floor,
+    Ceiling,
+    Slab
+  }
+}
