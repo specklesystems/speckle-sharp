0694ccc5e3fc8bb8c4358e43fc0750b9acfc3eaf
izzy lyseggen <izzy.lyseggen@gmail.com>
2022-05-30 17:02:45 +0100

    wip(objects): scaffold model organisation

---
+++ Objects/Objects/Organization/Model.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Speckle.Core.Models;
+
+namespace Objects.Organization
+{
+  /// <summary>
+  /// A basic container object representing a 3D model.
+  /// This could be used as a root commit object 
+  /// </summary>
+  public class Model : Base
+  {
+    /// <summary>
+    /// [Optional] A <see cref="ModelInfo"/> object containing metadata about this <see cref="Model"/>
+    /// </summary>
+    public ModelInfo info { get; set; }
+    
+    /// <summary>
+    /// [Optional] A <see cref="ModelSettings"/> object containing any specific settings for sending / receiving this data.
+    /// </summary>
+    public ModelSettings settings { get; set; }
+    
+    // TODO: do we want a defined field here for the objects or should you be able to add as many "layers" as you want
+    // like we currently do? Leaning towards the latter, but then we also prob want a "Layer" / "Container" object
+  }
+
+  public class ModelInfo : Base
+  {
+    public string name { get; set; }
+    public string description { get; set; }
+  }
+
+  public class ModelSettings : Base
+  {
+    
+  }
+}
