2fa082f4f6b81ae29845bb13b71124d68959a003
izzy lyseggen <izzy.lyseggen@gmail.com>
2022-06-27 11:44:00 +0100

    fix(objects): cleanup on the model objs

--- Objects/Objects/Organization/Model.cs
+++ Objects/Objects/Organization/Model.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Objects.Other;
 using Speckle.Core.Models;
 
 namespace Objects.Organization
@@ -7,17 +8,26 @@ namespace Objects.Organization
   /// Represents a model from an authoring application and can be used as the root commit object when sending.
   /// It contains <see cref="ModelInfo"/> and <see cref="Setting"/> objects 
   /// </summary>
-  public class Model: Base
+  public class Model : Base
   {
     /// <summary>
     /// General model-wide information stored in a <see cref="ModelInfo"/> object.
     /// This may include anything from simply a project / file name to specific location information (eg with <see cref="BIMModelInfo"/>)
     /// </summary>
     public ModelInfo info { get; set; }
-    
-    
+
+
     public List<Setting> settings { get; set; }
 
+    public Model()
+    {
+    }
+
+    public Model(ModelInfo info, List<Setting> settings = null)
+    {
+      this.info = info;
+      this.settings = settings;
+    }
   }
 
   /// <summary>
@@ -37,50 +47,63 @@ namespace Objects.Organization
     /// </summary>
     public string number { get; set; }
 
+    public ModelInfo()
+    {
+    }
+
     //  TODO: not sure about adding a typed `elements` list here? prob should let ppl add whatever named categories here?
   }
-  
+
   //  TODO: not quite sure about this name?
   /// <summary>
   /// Extended <see cref="ModelInfo"/> to be attached to the <see cref="Model.info"/> field on a <see cref="Model"/> object.
   /// This contains additional properties applicable to AEC projects.
   /// </summary>
-  public class BIMModelInfo : Base
+  public class BIMModelInfo : ModelInfo
   {
     /// <summary>
     /// The name of the client
     /// </summary>
     public string clientName { get; set; }
-    
+
     /// <summary>
     /// The name of the building
     /// </summary>
     public string buildingName { get; set; }
-    
+
     /// <summary>
     /// The status or phase of the model.
     /// </summary>
     public string status { get; set; }
-    
+
     /// <summary>
     /// The address of the model.
     /// </summary>
     public string address { get; set; }
-    
+
     /// <summary>
-    /// The angle to true North in decimal degrees.
+    /// The name of the site location as a string.
     /// </summary>
-    public double trueNorth { get; set; }
-    
+    public string siteName { get; set; }
+ 
     /// <summary>
-    /// The latitude of the model location in decimal degrees.
+    /// The latitude of the site location in radians.
     /// </summary>
     public double latitude { get; set; }
-    
+
     /// <summary>
-    /// The longitude of the model location in decimal degrees.
+    /// The longitude of the site location in radians.
     /// </summary>
     public double longitude { get; set; }
+
+    /// <summary>
+    /// A list of origin locations within this model as a list of <see cref="Transform"/>s
+    /// </summary>
+    public List<Base> locations { get; set; }
+
+    public BIMModelInfo()
+    {
+    }
   }
   
   public class Setting  : Base
