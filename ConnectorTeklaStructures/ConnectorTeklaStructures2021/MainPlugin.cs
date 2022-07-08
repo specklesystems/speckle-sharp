f154448b44582caa11d21f5f2ffda12ce4aecacc
Reynold Chan <reynold.ch@gmail.com>
2022-07-08 11:21:58 -0400

    fix CI build failure

--- ConnectorTeklaStructures/ConnectorTeklaStructures2021/MainPlugin.cs
+++ ConnectorTeklaStructures/ConnectorTeklaStructures2021/MainPlugin.cs
@@ -1,7 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSDatatype = Tekla.Structures.Datatype;
+using Tekla.Structures.Model;
 using Tekla.Structures.Plugins;
+using Tekla.Structures;
+using Tekla.Structures.Geometry3d;
+using Tekla.Structures.Model.UI;
+using System.Reflection;
 
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+
+using DesktopUI2.ViewModels;
+using DesktopUI2.Views;
+using System.Threading.Tasks;
+using System.IO;
+using Assembly = System.Reflection.Assembly;
+using Speckle.ConnectorTeklaStructures.UI;
 namespace Speckle.ConnectorTeklaStructures
 {
   public class StructuresData
@@ -9,7 +28,7 @@ namespace Speckle.ConnectorTeklaStructures
   }
   [Plugin("Speckle.ConnectorTeklaStructures")]
   [PluginUserInterface("Speckle.ConnectorTeklaStructures.MainForm")]
-    [InputObjectDependency(InputObjectDependency.NOT_DEPENDENT)]
+  [InputObjectDependency(InputObjectDependency.NOT_DEPENDENT)]
 
   public class MainPlugin : PluginBase
   {
@@ -49,27 +68,24 @@ namespace Speckle.ConnectorTeklaStructures
     // Enable inserting of objects in a model
     private readonly Model model;
 
-
-    public Model Model
+    public override List<InputDefinition> DefineInput()
     {
-        public override List<InputDefinition> DefineInput()
-        {
-            return new List<InputDefinition>();
-            // Define input objects.     
-        }
-
-        // Enable retrieving of input values
-        private readonly StructuresData _data;
-
-        public MainPlugin(StructuresData data)
-        {
-
-        }
-
-        // This method is called upon execution of the plug-in and it´s the main method of the plug-in
-        public override bool Run(List<InputDefinition> input)
-        {
-            return true;
-        }
+      return new List<InputDefinition>();
+      // Define input objects.     
     }
+
+    // Enable retrieving of input values
+    private readonly StructuresData _data;
+
+    public MainPlugin(StructuresData data)
+    {
+
+    }
+
+    // This method is called upon execution of the plug-in and it´s the main method of the plug-in
+    public override bool Run(List<InputDefinition> input)
+    {
+      return true;
+    }
+  }
 }
