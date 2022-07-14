b38cb921d63459664a33615cbfce6d8479abe59a
Alan Rynne <alan@speckle.systems>
2022-07-14 21:35:14 +0200

    feat(objects): Made Arc Transformable

--- Objects/Objects/Geometry/Arc.cs
+++ Objects/Objects/Geometry/Arc.cs
@@ -4,18 +4,19 @@ using Speckle.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Objects.Other;
 using Speckle.Core.Logging;
 
 namespace Objects.Geometry
 {
-  public class Arc : Base, IHasBoundingBox, ICurve, IHasArea
+  public class Arc : Base, IHasBoundingBox, ICurve, IHasArea, ITransformable<Arc>
   {
     public double? radius { get; set; }
     public double? startAngle { get; set; }
 
     public double? endAngle { get; set; }
 
-    public double? angleRadians { get; set; }
+    public double angleRadians { get; set; }
 
     /// <summary>
     /// Gets or sets the plane of the <see cref="Arc"/>. The plane origin is the <see cref="Arc"/> center.
@@ -117,14 +118,62 @@ namespace Objects.Geometry
         startAngle = Math.PI - startAngle;
       endAngle = startAngle + angleRadians;
     }
+    public Arc(Plane plane, Point startPoint, Point endPoint, double angleRadians, string units = Units.Meters,
+      string applicationId = null)
+    {
+      // don't be annoying
+      if ( angleRadians > Math.PI * 2 )
+        throw new SpeckleException("Can't create an arc with an angle greater than 2pi");
+      if (startPoint == endPoint)
+        throw new SpeckleException("Can't create an arc where the start and end points are the same");
 
+      this.units = units;
+      this.startPoint = startPoint;
+      this.endPoint = endPoint;
+      this.angleRadians = angleRadians;
+      this.applicationId = applicationId;
+
+      // find chord and chord angle which may differ from the arc angle
+      var chordMidpoint = Point.Midpoint(startPoint, endPoint);
+      var chordLength = Point.Distance(startPoint, endPoint);
+      var chordAngle = angleRadians;
+      if ( chordAngle > Math.PI )
+        chordAngle -= Math.PI * 2;
+      else if ( chordAngle < -Math.PI )
+        chordAngle += Math.PI * 2;
+      // use the law of cosines for an isosceles triangle to get the radius
+      radius = chordLength / Math.Sqrt(2 - 2 * Math.Cos(chordAngle));
+
+      // find the chord vector then calculate the perpendicular vector which points to the centre
+      // which can be used to find the circle centre point
+      var dir = chordAngle < 0 ? -1 : 1;
+      var centreToChord = Math.Sqrt(Math.Pow(( double )radius, 2) - Math.Pow(chordLength * 0.5, 2));
+      var perp = Vector.CrossProduct(new Vector(endPoint - startPoint), plane.normal);
+      var circleCentre = chordMidpoint + new Point(perp.Unit() * centreToChord * -dir);
+      plane.origin = circleCentre;
+
+      // use the perpendicular vector in the other direction (from the centre to the arc) to find the arc midpoint
+      midPoint = angleRadians > Math.PI
+        ? chordMidpoint + new Point(perp.Unit() * ( ( double )radius + centreToChord ) * -dir)
+        : chordMidpoint + new Point(perp.Unit() * ( ( double )radius - centreToChord ) * dir);
+
+      // find the start angle using trig (correcting for quadrant position) and add the arc angle to get the end angle
+      startAngle = Math.Tan(( startPoint.y - circleCentre.y ) / ( startPoint.x - circleCentre.x )) % ( 2 * Math.PI );
+      if ( startPoint.x > circleCentre.x && startPoint.y < circleCentre.y )       // Q4
+        startAngle *= -1;
+      else if ( startPoint.x < circleCentre.x && startPoint.y < circleCentre.y )  // Q3
+        startAngle += Math.PI;
+      else if ( startPoint.x < circleCentre.x && startPoint.y > circleCentre.y )  // Q2
+        startAngle = Math.PI - startAngle;
+      endAngle = startAngle + angleRadians;
+    }
     public List<double> ToList()
     {
       var list = new List<double>();
       list.Add(radius ?? 0);
       list.Add(startAngle ?? 0);
       list.Add(endAngle ?? 0);
-      list.Add(angleRadians ?? 0);
+      list.Add(angleRadians);
       list.Add(domain.start ?? 0);
       list.Add(domain.end ?? 0);
 
@@ -155,6 +204,21 @@ namespace Objects.Geometry
       arc.plane.units = arc.units;
 
       return arc;
+    }
+
+    public bool TransformTo(Transform transform, out Arc transformed)
+    {
+      plane.TransformTo(transform, out Plane pln);
+      transformed = new Arc(pln,transform.ApplyToPoint(startPoint),transform.ApplyToPoint(endPoint), angleRadians, units);
+      return true;
+    }
+
+    public bool TransformTo(Transform transform, out ITransformable transformed)
+    {
+      var res = TransformTo(transform, out Arc arc);
+      arc.domain = domain;
+      transformed = arc;
+      return res;
     }
   }
 }
