4a93d094ced9c5d312b2da1baa4bf244af4a61d1
Claire Kuang <kuang.claire@gmail.com>
2022-08-02 10:52:46 +0100

    Added new network classes

---
+++ Objects/Objects/Organization/Network.cs
@@ -0,0 +1,35 @@
+﻿using Objects.Geometry;
+using Speckle.Core.Kits;
+using Speckle.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Speckle.Newtonsoft.Json;
+
+namespace Objects.Organization
+{
+  /// <summary>
+  /// Represents graph connections between objects
+  /// </summary>
+  /// <remarks>
+  /// Network <see cref="elements"/> may need to be created first in native applications before they are linked.
+  /// </remarks>
+  public class Network : Base
+  {
+    public string name { get; set; }
+
+    /// <summary>
+    /// The elements contained
+    /// </summary>
+    public List<Base> elements { get; set; }
+
+    /// <summary>
+    /// The connections between <see cref="elements"/>
+    /// </summary>
+    public List<NetworkLink> links { get; set; }
+
+    public Network() { }
+  }
+}
---
+++ Objects/Objects/Organization/NetworkLink.cs
@@ -0,0 +1,69 @@
+﻿using Objects.Geometry;
+using Speckle.Core.Kits;
+using Speckle.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Speckle.Newtonsoft.Json;
+
+namespace Objects.Organization
+{
+  public class NetworkLink : Base
+  {
+    public string name { get; set; }
+
+    /// <summary>
+    /// The index of the network elements that this link connects
+    /// </summary>
+    public List<int> connections { get; set; }
+
+    public NetworkLink() { }
+  }
+}
+namespace Objects.Organization.Revit
+{
+  public class RevitNetworkLink : Base
+  {
+    /// <summary>
+    /// The shape of the <see cref="NetworkLink"/> at each <see cref="NetworkLink.connections"/>
+    /// </summary>
+    public List<NetworkLinkShape> shapes { get; set; } // should this just be one shape or can a link have 2?
+
+    public Vector direction { get; set; }
+
+    public bool connected { get; set; }
+
+    public double height { get; set; }
+
+    public double width { get; set; }
+
+    public double radius { get; set; }
+
+    /// <summary>
+    /// The system type
+    /// </summary>
+    public string type { get; set; }
+
+    /// <summary>
+    /// The system category
+    /// </summary>
+    public string category { get; set; }
+  }
+
+  /// <summary>
+  /// Represents the shape of a <see cref="NetworkLink"/>.
+  /// </summary>
+  public enum NetworkLinkShape
+  {
+    Unknown,
+    Rectangular,
+    Round,
+    Square
+  }
+}
+  
+
+  
+
