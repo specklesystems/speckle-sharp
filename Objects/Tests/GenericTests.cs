0c98675f03a9fce2a575f6f558c34d1670bbf4b7
Alan Rynne <alan@speckle.systems>
2022-09-20 19:38:13 +0200

    feat(objects): Added empty constructor test

---
+++ Objects/Tests/GenericTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Speckle.Core.Kits;
+
+namespace Tests
+{
+  [TestFixture]
+  public class GenericTests
+  {
+    public static IEnumerable AvailableTypesInKit()
+    {
+      return KitManager.GetDefaultKit().Types;
+    }
+    
+    [Test(Description = "Checks that all objects inside the Default Kit have empty constructors.")]
+    [TestCaseSource(nameof(AvailableTypesInKit))]
+    public void ObjectHasEmptyConstructor(Type t)
+    {
+      var constructor = t.GetConstructor(Type.EmptyTypes);
+      Assert.That(constructor, Is.Not.Null);
+    }
+  }
+}
