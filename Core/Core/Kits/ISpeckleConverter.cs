1923930b7db018803d34eefee956523059b7ad69
Matteo Cominetti <matteo@speckle.systems>
2022-10-24 13:47:26 +0100

    Dui/no comments placeholder (#1767)
    * feat: adds placeholder text when there are no omments
    
    * docs(dui): explains update methods
    
    * feat(dui): more logging

--- Core/Core/Kits/ISpeckleConverter.cs
+++ Core/Core/Kits/ISpeckleConverter.cs
@@ -95,10 +95,23 @@ namespace Speckle.Core.Kits
 
   }
 
+  // NOTE: Do not change the order of the existing ones
+  /// <summary>
+  /// Receive modes indicate what to do and not do when receiving objects
+  /// </summary>
   public enum ReceiveMode
   {
+    /// <summary>
+    /// Attemts updating previously received objects by ID, deletes previously received objects that do not exist anymore and creates new ones
+    /// </summary>
     Update,
+    /// <summary>
+    /// Always creates new objects
+    /// </summary>
     Create,
+    /// <summary>
+    /// Ignores updating previously received objects and does not attempt updating or deleting them, creates new objects
+    /// </summary>
     Ignore
   }
 }
