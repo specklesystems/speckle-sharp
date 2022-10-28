45d8d55d1d3c3d8195baf5b4d5636f64caacae04
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-10-28 17:37:42 -0400

    Added Bundle Reference Args to Core

--- Core/Core/Models/Extras.cs
+++ Core/Core/Models/Extras.cs
@@ -209,6 +209,11 @@ namespace Speckle.Core.Models
     /// </summary>
     public List<string> ConversionLog { get; } = new List<string>();
 
+   /// <summary>
+    /// List of bundles to pull down
+    /// </summary>
+    public List<BundleReferenceArgs> BundleReferenceArgs { get; set; } = new List<BundleReferenceArgs>();
+   
     private readonly object ConversionLogLock = new object();
     public string ConversionLogString
     {
@@ -323,5 +328,46 @@ namespace Speckle.Core.Models
           }
       }
     }
+  }
+   /// <summary>
+  /// A simple object for keeping track of the information in a bundle
+  /// </summary>
+  public class BundleItem : Base
+  {
+    /// <summary>
+    /// stream id to reference
+    /// </summary>
+    public string streamId { get; set; }
+
+    /// <summary>
+    /// List of objects to pull from stream
+    /// </summary>
+    public List<string> objectIds { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public BundleItem()
+    {
+    }
+
+    /// <summary>
+    /// Simple way of building item
+    /// </summary>
+    /// <param name="streamId"></param>
+    /// <param name="objectIds"></param>
+    public BundleItem(string streamId, List<string> objectIds)
+    {
+      this.streamId = streamId;
+      this.objectIds = objectIds;
+    }
+
+  public class BundleReferenceArgs : EventArgs
+  {
+    public List<BundleItem> items { get; set; }
+
+    public string name { get; set; }
+
+    public string id { get; set; }
   }
 }
