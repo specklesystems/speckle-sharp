b0df8d7ff87299d2f93a0a485d4637cd234d81a2
Baris Basa <46998262+barocuda@users.noreply.github.com>
2022-11-01 15:15:10 +0100

    Feature(Revit): Support sending fabrication parts from Revit (#1671)
    feat(revit): #1100 Support sending fabrication parts from Revit

--- Objects/Converters/ConverterRevit/ConverterRevitShared/ConverterRevit.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/ConverterRevit.cs
@@ -124,6 +124,9 @@ namespace Objects.Converter.Revit
         case DB.Floor o:
           returnObject = FloorToSpeckle(o, out notes);
           break;
+        case DB.FabricationPart o:
+          returnObject = FabricationPartToSpeckle(o, out notes);
+          break;
         case DB.Level o:
           returnObject = LevelToSpeckle(o);
           break;
