8fa0b89d3a47efcb6fcd990198748ce9411c2299
Connor Ivy <connor@speckle.systems>
2022-11-01 13:45:45 -0500

    Remove duplicate code

--- Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertFabricationPart.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertFabricationPart.cs
@@ -14,26 +14,7 @@ namespace Objects.Converter.Revit
     /// <returns>RevitElement which represents converted fabrication part as speckle object</returns>
     public RevitElement FabricationPartToSpeckle(FabricationPart revitElement, out List<string> notes)
     {
-      notes = new List<string>();
-
-      RevitElement speckleElement = new RevitElement();
-
-      speckleElement.type = revitElement.Name;
-
-      speckleElement.category = revitElement.Category.Name;
-
-      speckleElement.displayValue = GetFabricationMeshes(revitElement);
-
-      //Only send elements that have a mesh, if not we should probably support them properly via direct conversions
-      if (speckleElement.displayValue == null || speckleElement.displayValue.Count == 0)
-      {
-        notes.Add("Not sending elements without display meshes");
-        return null;
-      }
-
-      GetAllRevitParamsAndIds(speckleElement, revitElement);
-
-      return speckleElement;
+      return RevitElementToSpeckle(revitElement, out notes);
     }
   }
 }
