737cefaa9286f771655b9706a216fe9b6008a25e
Connor Ivy <connor@speckle.systems>
2022-11-01 12:03:38 -0500

    fix some tabs to spaces

--- Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertFabricationPart.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertFabricationPart.cs
@@ -4,36 +4,36 @@ using System.Collections.Generic;
 
 namespace Objects.Converter.Revit
 {
-    public partial class ConverterRevit
+  public partial class ConverterRevit
+  {
+    /// <summary>
+    /// Convert fabrication parts to speckle elements
+    /// </summary>
+    /// <param name="revitElement">Fabrication part in Revit</param>
+    /// <param name="notes">Conversion notes for the exceptional cases</param>
+    /// <returns>RevitElement which represents converted fabrication part as speckle object</returns>
+    public RevitElement FabricationPartToSpeckle(FabricationPart revitElement, out List<string> notes)
     {
-        /// <summary>
-        /// Convert fabrication parts to speckle elements
-        /// </summary>
-        /// <param name="revitElement">Fabrication part in Revit</param>
-        /// <param name="notes">Conversion notes for the exceptional cases</param>
-        /// <returns>RevitElement which represents converted fabrication part as speckle object</returns>
-        public RevitElement FabricationPartToSpeckle(FabricationPart revitElement, out List<string> notes)
-        {
-            notes = new List<string>();
+      notes = new List<string>();
 
-            RevitElement speckleElement = new RevitElement();
+      RevitElement speckleElement = new RevitElement();
 
-            speckleElement.type = revitElement.Name;
+      speckleElement.type = revitElement.Name;
 
-            speckleElement.category = revitElement.Category.Name;
+      speckleElement.category = revitElement.Category.Name;
 
-            speckleElement.displayValue = GetFabricationMeshes(revitElement);
+      speckleElement.displayValue = GetFabricationMeshes(revitElement);
 
-            //Only send elements that have a mesh, if not we should probably support them properly via direct conversions
-            if (speckleElement.displayValue == null || speckleElement.displayValue.Count == 0)
-            {
-                notes.Add("Not sending elements without display meshes");
-                return null;
-            }
+      //Only send elements that have a mesh, if not we should probably support them properly via direct conversions
+      if (speckleElement.displayValue == null || speckleElement.displayValue.Count == 0)
+      {
+        notes.Add("Not sending elements without display meshes");
+        return null;
+      }
 
-            GetAllRevitParamsAndIds(speckleElement, revitElement);
+      GetAllRevitParamsAndIds(speckleElement, revitElement);
 
-            return speckleElement;
-        }
+      return speckleElement;
     }
+  }
 }
