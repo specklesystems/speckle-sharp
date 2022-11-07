6420976f15fe6992532b2edf8b795c2e0531b204
Connor Ivy <connor@speckle.systems>
2022-11-07 15:20:24 -0600

    directContext3DServer class

---
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/DirectContext3DServer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.DirectContext3D;
+using Autodesk.Revit.DB.ExternalService;
+
+namespace ConverterRevitShared
+{
+  internal class DirectContext3DServer : IDirectContext3DServer
+  {
+    public bool CanExecute(View dBView)
+    {
+      throw new NotImplementedException();
+    }
+
+    public string GetApplicationId()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Outline GetBoundingBox(View dBView)
+    {
+      throw new NotImplementedException();
+    }
+
+    public string GetDescription()
+    {
+      throw new NotImplementedException();
+    }
+
+    public string GetName()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Guid GetServerId()
+    {
+      throw new NotImplementedException();
+    }
+
+    public ExternalServiceId GetServiceId() => ExternalServices.BuiltInExternalServices.DirectContext3DService;
+
+    public string GetSourceId()
+    {
+      throw new NotImplementedException();
+    }
+
+    public string GetVendorId() => "Speckle";
+
+    public void RenderScene(View dBView, DisplayStyle displayStyle)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool UseInTransparentPass(View dBView)
+    {
+      throw new NotImplementedException();
+    }
+
+    public bool UsesHandles()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
