2bd3574d49ccd2c6b903712b0a5b3495eb699b69
Alan Rynne <alan@speckle.systems>
2022-11-10 23:46:28 +0100

    feat(core): Added tests to ensure all obsolete methods can be replicated with new GetMembers

--- Core/Core/Models/DynamicBase.cs
+++ Core/Core/Models/DynamicBase.cs
@@ -153,7 +153,7 @@ namespace Speckle.Core.Models
     /// Gets all of the property names on this class, dynamic or not.
     /// </summary>
     /// <returns></returns>
-    [Obsolete("Use GetMembers().Keys instead")]
+    [Obsolete("Use `GetMembers(DynamicBaseMemberType.All).Keys` instead")]
     public override IEnumerable<string> GetDynamicMemberNames()
     {
       PopulatePropInfoCache(GetType());
@@ -170,7 +170,7 @@ namespace Speckle.Core.Models
     /// Gets the names of the defined class properties (typed).
     /// </summary>
     /// <returns></returns>
-    [Obsolete("Use GetMembers(DynamicBaseMemberType.Dynamic).Keys instead")]
+    [Obsolete("Use GetMembers(DynamicBaseMemberType.InstanceAll).Keys instead")]
     public IEnumerable<string> GetInstanceMembersNames() => GetInstanceMembersNames(GetType());
     
     public static IEnumerable<string> GetInstanceMembersNames(Type t)
