6319a91059feeb116cae29a9a6ce994bf4bcfd62
Alan Rynne <alan@speckle.systems>
2022-11-17 12:45:25 +0100

    fix(objects): Removed unnecessary using statements

--- Objects/Objects/Geometry/Box.cs
+++ Objects/Objects/Geometry/Box.cs
@@ -1,10 +1,6 @@
 ﻿using Objects.Primitive;
 using Speckle.Core.Kits;
 using Speckle.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json.Serialization;
 
 namespace Objects.Geometry
 {
