e82e2caf628175a818277f9c874470c6636c46ae
József L. Kiss <50739844+jozseflkiss@users.noreply.github.com>
2022-11-29 06:04:02 +0100

    Archicad/#1936 archicad connector crash during one click send (#1937)
    Archicad Connector: crash during OneClick send #1936

---
+++ ConnectorArchicad/ConnectorArchicad/Communication/Commands/Command_GetElementIds.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Archicad.Communication.Commands
+{
+  internal sealed class GetElementIds : ICommand<IEnumerable<string>>
+  {
+    public enum ElementFilter
+    {
+      All,
+      Selection
+    }
+
+    #region --- Classes ---
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public sealed class Parameters
+    {
+      #region --- Fields ---
+
+      [JsonProperty("elementFilter")]
+      [JsonConverter(typeof(StringEnumConverter))]
+      private ElementFilter Filter { get; }
+
+      #endregion
+
+      #region --- Ctor \ Dtor ---
+
+      public Parameters(ElementFilter filter)
+      {
+        Filter = filter;
+      }
+
+      #endregion
+    }
+
+    [JsonObject(MemberSerialization.OptIn)]
+    private sealed class Result
+    {
+      #region --- Fields ---
+
+      [JsonProperty("applicationIds")]
+      public IEnumerable<string> ApplicationIds { get; private set; }
+
+      #endregion
+    }
+
+    #endregion
+
+    #region --- Fields ---
+
+    private ElementFilter Filter { get; }
+
+    #endregion
+
+    #region --- Ctor \ Dtor ---
+
+    public GetElementIds(ElementFilter filter)
+    {
+      Filter = filter;
+    }
+
+    #endregion
+
+    #region --- Functions ---
+
+    public async Task<IEnumerable<string>> Execute()
+    {
+      Result result = await HttpCommandExecutor.Execute<Parameters, Result>("GetElementIds", new Parameters(Filter));
+      return result.ApplicationIds;
+    }
+
+    #endregion
+  }
+}
