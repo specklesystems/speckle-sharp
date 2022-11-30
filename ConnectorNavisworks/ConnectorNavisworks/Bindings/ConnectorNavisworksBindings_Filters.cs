3c776c2de68057243f079ffd9dac57e6e8681efa
jsdbroughton <jonathon@stardotbmp.com>
2022-11-30 01:06:08 +0000

    Implementing a TreeSelectFilter in Connector and DUI

---
+++ ConnectorNavisworks/ConnectorNavisworks/Bindings/ConnectorNavisworksBindings_Filters.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.DocumentParts;
+using DesktopUI2;
+using DesktopUI2.Models.Filters;
+using DesktopUI2.Models.Settings;
+using Speckle.Core.Kits;
+using Speckle.Core.Models;
+using Application = Autodesk.Navisworks.Api.Application;
+using Document = Autodesk.Navisworks.Api.Document;
+using MenuItem = DesktopUI2.Models.MenuItem;
+
+namespace Speckle.ConnectorNavisworks.Bindings
+{
+    public partial class ConnectorBindingsNavisworks
+    {
+        public override List<ISelectionFilter> GetSelectionFilters()
+        {
+            var filters = new List<ISelectionFilter>();
+
+            var manualFilter = new ManualSelectionFilter();
+
+            if (Doc == null) return filters;
+
+            filters.Add(manualFilter);
+
+            FolderItem selectSetsRootItem = Doc.SelectionSets.RootItem;
+
+            List<ObjectHierarchy> objectHierarchies = selectSetsRootItem.Children.Select(GetObjectHierarchy).ToList();
+
+            if (objectHierarchies.Count <= 0) return filters;
+
+            var selectionSetsFilter = new TreeSelectionFilter
+            {
+                Slug = "sets", Name = "Saved Sets and Selections", Icon = "FileTree",
+                Description = "Select saved selection and search sets to include in the commit.",
+                Values = objectHierarchies
+            };
+            filters.Add(selectionSetsFilter);
+
+            return filters;
+        }
+
+        private static ObjectHierarchy GetObjectHierarchy(SavedItem savedSetItem)
+        {
+            var hierarchyObject = new ObjectHierarchy
+            {
+                DisplayName = savedSetItem.DisplayName,
+                Guid = savedSetItem.Guid,
+                Indices = Doc.SelectionSets.CreateIndexPath(savedSetItem).ToArray(),
+                IndexWith = nameof(ObjectHierarchy.Guid)
+            };
+
+            if (!savedSetItem.IsGroup) return hierarchyObject;
+
+            //iterate the children and output
+            foreach (SavedItem childItem in ((GroupItem)savedSetItem).Children)
+            {
+                hierarchyObject.Elements.Add(GetObjectHierarchy(childItem));
+            }
+
+            return hierarchyObject;
+        }
+    }
+}
---
+++ DesktopUI2/DesktopUI2/Models/Filters/TreeSelectionFilter.cs
@@ -0,0 +1,139 @@
+﻿using DesktopUI2.Views.Filters;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using Autodesk.Navisworks.Gui;
+using DynamicData;
+
+namespace DesktopUI2.Models.Filters
+{
+    // Vanilla Tree Hierarchical Object type. Use-cases may extend with dynamic properties.
+    public class ObjectHierarchy : DynamicObject
+    {
+        public string DisplayName { get; set; }
+        public List<ObjectHierarchy> Elements { get; set; } = new List<ObjectHierarchy>();
+
+        // For use with LINQ queries and presentation
+        public bool IsSelected { get; set; } = false;
+
+        // For applications that record the pointer as a Guid
+        public Guid Guid { get; set; }
+
+        // For applications that record the pointer as successive indexes
+        public int[] Indices { get; set; }
+
+        // For applications that record the pointer as a hash
+        public object Hash { get; set; }
+
+        public string IndexWith { get; set; } = nameof(Guid);
+
+
+        #region Dynamic Property Handling
+
+        private readonly Dictionary<string, object> _members = new Dictionary<string, object>();
+
+        public override bool TryGetMember(
+            GetMemberBinder binder, out object result)
+        {
+            string name = binder.Name.ToLower();
+            return _members.TryGetValue(name, out result);
+        }
+
+        public override bool TrySetMember(
+            SetMemberBinder binder, object value)
+        {
+            _members[binder.Name.ToLower()] = value;
+
+            return true;
+        }
+
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return this._members.Keys;
+        }
+
+        #endregion
+
+        public override string ToString()
+        {
+            string value;
+
+            switch (IndexWith)
+            {
+                case nameof(Guid):
+                    value = $"{Guid}";
+                    break;
+                case nameof(Indices):
+                    value = string.Join(",", Indices);
+                    break;
+                case nameof(DisplayName):
+                    value = DisplayName;
+                    break;
+                case nameof(Hash):
+                    value = $"{Hash}";
+                    break;
+                default:
+                    PropertyInfo prop = typeof(ObjectHierarchy).GetProperty(IndexWith);
+                    value = (prop != null) ? prop.GetValue(this, null).ToString() : string.Empty;
+                    break;
+            }
+
+            return value;
+        }
+    }
+
+    public class SelectedItems : ObservableCollection<ObjectHierarchy>
+    {
+        public SelectedItems() : base()
+        {
+        }
+    }
+
+
+    public class TreeSelectionFilter : ISelectionFilter
+    {
+        public string Type => typeof(TreeSelectionFilter).ToString();
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Icon { get; set; }
+        public string Description { get; set; }
+
+        // Doesn't Start with the Root Node, but all list of all children of the theoretical Root Node
+        public List<ObjectHierarchy> Values { get; set; }
+        public List<string> Selection { get; set; } = new List<string>();
+        public SelectedItems _Items { get; set; }
+        public Type ViewType { get; } = typeof(TreeFilterView);
+
+        public TreeSelectionFilter()
+        {
+            _Items = new SelectedItems();
+
+            _Items.CollectionChanged += Items_CollectionChanged;
+        }
+
+        private void Items_CollectionChanged(object sender,
+            System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Selection.Clear();
+            Selection.AddRange(_Items.Select(item => item.ToString()).ToList());
+        }
+
+        public string Summary
+        {
+            get
+            {
+                if (_Items.Count != 0)
+                {
+                    return string.Join(", ", _Items.Select(item=>item.DisplayName));
+                }
+                else
+                {
+                    return "nothing";
+                }
+            }
+        }
+    }
+}
