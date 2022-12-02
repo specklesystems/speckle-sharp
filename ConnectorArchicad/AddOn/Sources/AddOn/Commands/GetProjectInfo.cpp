dd6226e11db46574f5a2c32ee08e2cbd19349049
József L. Kiss <jozsef.l.kiss@gmail.com>
2022-12-02 11:22:44 +0100

    C++ all

--- ConnectorArchicad/AddOn/Sources/AddOn/Commands/GetProjectInfo.cpp
+++ ConnectorArchicad/AddOn/Sources/AddOn/Commands/GetProjectInfo.cpp
@@ -5,45 +5,44 @@
 namespace AddOnCommands
 {
 
-  static const char* ProjectNameFieldName = "name";
-  static const char* ProjectLocationFieldName = "location";
-  static const char* ProjectLengthUnitsFieldName = "lengthUnit";
-  static const char* ProjectAreaUnitsFieldName = "areaUnit";
-  static const char* ProjectVolumeUnitsFieldName = "volumeUnit";
-  static const char* ProjectAngleUnitsFieldName = "angleUnit";
+static const char* ProjectNameFieldName = "name";
+static const char* ProjectLocationFieldName = "location";
+static const char* ProjectLengthUnitsFieldName = "lengthUnit";
+static const char* ProjectAreaUnitsFieldName = "areaUnit";
+static const char* ProjectVolumeUnitsFieldName = "volumeUnit";
+static const char* ProjectAngleUnitsFieldName = "angleUnit";
 
-  GS::String GetProjectInfo::GetName() const
-  {
-    return GetProjectInfoCommandName;
-  }
+GS::String GetProjectInfo::GetName () const
+{
+	return GetProjectInfoCommandName;
+}
 
-  GS::ObjectState GetProjectInfo::Execute(const GS::ObjectState& /*parameters*/, GS::ProcessControl& /*processControl*/) const
-  {
-    API_ProjectInfo projectInfo{};
+GS::ObjectState GetProjectInfo::Execute (const GS::ObjectState& /*parameters*/, GS::ProcessControl& /*processControl*/) const
+{
+	API_ProjectInfo projectInfo{};
 
-    const GSErrCode err = ACAPI_Environment(APIEnv_ProjectID, &projectInfo, nullptr);
-    if (err != NoError)
-    {
-      return GS::ObjectState{};
-    }
+	const GSErrCode err = ACAPI_Environment (APIEnv_ProjectID, &projectInfo, nullptr);
+	if (err != NoError) {
+		return GS::ObjectState{};
+	}
 
-    //if (projectInfo.untitled)
-    //{
-    //  return GS::ObjectState{ProjectNameFieldName, Untitled};
-    //}
+	//if (projectInfo.untitled)
+	//{
+	//  return GS::ObjectState{ProjectNameFieldName, Untitled};
+	//}
 
-    GS::ObjectState os;
-    os.Add(ProjectNameFieldName, *projectInfo.projectName);
-    os.Add(ProjectLocationFieldName, *projectInfo.projectPath);
+	GS::ObjectState os;
+	os.Add (ProjectNameFieldName, *projectInfo.projectName);
+	os.Add (ProjectLocationFieldName, *projectInfo.projectPath);
 
-    API_WorkingUnitPrefs unitPrefs;
-    ACAPI_Environment(APIEnv_GetPreferencesID, &unitPrefs, (void*)APIPrefs_WorkingUnitsID);
-    os.Add(ProjectLengthUnitsFieldName, unitPrefs.lengthUnit);
-    os.Add(ProjectAreaUnitsFieldName, unitPrefs.areaUnit);
-    os.Add(ProjectVolumeUnitsFieldName, unitPrefs.volumeUnit);
-    os.Add(ProjectAngleUnitsFieldName, unitPrefs.angleUnit);
+	API_WorkingUnitPrefs unitPrefs;
+	ACAPI_Environment (APIEnv_GetPreferencesID, &unitPrefs, (void*) APIPrefs_WorkingUnitsID);
+	os.Add (ProjectLengthUnitsFieldName, unitPrefs.lengthUnit);
+	os.Add (ProjectAreaUnitsFieldName, unitPrefs.areaUnit);
+	os.Add (ProjectVolumeUnitsFieldName, unitPrefs.volumeUnit);
+	os.Add (ProjectAngleUnitsFieldName, unitPrefs.angleUnit);
 
-    return os;
-  }
+	return os;
+}
 
 }
