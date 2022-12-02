2353b785751ebcebce08248b5a4f6fc41fd7bf10
jsdbroughton <jonathon@stardotbmp.com>
2022-12-02 16:37:32 +0000

    Logging

--- ConnectorNavisworks/ConnectorNavisworks/Utils.cs
+++ ConnectorNavisworks/ConnectorNavisworks/Utils.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Xml.Linq;
 using Autodesk.Navisworks.Api;
 using Speckle.Core.Kits;
+using System.Drawing;
+using Console = Colorful.Console;
 
 namespace Speckle.ConnectorNavisworks
 {
@@ -21,5 +23,24 @@ namespace Speckle.ConnectorNavisworks
 #endif
         public static string InvalidChars = @"<>/\:;""?*|=,‘";
         public static string ApplicationIdKey = "applicationId";
+
+
+        public static void ConsoleLog(string message, ConsoleColor color = ConsoleColor.Blue, string hex = "#3b82f6")
+        {
+            Console.WriteLine(message, color: ColorTranslator.FromHtml(hex));
+        }
+
+        public static void WarnLog(string warningMessage)
+        {
+            ConsoleLog(warningMessage, hex: "#f59e0b");
+        }
+
+        public static void ErrorLog(Exception err)
+        {
+            ErrorLog(err.Message);
+            throw err;
+        }
+
+        public static void ErrorLog(string errorMessage) => ConsoleLog(errorMessage, hex: "#ef4444");
     }
 }
