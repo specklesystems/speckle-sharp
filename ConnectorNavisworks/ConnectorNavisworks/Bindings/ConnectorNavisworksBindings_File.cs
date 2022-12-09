2da3f789a265769de415f83212dadcd773163f8e
jsdbroughton <jonathon@stardotbmp.com>
2022-12-09 00:11:45 +0000

    Fix: Saved StreamStates with TreeViewFilter objects recall correctly and restore intact

--- ConnectorNavisworks/ConnectorNavisworks/Bindings/ConnectorNavisworksBindings_File.cs
+++ ConnectorNavisworks/ConnectorNavisworks/Bindings/ConnectorNavisworksBindings_File.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using Autodesk.Navisworks.Api.Data;
+﻿using System.Collections.Generic;
 using DesktopUI2.Models;
-using Speckle.ConnectorNavisworks;
 using Speckle.ConnectorNavisworks.Storage;
 
 namespace Speckle.ConnectorNavisworks.Bindings
 {
-    public partial class ConnectorBindingsNavisworks
+  public partial class ConnectorBindingsNavisworks
+  {
+    public override void WriteStreamsToFile(List<StreamState> streams)
     {
-        public override void WriteStreamsToFile(List<StreamState> streams)
-        {
-            SpeckleStreamManager.WriteStreamStateList(Doc, streams);
-        }
-
-        public override List<StreamState> GetStreamsInFile()
-        {
-            var streams = new List<StreamState>();
-            if (Doc != null) streams = SpeckleStreamManager.ReadState(Doc);
-            return streams;
-        }
+      SpeckleStreamManager.WriteStreamStateList(Doc, streams);
     }
+
+    public override List<StreamState> GetStreamsInFile()
+    {
+      var streams = new List<StreamState>();
+      if (Doc != null) streams = SpeckleStreamManager.ReadState(Doc);
+      return streams;
+    }
+  }
 }
--- DesktopUI2/DesktopUI2/Models/Filters/TreeSelectionFilter.cs
+++ DesktopUI2/DesktopUI2/Models/Filters/TreeSelectionFilter.cs
@@ -7,135 +7,147 @@ using System.Linq;
 using System.Reflection;
 using Autodesk.Navisworks.Gui;
 using DynamicData;
+using Speckle.Newtonsoft.Json;
 
 namespace DesktopUI2.Models.Filters
 {
-    // Vanilla Tree Hierarchical Object type. Use-cases may extend with dynamic properties.
-    public class ObjectHierarchy : DynamicObject
+  public class TreeSelectionFilter : ISelectionFilter
+  {
+    /// <summary>
+    /// Used as the discriminator for deserialization.
+    /// </summary>
+    public string Type => typeof(TreeSelectionFilter).ToString();
+
+    /// <summary>
+    /// User friendly name displayed in the UI
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Internal filter name 
+    /// </summary>
+    public string Slug { get; set; }
+
+    /// <summary>
+    /// MaterialDesignIcon use the demo app from the MaterialDesignInXamlToolkit to get the correct name
+    /// </summary>
+    public string Icon { get; set; }
+
+    /// <summary>
+    /// Should contain a generic description of the filter and how it works.
+    /// </summary>
+    public string Description { get; set; }
+
+    // Doesn't Start with the Root Node, but all list of all first tier children of the theoretical Root Node
+    public List<TreeNode> Values { get; set; }
+
+    /// <summary>
+    /// Holds the values that the user selected from the filter. Not the actual objects.
+    /// </summary>    
+    public List<string> Selection { get; set; } = new List<string>();
+
+    public ObservableCollection<TreeNode> SelectedItems { get; set; } = new ObservableCollection<TreeNode>();
+    Type ISelectionFilter.ViewType { get; } = typeof(TreeFilterView);
+
+    public TreeSelectionFilter() => SelectedItems.CollectionChanged += Items_CollectionChanged;
+
+    /// <summary>
+    /// Should return a succinct summary of the filter: what does it contain inside?
+    /// </summary>
+    public string Summary
     {
-        public string DisplayName { get; set; }
-        public List<ObjectHierarchy> Elements { get; set; } = new List<ObjectHierarchy>();
-
-        // For use with LINQ queries and presentation
-        public bool IsSelected { get; set; } = false;
-
-        // For applications that record the pointer as a Guid
-        public Guid Guid { get; set; }
-
-        // For applications that record the pointer as a Guid
-        public String Reference { get; set; }
-
-        // For applications that record the pointer as successive indexes
-        public int[] Indices { get; set; }
-
-        // For applications that record the pointer as a hash
-        public object Hash { get; set; }
-
-        public string IndexWith { get; set; } = nameof(Guid);
-
-
-        #region Dynamic Property Handling
-
-        private readonly Dictionary<string, object> _members = new Dictionary<string, object>();
-
-        public override bool TryGetMember(
-            GetMemberBinder binder, out object result)
-        {
-            string name = binder.Name.ToLower();
-            return _members.TryGetValue(name, out result);
-        }
-
-        public override bool TrySetMember(
-            SetMemberBinder binder, object value)
-        {
-            _members[binder.Name.ToLower()] = value;
-
-            return true;
-        }
-
-        public override IEnumerable<string> GetDynamicMemberNames()
-        {
-            return this._members.Keys;
-        }
-
-        #endregion
-
-        public override string ToString()
-        {
-            string value;
-
-            switch (IndexWith)
-            {
-                case nameof(Guid):
-                    value = $"{Guid}";
-                    break;
-                case nameof(Indices):
-                    value = string.Join(",", Indices);
-                    break;
-                case nameof(DisplayName):
-                    value = DisplayName;
-                    break;
-                case nameof(Hash):
-                    value = $"{Hash}";
-                    break;
-                default:
-                    PropertyInfo prop = typeof(ObjectHierarchy).GetProperty(IndexWith);
-                    value = (prop != null) ? prop.GetValue(this, null).ToString() : string.Empty;
-                    break;
-            }
-
-            return value;
-        }
+      get
+      {
+        return SelectedItems.Count != 0
+          ? string.Join(", ", SelectedItems.Select(item => item.DisplayName))
+          : "nothing";
+      }
     }
 
-    public class SelectedItems : ObservableCollection<ObjectHierarchy>
+    private void Items_CollectionChanged(object sender,
+      System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
     {
-        public SelectedItems() : base()
-        {
-        }
+      Selection.Clear();
+      Selection.AddRange(SelectedItems.Select(item => item.ToString()).ToList());
+    }
+  }
+
+
+  // Vanilla Tree Hierarchical Object type. Use-cases may extend with dynamic properties.
+  public class TreeNode : DynamicObject
+  {
+    [JsonProperty("DisplayName")] public string DisplayName { get; set; }
+
+    [JsonProperty("Elements")] public List<TreeNode> Elements { get; set; } = new List<TreeNode>();
+
+    // For use with LINQ queries and presentation
+    [JsonProperty("IsSelected")] public bool IsSelected { get; set; } = false;
+
+    // For applications that record the pointer as a Guid
+    [JsonProperty("Guid")] public Guid Guid { get; set; }
+
+    // For applications that record the pointer as a Guid
+    [JsonProperty("Reference")] public String Reference { get; set; }
+
+    // For applications that record the pointer as successive indexes
+    [JsonProperty("Indices")] public int[] Indices { get; set; }
+
+    // For applications that record the pointer as a hash
+    [JsonProperty("Hash")] public object Hash { get; set; }
+
+    [JsonProperty("IndexWith")] public string IndexWith { get; set; } = nameof(Guid);
+
+    #region Dynamic Property Handling
+
+    private readonly Dictionary<string, object> _members = new Dictionary<string, object>();
+
+    public override bool TryGetMember(
+      GetMemberBinder binder, out object result)
+    {
+      string name = binder.Name.ToLower();
+      return _members.TryGetValue(name, out result);
     }
 
-
-    public class TreeSelectionFilter : ISelectionFilter
+    public override bool TrySetMember(
+      SetMemberBinder binder, object value)
     {
-        public string Type => typeof(TreeSelectionFilter).ToString();
-        public string Name { get; set; }
-        public string Slug { get; set; }
-        public string Icon { get; set; }
-        public string Description { get; set; }
+      _members[binder.Name.ToLower()] = value;
 
-        // Doesn't Start with the Root Node, but all list of all children of the theoretical Root Node
-        public List<ObjectHierarchy> Values { get; set; }
-        public List<string> Selection { get; set; } = new List<string>();
-
-        public SelectedItems SelectedItems { get; set; } = new SelectedItems();
-        public Type ViewType { get; } = typeof(TreeFilterView);
-
-        public TreeSelectionFilter()
-        {
-            SelectedItems.CollectionChanged += Items_CollectionChanged;
-        }
-
-        private void Items_CollectionChanged(object sender,
-            System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            Selection.Clear();
-            Selection.AddRange(SelectedItems.Select(item => item.ToString()).ToList());
-        }
-
-        public string Summary
-        {
-            get
-            {
-                if (SelectedItems.Count != 0)
-                {
-                    return string.Join(", ", SelectedItems.Select(item=>item.DisplayName));
-                }
-                else
-                {
-                    return "nothing";
-                }
-            }
-        }
+      return true;
     }
+
+    public override IEnumerable<string> GetDynamicMemberNames()
+    {
+      return this._members.Keys;
+    }
+
+    #endregion
+
+    public override string ToString()
+    {
+      string value;
+
+      switch (IndexWith)
+      {
+        case nameof(Guid):
+          value = $"{Guid}";
+          break;
+        case nameof(Indices):
+          value = string.Join(",", Indices);
+          break;
+        case nameof(DisplayName):
+          value = DisplayName;
+          break;
+        case nameof(Hash):
+          value = $"{Hash}";
+          break;
+        default:
+          PropertyInfo prop = typeof(TreeNode).GetProperty(IndexWith);
+          value = (prop != null) ? prop.GetValue(this, null).ToString() : string.Empty;
+          break;
+      }
+
+      return value;
+    }
+  }
 }
