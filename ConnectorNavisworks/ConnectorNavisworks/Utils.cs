2e0431240075d0cbf823097e2aad6acdfa0a4d69
jsdbroughton <jonathon@stardotbmp.com>
2022-12-09 00:15:16 +0000

    GetUnits

--- ConnectorNavisworks/ConnectorNavisworks/Utils.cs
+++ ConnectorNavisworks/ConnectorNavisworks/Utils.cs
@@ -10,10 +10,10 @@ using Console = Colorful.Console;
 
 namespace Speckle.ConnectorNavisworks
 {
-    public static class Utils
-    {
+  public static class Utils
+  {
 #if NAVMAN20
-        public static string VersionedAppName = HostApplications.Navisworks.GetVersion(HostAppVersion.v2023);
+    public static string VersionedAppName = HostApplications.Navisworks.GetVersion(HostAppVersion.v2023);
 #elif NAVMAN19
         public static string VersionedAppName = HostApplications.Navisworks.GetVersion(HostAppVersion.v2022);
 #elif NAVMAN18
@@ -21,26 +21,32 @@ namespace Speckle.ConnectorNavisworks
 #elif NAVMAN17
         public static string VersionedAppName = HostApplications.Navisworks.GetVersion(HostAppVersion.v2020);
 #endif
-        public static string InvalidChars = @"<>/\:;""?*|=,‘";
-        public static string ApplicationIdKey = "applicationId";
+    public static string InvalidChars = @"<>/\:;""?*|=,‘";
+    public static string ApplicationIdKey = "applicationId";
 
 
-        public static void ConsoleLog(string message, ConsoleColor color = ConsoleColor.Blue, string hex = "#3b82f6")
-        {
-            Console.WriteLine(message, color: ColorTranslator.FromHtml(hex));
-        }
-
-        public static void WarnLog(string warningMessage)
-        {
-            ConsoleLog(warningMessage, hex: "#f59e0b");
-        }
-
-        public static void ErrorLog(Exception err)
-        {
-            ErrorLog(err.Message);
-            throw err;
-        }
-
-        public static void ErrorLog(string errorMessage) => ConsoleLog(errorMessage, hex: "#ef4444");
+    public static void ConsoleLog(string message, ConsoleColor color = ConsoleColor.Blue, string hex = "#3b82f6")
+    {
+      Console.WriteLine(message, color: ColorTranslator.FromHtml(hex));
     }
+
+    public static void WarnLog(string warningMessage)
+    {
+      ConsoleLog(warningMessage, hex: "#f59e0b");
+    }
+
+    public static void ErrorLog(Exception err)
+    {
+      ErrorLog(err.Message);
+      throw err;
+    }
+
+    public static void ErrorLog(string errorMessage) => ConsoleLog(errorMessage, hex: "#ef4444");
+
+
+    public static string GetUnits(Document doc)
+    {
+      return nameof(doc.Units).ToLower();
+    }
+  }
 }
