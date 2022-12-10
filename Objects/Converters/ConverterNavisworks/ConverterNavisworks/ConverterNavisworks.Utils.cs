ddeb32d83c52a4d7071fdfe6af1f54ef301689c1
jsdbroughton <jonathon@stardotbmp.com>
2022-12-10 22:55:07 +0000

    Convert 1-based Array to 0-based Array

--- Objects/Converters/ConverterNavisworks/ConverterNavisworks/ConverterNavisworks.Utils.cs
+++ Objects/Converters/ConverterNavisworks/ConverterNavisworks/ConverterNavisworks.Utils.cs
@@ -1,12 +1,23 @@
 ﻿using Speckle.Core.Kits;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Objects.Converter.Navisworks
 {
-  public partial class ConverterNavisworks
+  internal static class ArrayExtension
+  {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T[] ToArray<T>(this Array arr) where T : struct
     {
-
+      T[] result = new T[arr.Length];
+      Array.Copy(arr, result, result.Length);
+      return result;
     }
+  }
+
+  public partial class ConverterNavisworks
+  {
+  }
 }
