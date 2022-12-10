ab31d39babf4dea793062afa876763ff28e64272
jsdbroughton <jonathon@stardotbmp.com>
2022-12-10 22:53:35 +0000

    Stubbed non implemented Receive methods

--- Objects/Converters/ConverterNavisworks/ConverterNavisworks/ConverterNavisworks.ToNative.cs
+++ Objects/Converters/ConverterNavisworks/ConverterNavisworks/ConverterNavisworks.ToNative.cs
@@ -8,22 +8,10 @@ namespace Objects.Converter.Navisworks
 {
   public partial class ConverterNavisworks
   {
-    public object ConvertToNative(Base @object)
-    {
-      throw new NotImplementedException();
-    }
-
-    public List<object> ConvertToNative(List<Base> objects)
-    {
-      throw new NotImplementedException();
-    }
-
-    public bool CanConvertToNative(Base @object)
-    {
-      switch (@object)
-      {
-        default: return false;
-      }
-    }
+    /// Methods included to satisfy the ISpeckleConverter requirements
+    /// No actual receiving exists
+    public object ConvertToNative(Base @object) => throw new NotImplementedException();
+    public List<object> ConvertToNative(List<Base> objects) => throw new NotImplementedException();
+    public bool CanConvertToNative(Base @object) => throw new NotImplementedException();
   }
 }
