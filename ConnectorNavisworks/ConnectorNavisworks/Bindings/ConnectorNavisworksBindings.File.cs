abfe2d036c7e795c419f410628b4aba994e0d71a
Jonathon Broughton <jonathon@stardotbmp.com>
2022-12-16 19:33:24 +0000

    Add Navisworks (alpha): Connector, Converter and DUI2 additions (#2008)
    * Ported project in from speckle-navis
    
    * Adding Navisworks as a HostApplication
    
    * Adding ConverterNavisworks
    
    * Adding net48 as a target for use in Navisworks
    
    * Shuffling the Filters
    
    * Implementing a TreeSelectFilter in Connector and DUI
    
    * Nips and Tucks
    
    * Launch with command line flags to open Sample Model
    
    * feat(dui): adds support for hiding the receive tab
    
    * fix: loads receive modes before checking for them :)
    
    * fix(dui): hides setting button if none is available
    
    * chore: removes unused bindings
    
    * Logging
    
    * Working NWF
    
    * Clashes
    
    * Differentiation of generic solution to specific
    
    * Store states in file
    
    * Tidy up
    
    * further integration for treeviewfilter
    
    * Binding tidyups
    
    * Example Model Changes
    
    * Fix: Saved StreamStates with TreeViewFilter objects recall correctly and restore intact
    
    * Basic Send Implementation in-lieu of the Converter being ready
    
    TODO: Needs more Navisworks like traversal
    
    * Code Cleanup
    
    * GetUnits
    
    * test build
    
    * Postbuild events switched to per user targets
    
    * Convert Selections from SavedStream Filters to ObjectIds
    
    Saved Sets and Clash Results stubbed only
    
    * Sample Model saves
    
    * Fix: Actual Units returned
    
    * Convert 1-based Array to addressable Array
    
    * Remove ConvertableObject
    
    Shifted to more the lightweight pseudoId from ModelItem InwOaPaths
    
    * Get Select Objects
    
    Uses a pseudoId generated from the InwOaPath ArrayData object.
    
    TODO: Longer term this should imply a guard function to detect if the path structure is still valid
    
    * Handle converted pseudoId addressed objects and Send
    
    The toConvertDict and the While loop addresses the need to have recursion and a tree of Base objects, while also allowing for the flat list to represent all objects
    
    * Stubbed non implemented Receive methods
    
    * tidying up
    
    * Convert 1-based Array to 0-based Array
    
    * First port of conversions from Rimshot
    
    * Remove ConvertableObject
    
    * Wrapper for ConvertToSpeckle to address the future recursive conversion methods
    
    * Stubbed CanConvertToSpeckle - returns true for Geometry Elements
    
    * Magic gathering of ModelItem from the pseudoId
    
    * Initial Send Traversal
    
    * ToSpeckle Fundamentals
    
    * Object Sorter
    
    * SavedStreams
    
    * Simple displayValue addition.
    
    Geometry only at this point. Bare properties.
    
    * Sample Model Update
    
    * Add Navisworks projects to All.sln
    
    * Added 2020, 2021, 2022 Navisworks versions
    
    * Expand CanConvert to all Geometry Nodes
    
    TODO: Some nodes are parent nodes that artificially report HasGeometry=True - leading to dupe elements.
    
    * Adoption of Speckle.Navisworks.Api Nugets
    
    * Rebuild of Connector Solution
    
    * Send traversal refactor 1
    
    Thread safety, memory guards, Navisworks progress bar
    
    * Remove local assembly references
    
    * Additional Converter Versions added for Navisworks
    
    * style: Renaming Bindings partials
    
    * delete: cruft file
    
    * style: renaming Version Projects for Connector and Converter
    
    * fix: remove errant dependency and missing file
    
    * fix: repoint solution to renamed version projects
    
    * fix: repointing to renamed projects
    
    * fix: repointing to renamed projects
    
    * fix: Remove Colorful.Console package
    
    * style: remove unused directives
    
    * fix: adapting logging helpers to do without Colurful.Console
    
    Co-authored-by: Matteo Cominetti <matteo@speckle.systems>

---
+++ ConnectorNavisworks/ConnectorNavisworks/Bindings/ConnectorNavisworksBindings.File.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using DesktopUI2.Models;
+using Speckle.ConnectorNavisworks.Storage;
+
+namespace Speckle.ConnectorNavisworks.Bindings
+{
+  public partial class ConnectorBindingsNavisworks
+  {
+    public override void WriteStreamsToFile(List<StreamState> streams)
+    {
+      SpeckleStreamManager.WriteStreamStateList(Doc, streams);
+    }
+
+    public override List<StreamState> GetStreamsInFile()
+    {
+      var streams = new List<StreamState>();
+      if (Doc != null) streams = SpeckleStreamManager.ReadState(Doc);
+      return streams;
+    }
+  }
+}
