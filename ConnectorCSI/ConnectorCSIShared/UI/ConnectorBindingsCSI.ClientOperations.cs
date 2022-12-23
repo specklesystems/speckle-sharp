7737e274a1bd3a36e6e489f12229f76c0d1d12c3
Connor Ivy <connor@speckle.systems>
2022-12-23 15:13:48 -0600

    fix stream state loading and saving

--- ConnectorCSI/ConnectorCSIShared/UI/ConnectorBindingsCSI.ClientOperations.cs
+++ ConnectorCSI/ConnectorCSIShared/UI/ConnectorBindingsCSI.ClientOperations.cs
@@ -23,12 +23,12 @@ namespace Speckle.ConnectorCSI.UI
     public override void WriteStreamsToFile(List<StreamState> streams)
     {
       StreamStateManager.ClearStreamStateList(Model);
+      DocumentStreams = new List<StreamState>();
 
       foreach (var s in streams)
-      {
         DocumentStreams.Add(s);
-        WriteStateToFile();
-      }
+
+      WriteStateToFile();
     }
 
     //public override void AddNewStream(StreamState state)
