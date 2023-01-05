068f48522180b18cbda7745afc32898321aed0ff
Gergő Jedlicska <gergo@jedlicska.com>
2023-01-05 17:12:43 +0100

    add do spaces context to deploy job creator

--- .circleci/scripts/config-generator.py
+++ .circleci/scripts/config-generator.py
@@ -186,6 +186,7 @@ def getNewDeployJob(jobName: str):
         "extension": "zip" if isMac else "exe",
         "requires": ["deploy-connectors", jobName],
         "filters": getTagFilter([jobName]),
+        "context": "do-spaces-speckle-releases",
     }
     return {"deploy-connector-new": deployJob}
 
