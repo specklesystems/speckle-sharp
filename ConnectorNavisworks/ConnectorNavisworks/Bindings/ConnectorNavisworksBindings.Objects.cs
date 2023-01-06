51d7bf95330af5d3d431af335d6da63de2c170a3
Jonathon Broughton <jonathon@stardotbmp.com>
2023-01-06 16:08:27 +0000

    Fix (navisworks): Model YZ orientation (#2044)
    * Expanding the Conversion State possibilities to Enum from bool
    
    In theory, further states could be added and useful
    
    * alpha3 version flag
    
    * fix: Kit loading logic and instantiation
    
    * fix: smarter conversion state tracking
    
    * fix: Model Orientation XY problem with non-architectural models
    
    ElevationMode implementation checks for the current UP vector for the document and overrides YZ axis flipping on the basis that Speckle assumes XY ground plane with Z elevation.
    
    * fix: Correctly process aggregate models that promote to the root node.
    
    When aggregating models, Navisworks aggregates some formats to be at the root node and in others, the root node is void and the first child is the quasi root node. This adds a pseudoId just for root.
    
    TODO: detect both cases and only use this root node pseudoId when needed.
    
    * fix: tweak to the YZ issue fix #2015
    
    TODO: detect for the handedness of the model to correctly apply the negative Z fix only when necessary.
    
    * fix: selection logic improvements
    
    distinction between selected object count, objects to convert count, and converted counts
    
    * fix: further code legibility improvements

--- ConnectorNavisworks/ConnectorNavisworks/Bindings/ConnectorNavisworksBindings.Objects.cs
+++ ConnectorNavisworks/ConnectorNavisworks/Bindings/ConnectorNavisworksBindings.Objects.cs
@@ -1,10 +1,11 @@
-﻿using DesktopUI2.Models.Filters;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Autodesk.Navisworks.Api;
-using Cursor = System.Windows.Forms.Cursor;
 using System.Windows.Forms;
+using Autodesk.Navisworks.Api;
+using DesktopUI2.Models.Filters;
+using Cursor = System.Windows.Forms.Cursor;
+using static Speckle.ConnectorNavisworks.Utils;
 
 namespace Speckle.ConnectorNavisworks.Bindings
 {
@@ -18,7 +19,7 @@ namespace Speckle.ConnectorNavisworks.Bindings
 
     private static IEnumerable<string> GetObjectsFromFilter(ISelectionFilter filter)
     {
-      List<string> filteredObjects = new List<string>();
+      var filteredObjects = new List<string>();
 
       switch (filter.Slug)
       {
@@ -41,41 +42,63 @@ namespace Speckle.ConnectorNavisworks.Bindings
 
     private static IEnumerable<string> GetObjectsFromSelection(ISelectionFilter filter)
     {
-      // Manual Selection is a straightforward collection of the pseudoIds
-
-      // TODO: Handle a resorted selection Tree. Effectively this could be:
+      // TODO: Handle a resorted selection Tree as this invalidates any saves filter selections. Effectively this could be:
       // a) Delete any saved streams based on Manual Selection
       // b) Change what is stored to allow for a cross check that the pseudoId still matches the original item at the path
       // c) As a SelectionTree isChanging event load in all manual selection saved states and watch the changes and rewrite the result
 
-      return filter.Selection;
+      // Manual Selection becomes a straightforward collection of the pseudoIds and all descendant nodes
+      // The saved filter should store only the visible selected nodes
+
+      var selection = filter.Selection;
+
+      HashSet<string> uniqueIds = new HashSet<string>();
+
+      for (int i = 0; i < selection.Count; i += 1)
+      {
+        var modelItem = PointerToModelItem(selection[i]);
+
+        if (modelItem == null) continue;
+
+        if (!IsElementVisible(modelItem)) continue;
+        var descendants = modelItem.DescendantsAndSelf.ToList();
+
+        for (int j = 0; j < descendants.Count; j += 1)
+        {
+          var item = descendants[j];
+          if (!IsElementVisible(item)) continue;
+          uniqueIds.Add(GetPseudoId(item));
+        }
+      }
+
+      return uniqueIds.ToList();
     }
 
     private static IEnumerable<string> GetObjectsFromSavedSets(ISelectionFilter filter)
     {
       Cursor.Current = Cursors.WaitCursor;
       // Saved Sets filter stores Guids of the selection sets. This can be converted to ModelItem pseudoIds
-      List<Guid> selections = filter.Selection.Select(guid => new Guid(guid)).ToList();
-      List<SelectionSet> savedItems = selections
+      var selections = filter.Selection.Select(guid => new Guid(guid)).ToList();
+      var savedItems = selections
         .Select(Doc.SelectionSets.ResolveGuid)
         .Cast<SelectionSet>()
         .ToList();
 
-      HashSet<string> objectPseudoIds = new HashSet<string>();
+      var objectPseudoIds = new HashSet<string>();
 
       savedItems.ForEach(
         item =>
         {
           // If the Saved Set is a Selection, add all the saved items and map to pseudoIds
           if (item.HasExplicitModelItems)
-            objectPseudoIds.UnionWith(item.ExplicitModelItems.Select(Utils.GetPseudoId)
+            objectPseudoIds.UnionWith(item.ExplicitModelItems.Select(GetPseudoId)
             );
 
           // If the Saved Set is a Search, add all the matching items and map to pseudoIds
           if (item.HasSearch)
             objectPseudoIds
               .UnionWith(item.Search
-                .FindAll(Doc, false).Select(Utils.GetPseudoId)
+                .FindAll(Doc, false).Select(GetPseudoId)
               );
         });
 
