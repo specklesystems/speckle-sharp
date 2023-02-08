e58d10c61e93ee5fe1bb372522a9ebf7a584517c
jenessaman <69314485+jenessaman@users.noreply.github.com>
2023-02-08 13:16:21 -0500

    feat(microstation): Reorganise and rename Bentley solution and projects, remove DUI1 support (#2094)
    * chore: make DUI the default UI in standalone mode
    
    * Reorganise and rename Bentley solution and projects, remove DUI1 support
    
    ---------
    
    Co-authored-by: Matteo Cominetti <matteo@speckle.systems>

---
+++ ConnectorBentley/ConnectorBentleyShared/Entry/App.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+using Speckle.DesktopUI;
+using Speckle.DesktopUI.Utils;
+using Stylet.Xaml;
+
+using Bentley.DgnPlatformNET;
+using Bentley.DgnPlatformNET.Elements;
+using Bentley.GeometryNET;
+using Bentley.MstnPlatformNET;
+
+using Speckle.ConnectorBentley.UI;
+using System.Reflection;
+using System.IO;
+
+namespace Speckle.ConnectorBentley.Entry
+{
+  [AddIn(MdlTaskID = "Speckle")]
+  public class SpeckleBentleyApp : AddIn
+  {
+    public static SpeckleBentleyApp App;
+
+    public SpeckleBentleyApp(IntPtr mdlDesc) : base(mdlDesc)
+    {
+    }
+
+    protected override int Run(string[] commandLine)
+    {
+      App = this;
+      return 0;
+    }
+
+    internal static SpeckleBentleyApp Instance
+    {
+      get { return App; }
+    }
+
+    // for DUI2
+    public void Start(string unparsed)
+    {
+      AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(OnAssemblyResolve);
+
+      SpeckleBentleyCommand.InitAvalonia();
+      SpeckleBentleyCommand.Bindings = new ConnectorBindingsBentley();
+      SpeckleBentleyCommand.CreateOrFocusSpeckle();
+    }
+
+    // This is necessary for finding assemblies when building Avalonia 
+    static Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+    {
+      Assembly a = null;
+      var name = args.Name.Split(',')[0];
+      string path = Path.GetDirectoryName(typeof(App).Assembly.Location);
+
+      string assemblyFile = Path.Combine(path, name + ".dll");
+
+      if (File.Exists(assemblyFile))
+        a = Assembly.LoadFrom(assemblyFile);
+
+      return a;
+    }
+  }
+  
+}
