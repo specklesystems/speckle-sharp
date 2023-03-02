81c280d78fe4ac0c9e7c4570824080943694b9fa
Alan Rynne <alan@speckle.systems>
2023-03-02 17:48:50 +0100

    fix(objects): Instances review with claire

--- Objects/Objects/Other/Instance.cs
+++ Objects/Objects/Other/Instance.cs
@@ -36,33 +36,6 @@ namespace Objects.Other
     {
       this.transform = transform ?? new Transform();
     }
-
-    /// <summary>
-    /// Returns the a copy of the Block Definition's geometry transformed with this BlockInstance's transform.
-    /// </summary>
-    /// <returns>The transformed geometry for this BlockInstance.</returns>
-    public abstract List<ITransformable> GetTransformedGeometry();
-
-    /// <summary>
-    /// Returns a plane representing the insertion point and orientation of this Block instance.
-    /// </summary>
-    /// <remarks>This method will skip scaling. If you need scaling, we recommend using the transform instead.</remarks>
-    /// <returns>A Plane on the insertion point of this Block Instance, with the correct 3-axis rotations.</returns>
-    public abstract Plane GetInsertionPlane();
-
-    /// <summary>
-    /// Retrieves Instance insertion point by applying <see cref="transform"/> to <see cref="BlockDefinition.basePoint"/>
-    /// </summary>
-    /// <returns>Insertion point as a <see cref="Point"/></returns>
-    public abstract Point GetInsertionPoint();
-    
-    /// <inheritdoc cref="GetInsertionPlane"/>
-    [JsonIgnore]
-    public Plane insertionPlane => GetInsertionPlane();
-    
-    /// <inheritdoc cref="GetTransformedGeometry"/>
-    [JsonIgnore]
-    public List<ITransformable> transformedGeometry => GetTransformedGeometry();
   }
   
   /// <summary>
@@ -98,9 +71,6 @@ namespace Objects.Other
   /// </summary>
   public class BlockInstance : Instance<BlockDefinition>
   {
-    [JsonIgnore, Obsolete("Use GetInsertionPoint method"), SchemaIgnore]
-    public Point insertionPoint { get => GetInsertionPoint(); set { } }
-    
     [DetachProperty, Obsolete("Use definition property")]
     public BlockDefinition blockDefinition { get => _definition; set => _definition = value; }
 
@@ -114,8 +84,9 @@ namespace Objects.Other
       // whereas the instance transform assumes it contains the basePoint translation already.
       //this.transform = transform * blockDefinition.GetBasePointTransform();
     }
-
-    public override Point GetInsertionPoint()
+    
+    [Obsolete("Use GetInsertionPlane().origin")]
+    public Point GetInsertionPoint()
     {
       var newMatrix = transform.matrix;
       newMatrix.M14 -= Convert.ToSingle(_definition.basePoint.x);
@@ -125,7 +96,7 @@ namespace Objects.Other
       return transformed;
     }
 
-    public override List<ITransformable> GetTransformedGeometry()
+    public List<ITransformable> GetTransformedGeometry()
     {
       return _definition.geometry.SelectMany(b =>
       {
@@ -147,19 +118,27 @@ namespace Objects.Other
       }).Where(b => b != null).ToList();
     }
 
-    public override Plane GetInsertionPlane()
+    public Plane GetInsertionPlane()
     {
       // TODO: UPDATE!
       var plane = new Plane(_definition.basePoint ?? new Point(0,0), new Vector(0, 0, 1, units), new Vector(1, 0, 0, units), new Vector(0, 1, 0, units), units);
       plane.TransformTo(transform, out Plane tPlane);
       return tPlane;
     }
+    
+    /// <inheritdoc cref="GetInsertionPlane"/>
+    [JsonIgnore]
+    public Plane insertionPlane => GetInsertionPlane();
+    
+    /// <inheritdoc cref="GetTransformedGeometry"/>
+    [JsonIgnore]
+    public List<ITransformable> transformedGeometry => GetTransformedGeometry();
   }
 }
 
 namespace Objects.Other.Revit
 {
-  public class RevitInstance : Instance<FamilyType>
+  public class RevitInstance : Instance<RevitSymbolElementType>
   {
     public Level level { get; set; }
     public bool facingFlipped { get; set; }
@@ -167,39 +146,28 @@ namespace Objects.Other.Revit
     public Base parameters { get; set; }
     public string elementId { get; set; }
     
-
-    public override Base definition { 
-      get 
-      {
-        return elementDefinition;
-      }
-      set 
-      {
-        if (value is RevitSymbolElementType)
-        {
-          elementDefinition = (RevitSymbolElementType)value;
-        }
-      }
+    public List<ITransformable> GetTransformedGeometry()
+    {
+      // TODO: Figure out transform of revit instances for GH/DYN
+      return new List<ITransformable>();
     }
 
+    public Plane GetInsertionPlane()
+    {
+      // TODO: Check for Revit in GH/DYN
+      var plane = new Plane(new Point(0,0), new Vector(0, 0, 1, units), new Vector(1, 0, 0, units), new Vector(0, 1, 0, units), units);
+      plane.TransformTo(transform, out Plane tPlane);
+      return tPlane;
+    }
+
+    /// <inheritdoc cref="GetInsertionPlane"/>
     [JsonIgnore]
-    private RevitSymbolElementType elementDefinition { get; set; }
-
+    public Plane insertionPlane => GetInsertionPlane();
+    
+    /// <inheritdoc cref="GetTransformedGeometry"/>
+    [JsonIgnore]
+    public List<ITransformable> transformedGeometry => GetTransformedGeometry();
+    
     public RevitInstance() { }
-
-    public override List<ITransformable> GetTransformedGeometry()
-    {
-      throw new NotImplementedException();
-    }
-
-    public override Plane GetInsertionPlane()
-    {
-      throw new NotImplementedException();
-    }
-
-    public override Point GetInsertionPoint()
-    {
-      throw new NotImplementedException();
-    }
   }
 }
