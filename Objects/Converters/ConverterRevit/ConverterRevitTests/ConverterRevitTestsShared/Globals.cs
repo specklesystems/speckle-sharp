86f2a7c201819e5f8dea74c29bf4ab0beb105421
Connor Ivy <connor@speckle.systems>
2023-03-03 12:48:28 -0600

    rearrange tests into different folders

---
+++ Objects/Converters/ConverterRevit/ConverterRevitTests/ConverterRevitTestsShared/Globals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterRevitTests
+{
+  public static class Globals
+  {
+    public static string GetTestModel(string filename)
+    {
+#if REVIT2021
+      var path = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "TestModels", "2021", filename);
+#elif REVIT2022
+#elif REVIT2023
+      var path = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "TestModels", "2023", filename);
+#endif
+      return path;
+
+    }
+  }
+}
