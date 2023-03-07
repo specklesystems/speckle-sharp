243769a4dccc815fb59c0bc02c1fcf3f292f3098
Gergő Jedlicska <gergo@jedlicska.com>
2023-03-07 17:23:45 +0100

    fix(core http): fix ping error reporting

--- Core/Core/Helpers/Http.cs
+++ Core/Core/Helpers/Http.cs
@@ -142,9 +142,11 @@ namespace Speckle.Core.Helpers
         int timeout = 1000;
         PingOptions pingOptions = new PingOptions();
         PingReply reply = await myPing.SendPingAsync(hostname, timeout, buffer, pingOptions);
-        return (reply.Status == IPStatus.Success);
+        if (reply.Status != IPStatus.Success)
+          throw new Exception($"The ping operation failed with status {reply.Status}");
+        return true;
       });
-      if (policyResult.Result == true)
+      if (policyResult.Outcome == OutcomeType.Successful)
         return true;
       Log.Warning(
         policyResult.FinalException,
