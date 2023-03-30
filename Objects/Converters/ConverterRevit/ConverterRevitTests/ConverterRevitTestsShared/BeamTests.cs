ffcc8309267d6f65a6bf495e943c1319efa47589
connorivy <43247197+connorivy@users.noreply.github.com>
2023-03-30 16:48:38 -0500

    Feat(Revit) Revamp integration tests (#2366)
    * rearrange tests into different folders
    
    * change to using revit async
    
    * added speckle specific utils
    
    * add xUnitRevit as submodule
    
    * add refs to submodule
    
    * feat(xUnit): don't close files if tests fail
    
    * silence speckle connector
    
    * only run one test fixture at a time
    
    * added speckleObjectTestNumber to "Beam" file
    
    * run the send operation in revit context
    
    * change adaptive components, beams, breps, columns
    
    * modify curve tests
    
    * modifications to some files
    
    * fix direct shape test
    
    * fix duct test
    
    * fix family instance tests
    
    * fix floor tests
    
    * fix opening tests
    
    * delete old files
    
    * fix pipe tests
    
    * fix roof tests
    
    * fix room tests
    
    * fix wall tests
    
    * fix wire tests
    
    * delete unneeded files
    
    * again fix wire tests
    
    * add back the speckle connector
    
    * don't build test projects in release mode
    
    ---------
    
    Co-authored-by: Connor Ivy <connor@speckle.systems>

---
+++ Objects/Converters/ConverterRevit/ConverterRevitTests/ConverterRevitTestsShared/BeamTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Autodesk.Revit.DB;
+using DB = Autodesk.Revit.DB;
+using System.Collections.Generic;
+
+using Xunit;
+using System.Threading.Tasks;
+
+namespace ConverterRevitTests
+{
+  public class BeamFixture : SpeckleConversionFixture
+  {
+    public override string TestFile => Globals.GetTestModel("Beam.rvt");
+
+    public override string UpdatedTestFile => Globals.GetTestModel("BeamUpdated.rvt");
+    public override string NewFile => Globals.GetTestModel("BeamToNative.rvt");
+    public override List<BuiltInCategory> Categories => new List<BuiltInCategory> { BuiltInCategory.OST_StructuralFraming };
+    public BeamFixture() : base()
+    {
+    }
+  }
+
+  public class BeamTests : SpeckleConversionTest, IClassFixture<BeamFixture>
+  {
+    public BeamTests(BeamFixture fixture) : base(fixture)
+    {
+    }
+
+    [Fact]
+    [Trait("Beam", "ToSpeckle")]
+    public async Task BeamToSpeckle()
+    {
+      await NativeToSpeckle();
+    }
+
+    #region ToNative
+
+    [Fact]
+    [Trait("Beam", "ToNative")]
+    public async Task BeamToNative()
+    {
+      await SpeckleToNative<DB.FamilyInstance>(AssertFamilyInstanceEqual);
+    }
+
+    [Fact]
+    [Trait("Beam", "ToNativeUpdates")]
+    public async Task BeamToNativeUpdates()
+    {
+      await SpeckleToNativeUpdates<DB.FamilyInstance>(AssertFamilyInstanceEqual);
+    }
+
+    [Fact]
+    [Trait("Beam", "Selection")]
+    public async Task BeamSelectionToNative()
+    {
+      await SelectionToNative<DB.FamilyInstance>(AssertFamilyInstanceEqual);
+    }
+
+    #endregion
+
+  }
+}
---
+++ Objects/Converters/ConverterRevit/ConverterRevitTests/ConverterRevitTestsShared/Globals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterRevitTests
+{
+  public static class Globals
+  {
+    public static string GetTestModel(string filename)
+    {
+      var path = string.Empty;
+#if REVIT2021
+      path = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "TestModels", "2021", filename);
+#elif REVIT2022
+#elif REVIT2023
+      path = Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "TestModels", "2023", filename);
+#endif
+      return path;
+
+    }
+  }
+}
