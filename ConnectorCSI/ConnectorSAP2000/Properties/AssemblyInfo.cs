920859335adc06315b3e64429106724706c668ca
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2023-03-30 16:10:38 +0100

    Monorepo cleanup/csi (#2364)
    * refactor(csi): wip repo cleanup csi
    
    * (no branch): Auto stash before checking out "HEAD"
    
    ---------
    
    Co-authored-by: Alan Rynne <alan@speckle.systems>

--- ConnectorCSI/ConnectorSAP2000/Properties/AssemblyInfo.cs
+++ ConnectorCSI/ConnectorSAP2000/Properties/AssemblyInfo.cs
@@ -1,18 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ConnectorSAP2000")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConnectorSAP2000")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,17 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("31e0c098-6813-4571-ab96-a245e0fc1c23")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]
