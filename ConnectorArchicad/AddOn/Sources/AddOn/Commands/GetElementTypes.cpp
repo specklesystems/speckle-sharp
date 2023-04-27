253792ceae235ac09fb9287a59c41f5a245ed699
József L. Kiss <>
2023-04-27 15:50:53 +0200

    feat(Archicad): Archicad stories/levels send and receive #2341

--- ConnectorArchicad/AddOn/Sources/AddOn/Commands/GetElementTypes.cpp
+++ ConnectorArchicad/AddOn/Sources/AddOn/Commands/GetElementTypes.cpp
@@ -19,16 +19,16 @@ GS::String GetElementTypes::GetName () const
 GS::ObjectState GetElementTypes::Execute (const GS::ObjectState& parameters, GS::ProcessControl& /*processControl*/) const
 {
 	GS::Array<GS::UniString> ids;
-	parameters.Get (ApplicationIds, ids);
+	parameters.Get (ElementBase::ApplicationIds, ids);
 
 	GS::ObjectState result;
 
-	const auto& listAdder = result.AddList<GS::ObjectState> (ElementTypes);
+	const auto& listAdder = result.AddList<GS::ObjectState> (ElementBase::ElementTypes);
 	for (const GS::UniString& id : ids) {
 		API_Guid guid = APIGuidFromString (id.ToCStr ());
 		API_ElemTypeID elementTypeId = Utility::GetElementType (guid);
 		GS::UniString elemType = elementNames.Get (elementTypeId);
-		GS::ObjectState listElem{ApplicationId, id, ElementType, elemType};
+		GS::ObjectState listElem{ElementBase::ApplicationId, id, ElementBase::ElementType, elemType};
 		listAdder (listElem);
 	}
 
