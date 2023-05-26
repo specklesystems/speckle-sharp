87ee0f38f09f7cd81a143b4181a8c7df733f9821
connorivy <43247197+connorivy@users.noreply.github.com>
2023-05-26 12:50:16 -0500

    Refactor(Revit): Abstract previously received objects logic into interface (#2596)
    * added revitSharedResources2023
    
    * add reference to shared resources in converter
    
    * add for revit2022
    
    * everything works except for the interface segregation principle
    
    * current functionality with interface
    
    * undo works
    
    * fix location of revitSharedResources2022
    
    * add revitSharedResources2021
    
    * add revitSharedResources2020
    
    * Revert "undo works"
    
    This reverts commit fef8334cf839de6740d5164bec817787ae9a04af.
    
    * remove dependency on previously received objects
    
    * fix tests
    
    * created IConvertObjectsCache
    
    * add new project to test project
    
    * add projects to sln
    
    * fix dynamo build
    
    * removed revit specific type from connector converter proj
    
    * remove revit specific project
    
    * rename cache to map and cleanup old implementation
    
    * fixed connector.sln
    
    * fix dynamo
    
    * remove traces of revit specific projects
    
    ---------
    
    Co-authored-by: Connor Ivy <connor@speckle.systems>

---
+++ ConnectorRevit/RevitSharedResources/Interfaces/IConvertedObjectsCache.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace RevitSharedResources.Interfaces
+{
+  /// <summary>
+  /// Objects that implement the IConvertedObjectsCache interface are responsible for
+  /// querying and mutating a cache of objects that have been converted during the current converion operation.
+  /// This object will then get passed into an IReceivedObjectsCache to be saved
+  /// </summary>
+  public interface IConvertedObjectsCache<TFrom, TTo>
+  {
+    #region Add
+    public void AddConvertedObjects(TFrom converted, IList<TTo> created);
+    #endregion
+
+    #region Query
+
+    #region GetConverted
+    public IEnumerable<TFrom> GetConvertedObjects();
+    public IEnumerable<TFrom> GetConvertedObjectsFromCreatedId(string id);
+    public bool HasConvertedObjectWithId(string id);
+    #endregion
+
+    #region GetCreated
+    public IEnumerable<TTo> GetCreatedObjects();
+    public IEnumerable<TTo> GetCreatedObjectsFromConvertedId(string id);
+    public bool HasCreatedObjectWithId(string id);
+    #endregion
+
+    #endregion
+  }
+}
