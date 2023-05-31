2860447778684a370a3cae2ee1bdc40a8e950c3e
Connor Ivy <connor@speckle.systems>
2023-05-31 07:04:57 -0500

    remove unused code

--- Objects/Converters/ConverterRevit/ConverterRevitTests/TestGenerator/TestTemplate.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitTests/TestGenerator/TestTemplate.cs
@@ -17,52 +17,6 @@ namespace ConverterRevitTests
     public const string EndNamespace = @"
 }
 ";
-    //    public static string Create(string category, string fileName, string revitType, string syncAssertFunc) => $@"
-    //  public class {category}{fileName}Fixture : SpeckleConversionFixture
-    //  {{
-    //    public override string Category => ""{category}"";
-    //    public override string TestName => ""{fileName}"";
-
-    //    public {category}{fileName}Fixture() : base()
-    //    {{
-    //    }}
-    //  }}
-
-    //  public class {category}{fileName}Tests : SpeckleConversionTest, IClassFixture<{category}{fileName}Fixture>
-    //  {{
-    //    public {category}{fileName}Tests({category}{fileName}Fixture fixture) : base(fixture)
-    //    {{
-    //    }}
-
-    //    [Fact]
-    //    [Trait(""{category}"", ""{fileName}ToSpeckle"")]
-    //    public async Task {category}{fileName}ToSpeckle()
-    //    {{
-    //      await NativeToSpeckle();
-    //    }}
-
-    //    [Fact]
-    //    [Trait(""{category}"", ""{fileName}ToNative"")]
-    //    public async Task {category}{fileName}ToNative()
-    //    {{
-    //      await SpeckleToNative<{revitType}>({syncAssertFunc});
-    //    }}
-
-    //    [Fact]
-    //    [Trait(""{category}"", ""{fileName}ToNativeUpdates"")]
-    //    public async Task {category}{fileName}ToNativeUpdates()
-    //    {{
-    //      await SpeckleToNativeUpdates<{revitType}>({syncAssertFunc});
-    //    }}
-
-    //    [Fact]
-    //    [Trait(""{category}"", ""{fileName}Selection"")]
-    //    public async Task {category}{fileName}SelectionToNative()
-    //    {{
-    //      await SelectionToNative<{revitType}>({syncAssertFunc});
-    //    }}
-    //  }}
-    //";
     public static string CreateFixture(string category, string fileName) => $@"
   public class {category}{fileName}Fixture : SpeckleConversionFixture
   {{
