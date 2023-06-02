71027c9eeb0beeadb8a98d306d6b41fa35957ee6
Jonathon Broughton <jonathon@stardotbmp.com>
2023-06-02 17:33:55 +0100

    Removing placeholder code

--- ConnectorNavisworks/ConnectorNavisworks/Entry/Commands.cs
+++ ConnectorNavisworks/ConnectorNavisworks/Entry/Commands.cs
@@ -6,11 +6,6 @@ public abstract class LaunchSpeckleConnector
   public const string Plugin = "SpeckleUI";
 }
 
-//public class OneClickSend
-//{
-//    public const string Command = "Speckle_OneClickSend";
-//}
-
 public abstract class Community
 {
   public const string Command = "Speckle_Community";
