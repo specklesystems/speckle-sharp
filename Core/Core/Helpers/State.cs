80924ba5daa9c42ac2d0b47dffccfa89da2b7fac
József L. Kiss <>
2023-06-12 14:54:16 +0200

    StateChangeLock

--- Core/Core/Helpers/State.cs
+++ Core/Core/Helpers/State.cs
@@ -8,51 +8,69 @@ public class State<T> : IDisposable where T : State<T>, new()
   static T current;
   T previous = current;
 
+  private static readonly object StateChangeLock = new();
+
   protected State()
   {
-    current = (T) this;
-    if (root == null)
-      root = (T) this;
+    lock (StateChangeLock)
+    {
+      current = (T)this;
+      if (root == null)
+        root = (T)this;
+    }
   }
 
   void IDisposable.Dispose()
   {
-    if (previous == null)
-      return; // Already disposed or root
-
-    if (current == this) current = previous;
-    else
+    lock (StateChangeLock)
     {
-      // If this state is still in the stack is safe to pop it
-      var state = this;
-      do
+      if (previous == null)
+        return; // Already disposed or root
+
+      if (current == this) current = previous;
+      else
       {
-        if (state == root) { current = previous; break; }
+        // If this state is still in the stack is safe to pop it
+        var state = this;
+        do
+        {
+          if (state == root) { current = previous; break; }
 
-        state = state.previous;
-      } while (state != null);
+          state = state.previous;
+        } while (state != null);
+      }
+
+      previous = null;
     }
-
-    previous = null;
   }
 
   public static T Peek => current;
 
   public static T Push()
   {
-    var peek = current ?? new T();
-    var top = (T) peek.MemberwiseClone();
-    top.previous = current;
-    return current = top;
+    lock (StateChangeLock)
+    {
+      var peek = current ?? new T();
+      var top = (T)peek.MemberwiseClone();
+      top.previous = current;
+
+      return current = top;
+    }
   }
 
   public static void Pop()
   {
-    ((IDisposable) current).Dispose();
+    lock (StateChangeLock)
+    {
+      ((IDisposable)current).Dispose();
+    }
   }
 
   protected void Pull()
   {
-    ((IDisposable) this).Dispose();
+    lock (StateChangeLock)
+    {
+      ((IDisposable)this).Dispose();
+    }
   }
 }
