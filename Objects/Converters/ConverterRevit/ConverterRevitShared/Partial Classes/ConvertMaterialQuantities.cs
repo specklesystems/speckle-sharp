4e6a501a8f71ff9478c5a542ffafe98cc17b5afb
Kilian Speiser <kilsp@dtu.dk>
2023-06-14 17:18:52 +0200

    computing quantities based on geometry for selelcted categories.

--- Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertMaterialQuantities.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertMaterialQuantities.cs
@@ -3,6 +3,8 @@ using Autodesk.Revit.DB.Structure;
 using Objects.BuiltElements;
 using Objects.BuiltElements.Revit;
 using Speckle.Core.Models;
+using Speckle.netDxf.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using DB = Autodesk.Revit.DB;
@@ -22,11 +24,30 @@ namespace Objects.Converter.Revit
     {
       if (material == null || element == null) 
         return null;
+      double volume = 0;double area=0;
 
-      // To-Do: These methods from the Revit API appear to have bugs.
-      double volume = element.GetMaterialVolume(material.Id);
-      double area = element.GetMaterialArea(material.Id, false);
-
+      if(element is DB.MEPCurve )
+      {
+        GetGeometry(element, out List<DB.Mesh> meshes, out List<DB.Solid> solids);
+        volume = solids.Select(s => s.Volume).Sum();
+        var a = solids.Select(s => s.Faces.Cast<DB.Face>().Select(face => face.Area).Max());
+        area = solids.Select(s => s.Faces.Cast<DB.Face>().Select(face => face.Area).Max()).Sum();
+      }
+      else if (RequiresGeometryComputation(element))
+      {
+        GetGeometry(element, out List<DB.Mesh> meshes, out List<DB.Solid> solids);
+        volume = solids.Where(solid => solid.Volume > 0 && !solid.Faces.IsEmpty && solid.Faces.get_Item(0).MaterialElementId == material.Id)
+                   .Sum(solid => solid.Volume);
+        var all = solids.Where(solid => solid.Volume > 0 && !solid.Faces.IsEmpty && solid.Faces.get_Item(0).MaterialElementId == material.Id)
+          .Select(solid => solid.Faces.Cast<Face>().Select(face => face.Area)
+          .Max()).Sum();
+      }
+      else
+      {
+        // To-Do: These methods from the Revit API appear to have bugs.
+        volume += element.GetMaterialVolume(material.Id);
+        area += element.GetMaterialArea(material.Id, false);
+      }
       // Convert revit interal units to speckle commit units
       double factor = ScaleToSpeckle(1);
       volume *= factor * factor * factor;
@@ -73,7 +94,66 @@ namespace Objects.Converter.Revit
     }
 
     #endregion
-    
+    /// <summary>
+    /// Revit API default methods for computing volumes and areas is malfunctioning for some objects
+    /// 
+    /// </summary>
+    /// <param name="element"></param>
+    /// <returns>true f the element's quantities needs to be computetd based on the geometry.returns>
+    private bool RequiresGeometryComputation(DB.Element element)
+    {
+      if (element.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Windows || element.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Doors) return true;
+      return false;
+    }
+    private void GetGeometry(DB.Element element, out List<DB.Mesh> meshes, out List<DB.Solid> solids, bool isConvertedAsInstance = false, DB.Options options = null)
+    {
+      options ??= new DB.Options();
+      GeometryElement geom = null;
+      solids = null;
+      meshes = null;
+      try
+      {
+        geom = element.get_Geometry(options);
+      }
+      catch (Autodesk.Revit.Exceptions.ArgumentException)
+      {
+        options.ComputeReferences = false;
+        geom = element.get_Geometry(options);
+      }
+      if (geom != null)
+      {
+        // retrieves all meshes and solids from a geometry element
+        List<DB.Solid> sol = new List<DB.Solid>();
+        List<DB.Mesh> meshs = new List<DB.Mesh>();
+        SortGeometry(geom);
+        void SortGeometry(GeometryElement geom)
+        {
+          foreach (GeometryObject geomObj in geom)
+          {
+            switch (geomObj)
+            {
+              case DB.Solid solid:
+                if (solid.Faces.Size > 0 && Math.Abs(solid.SurfaceArea) > 0) // skip invalid solid
+                  sol.Add(solid);
+                break;
+              case DB.Mesh mesh:
+                meshs.Add(mesh);
+                break;
+              case GeometryInstance instance:
+                var instanceGeo = isConvertedAsInstance ? instance.GetSymbolGeometry() : instance.GetInstanceGeometry();
+                SortGeometry(instanceGeo);
+                break;
+              case GeometryElement element:
+                SortGeometry(element);
+                break;
+            }
+          }
+        }
+        solids = sol;
+        meshes = meshs;
+      }
+    }
+
   }
 
 }
