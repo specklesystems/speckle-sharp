7afbee32a74e548de7d6778efd4fe1279fa3f541
Jonathon Broughton <jonathon@stardotbmp.com>
2023-06-22 22:09:34 +0100

    Add NavisworksOptionsManager class with Autosave set/get
    This commit adds a new Autosave.cs file to the ConnectorNavisworks project. The file contains a NavisworksSettingsManager class that handles updating and restoring the auto-save setting in Navisworks.
    
    It is modelled as a Partial class as a stub for potentially handling other Host Application Global Options interop.

---
+++ ConnectorNavisworks/ConnectorNavisworks/NavisworksOptions/Autosave.cs
@@ -0,0 +1,46 @@
+﻿using Autodesk.Navisworks.Api.Interop;
+using static Autodesk.Navisworks.Api.Interop.LcOpRegistry;
+using static Autodesk.Navisworks.Api.Interop.LcUOption;
+
+namespace Speckle.ConnectorNavisworks.NavisworksOptions;
+
+/// <summary>
+/// Manages the Autosave settings.
+/// </summary>
+public partial class NavisworksOptionsManager
+{
+  private bool _autosaveSetting;
+
+  /// <summary>
+  /// Updates the auto-save setting.
+  /// </summary>
+  /// <param name="enable">A boolean value indicating whether to enable or disable auto-save.</param>
+  private void UpdateAutoSaveSetting(bool enable)
+  {
+    using var optionLock = new LcUOptionLock();
+    var rootOptions = GetRoot(optionLock);
+    _autosaveSetting = rootOptions.GetBoolean("general.autosave.enable");
+
+    if (_autosaveSetting == enable)
+      return;
+
+    rootOptions.SetBoolean("general.autosave.enable", enable);
+    SaveGlobalOptions();
+  }
+
+  /// <summary>
+  /// Disables the auto-save feature.
+  /// </summary>
+  public void DisableAutoSave()
+  {
+    UpdateAutoSaveSetting(false);
+  }
+
+  /// <summary>
+  /// Restores the auto-save setting to its original state after the send process.
+  /// </summary>
+  public void RestoreAutoSave()
+  {
+    UpdateAutoSaveSetting(true);
+  }
+}
