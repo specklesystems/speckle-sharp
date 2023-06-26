dbe1517429f83e7346de91918577f6ef3834c281
József L. Kiss <>
2023-06-26 16:24:30 +0200

    merge fix

--- ConnectorArchicad/ConnectorArchicad/Converters/Converters/ObjectConverter.cs
+++ ConnectorArchicad/ConnectorArchicad/Converters/Converters/ObjectConverter.cs
@@ -30,11 +30,12 @@ namespace Archicad.Converters
     static List<Mesh> GetDisplayValue(Base element)
     {
       return DefaultTraversal.displayValueAliases.Concat(DefaultTraversal.geometryAliases)
-        .SelectMany(m => {
+        .SelectMany(m =>
+        {
           if (element[m] is IEnumerable<object> meshes)
             return meshes.Where(mesh => mesh is Mesh).Cast<Mesh>();
           return new List<Mesh>();
-          }
+        }
         ).ToList();
     }
 
@@ -104,7 +105,8 @@ namespace Archicad.Converters
     private static List<string> Store(TraversalContext tc, Dictionary<string, Transform> transformMatrixById, Dictionary<string, Mesh> transformedMeshById)
     {
       var meshes = GetDisplayValue(tc.current);
-      return meshes.Select(mesh => {
+      return meshes.Select(mesh =>
+      {
         string cumulativeTransformId = (tc as ArchicadDefinitionTraversalContext).cumulativeTransformKey;
         var transformedMeshId = Utilities.hashString(cumulativeTransformId + mesh.id);
         if (!transformedMeshById.TryGetValue(transformedMeshId, out Mesh transformedMesh))
@@ -125,7 +127,7 @@ namespace Archicad.Converters
       var meshModels = new List<MeshModel>();
       var transformMatrixById = new Dictionary<string, Transform>();
       var transformedMeshById = new Dictionary<string, Mesh>();
-      
+
       var context = Archicad.Helpers.Timer.Context.Peek;
       using (context?.cumulativeTimer?.Begin(ConnectorArchicad.Properties.OperationNameTemplates.ConvertToNative, "Object"))
       {
@@ -146,7 +148,7 @@ namespace Archicad.Converters
           {
             meshIdHashes = CreateArchicadDefinitionTraverseFunc().Traverse(element)
               .Select(tc => StoreTransformationMatrix(tc, transformMatrixById))
-              .Where(tc => DefaultTraversal.HasDisplayValue(tc.current) || DefaultTraversal.HasGeometry (tc.current))
+              .Where(tc => DefaultTraversal.HasDisplayValue(tc.current) || DefaultTraversal.HasGeometry(tc.current))
               .SelectMany(tc => Store(tc, transformMatrixById, transformedMeshById))
               .ToList();
 
@@ -157,48 +159,21 @@ namespace Archicad.Converters
           // if the same geometry representation is not used before
           if (!archicadObjects.Any(archicadObject => archicadObject.modelIds.SequenceEqual(meshIdHashes)))
           {
-            var element = tc.current;
-
-            // base point
-            var basePoint = new Point(0, 0, 0);
-            // var specialKeys = element.GetMembers();
-            var transform = (Transform)(element["transform"]) ?? new Transform();
-
-            // todo Speckle schema
-            //if (specialKeys.ContainsKey("basePoint"))
-            //  basePoint = (Point)element["basePoint"];
-
-            List<string> meshIdHashes;
-            {
-              Transform baseTransform = new Transform();
-              baseTransform.id = "1530eda076784bfaa61728b060ed8d43";
-              Transform newTransform = new Transform();
-              newTransform.id = "0cecc0af9be7465b958d736618817315";
-
-              meshIdHashes = TraverseDefinitions(element, baseTransform, newTransform, transformMatrixById, transformedMeshById);
-
-              if (meshIdHashes == null)
-                continue;
-            }
-
-            // if the same geometry representation is not used before
-            if (!archicadObjects.Any(archicadObject => archicadObject.modelIds.SequenceEqual(meshIdHashes)))
-            {
-              var meshes = meshIdHashes.ConvertAll(meshIdHash => transformedMeshById[meshIdHash]);
-              var meshModel = ModelConverter.MeshToNative(meshes);
-              meshModels.Add(meshModel);
-            }
-
-            var newObject = new Archicad.ArchicadObject
-            {
-              id = element.id,
-              applicationId = element.applicationId,
-              pos = Utils.ScaleToNative(basePoint),
-              transform = new Objects.Other.Transform(transform.ConvertToUnits(Units.Meters), Units.Meters),
-              modelIds = meshIdHashes
-            };
-            archicadObjects.Add(newObject);
+            var meshes = meshIdHashes.ConvertAll(meshIdHash => transformedMeshById[meshIdHash]);
+            var meshModel = ModelConverter.MeshToNative(meshes);
+            meshModels.Add(meshModel);
           }
+
+          var newObject = new Archicad.ArchicadObject
+          {
+            id = element.id,
+            applicationId = element.applicationId,
+            pos = Utils.ScaleToNative(basePoint),
+            transform = new Objects.Other.Transform(transform.ConvertToUnits(Units.Meters), Units.Meters),
+            modelIds = meshIdHashes
+          };
+
+          archicadObjects.Add(newObject);
         }
       }
 
