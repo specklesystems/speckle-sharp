ceee489a51397608df662a3c4648f39caf3bc9e5
Jonathon Broughton <jonathon@stardotbmp.com>
2023-07-12 18:45:12 +0100

    Amend GetFileName method to handle exceptions and improve error handling
    - Added checks for active document and models in the active document.
    - If no active document is found, a FileNotFoundException is thrown with an appropriate message.
    - If no models are found in the active document, a FileNotFoundException is thrown with an appropriate message.

--- ConnectorNavisworks/ConnectorNavisworks/Bindings/ConnectorNavisworksBindings.File.cs
+++ ConnectorNavisworks/ConnectorNavisworks/Bindings/ConnectorNavisworksBindings.File.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using DesktopUI2.Models;
+using Sentry.Protocol;
 using Speckle.ConnectorNavisworks.Storage;
+using Application = Autodesk.Navisworks.Api.Application;
 
 namespace Speckle.ConnectorNavisworks.Bindings;
 
@@ -17,5 +20,18 @@ public partial class ConnectorBindingsNavisworks
     if (_doc != null)
       streams = SpeckleStreamManager.ReadState(_doc);
     return streams;
+  }
+
+  public override string GetFileName()
+  {
+    var activeDoc = Application.ActiveDocument;
+
+    if (activeDoc == null)
+      throw (new FileNotFoundException("No active document found."));
+
+    if (activeDoc.Models.Count == 0)
+      throw (new FileNotFoundException("No models found in active document."));
+
+    return Application.ActiveDocument.CurrentFileName ?? string.Empty;
   }
 }
