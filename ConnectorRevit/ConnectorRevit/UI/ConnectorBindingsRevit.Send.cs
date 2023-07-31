be218945b739b54a94bb5012688a89a658717b98
Connor Ivy <connor@speckle.systems>
2023-07-31 10:10:27 -0500

    view specific design option fix

--- ConnectorRevit/ConnectorRevit/UI/ConnectorBindingsRevit.Send.cs
+++ ConnectorRevit/ConnectorRevit/UI/ConnectorBindingsRevit.Send.cs
@@ -48,7 +48,7 @@ namespace Speckle.ConnectorRevit.UI
       var streamId = state.StreamId;
       var client = state.Client;
 
-      var selectedObjects = GetSelectionFilterObjectsWithDesignOptions(converter, state.Filter);
+      var selectedObjects = GetSelectionFilterObjects(converter, state.Filter);
       state.SelectedObjectIds = selectedObjects.Select(x => x.UniqueId).ToList();
 
       if (!selectedObjects.Any())
