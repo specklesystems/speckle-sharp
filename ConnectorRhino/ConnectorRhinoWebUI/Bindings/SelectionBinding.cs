43403e8846f4a2837f9794f0892e1a6efa160a84
oguzhankoral <oguzhankoral@gmail.com>
2023-08-07 23:41:07 +0300

    Self destructive onIdle for selection

--- ConnectorRhino/ConnectorRhinoWebUI/Bindings/SelectionBinding.cs
+++ ConnectorRhino/ConnectorRhinoWebUI/Bindings/SelectionBinding.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using DUI3;
@@ -12,31 +12,59 @@ public class SelectionBinding : ISelectionBinding
   public string Name { get; set; } = "selectionBinding";
   public IBridge Parent { get; set; }
 
-  private bool _selectionExpired;
-  
+  /// <summary>
+  /// Gets event handler of <see cref="OnIdle(object, EventArgs)"/>.
+  /// </summary>
+  private EventHandler? idle;
+
   public SelectionBinding()
   {
-    RhinoDoc.SelectObjects += (sender, args) => { _selectionExpired = true; };
-    RhinoDoc.DeselectObjects += (sender, args) => { _selectionExpired = true; };
-    RhinoDoc.DeselectAllObjects += (sender, args) => { _selectionExpired = true; };
+    RhinoDoc.SelectObjects += (sender, args) => { this.EnableIdle(); };
+    RhinoDoc.DeselectObjects += (sender, args) => { this.EnableIdle(); };
+    RhinoDoc.DeselectAllObjects += (sender, args) => { this.EnableIdle(); };
 
     RhinoDoc.EndOpenDocumentInitialViewUpdate += (sender, args) =>
     {
       // Resets selection doc change
       Parent?.SendToBrowser(DUI3.Bindings.SelectionBindingEvents.SetSelection, new SelectionInfo());
     };
-    
-    RhinoApp.Idle += (sender, args) =>
-    {
-      if (!_selectionExpired) return;
-      var selInfo = GetSelection();
-      Parent?.SendToBrowser(DUI3.Bindings.SelectionBindingEvents.SetSelection, selInfo);
-      _selectionExpired = false;
-    };
-  } 
- 
-  public SelectionInfo GetSelection()
+  }
+
+  /// <summary>
+  /// Enables idle event.
+  /// </summary>
+  public void EnableIdle()
   {
+    if (this.idle == null)
+    {
+      RhinoApp.Idle += this.idle = this.OnIdle;
+    }
+  }
+
+  /// <summary>
+  /// Disables idle event.
+  /// </summary>
+  private void DisableIdle()
+  {
+    if (this.idle != null)
+    {
+      RhinoApp.Idle -= this.idle;
+      this.idle = null;
+    }
+  }
+
+  /// <inheritdoc cref="RhinoApp.Idle"/>
+  private void OnIdle(object sender, EventArgs e)
+  {
+    // Disable idle event handler until enabled by others.
+    this.DisableIdle();
+
+    var selInfo = GetSelection();
+    Parent?.SendToBrowser(DUI3.Bindings.SelectionBindingEvents.SetSelection, selInfo);
+  }
+
+  public SelectionInfo GetSelection()
+    {
     var objects = RhinoDoc.ActiveDoc.Objects.GetSelectedObjects(false, false).ToList();
     var objectIds = objects.Select(o => o.Id.ToString()).ToList();
     var layerCount = objects.Select(o => o.Attributes.LayerIndex).Distinct().Count();
@@ -47,5 +75,4 @@ public class SelectionBinding : ISelectionBinding
       Summary = $"{objectIds.Count} objects ({String.Join(", ", objectTypes)}) from {layerCount} layer{(layerCount != 1 ? "s" : "")}"
     };
   }
-
 }
