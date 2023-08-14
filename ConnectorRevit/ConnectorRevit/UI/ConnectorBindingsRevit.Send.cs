a7014e37711e361a718e38c613224e4f14ff84dc
Connor Ivy <connor@speckle.systems>
2023-08-14 14:56:24 -0500

    fix unintented change

--- ConnectorRevit/ConnectorRevit/UI/ConnectorBindingsRevit.Send.cs
+++ ConnectorRevit/ConnectorRevit/UI/ConnectorBindingsRevit.Send.cs
@@ -80,7 +80,7 @@ namespace Speckle.ConnectorRevit.UI
       var conversionProgressDict = new ConcurrentDictionary<string, int> { ["Conversion"] = 0 };
       var convertedCount = 0;
 
-      await APIContext.Run(_ =>
+      await APIContext.Run(() =>
       {
         using var _d0 = LogContext.PushProperty("converterName", converter.Name);
         using var _d1 = LogContext.PushProperty("converterAuthor", converter.Author);
