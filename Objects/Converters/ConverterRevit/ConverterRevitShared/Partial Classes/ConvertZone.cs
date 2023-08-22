76016dd68998b1b1356bbe814d29801e4fa966b4
Jonathon Broughton <jonathon@stardotbmp.com>
2023-08-22 20:45:28 +0100

    feat: Add Zone class and conversion methods
    This commit adds the `Zone` class to the `Objects.BuiltElements.Revit` namespace. The `Zone` class represents a collection of one or more spaces in Revit. It has properties such as name, level, service type, isDefault, units, area, volume, and perimeter.
    
    The commit also includes conversion methods in the `ConverterRevit` class for converting a Speckle zone to a native Revit zone (`ZoneToNative`) and vice versa (`ZoneToSpeckle`). Additionally, there is a method (`ConvertAndCacheZone`) for converting and caching a Revit zone.

---
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertZone.cs
@@ -0,0 +1,69 @@
+using Autodesk.Revit.DB;
+using Objects.BuiltElements;
+using Speckle.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Objects.BuiltElements.Revit;
+using DB = Autodesk.Revit.DB.Mechanical;
+using Point = Objects.Geometry.Point;
+
+namespace Objects.Converter.Revit
+{
+  public partial class ConverterRevit
+  {
+    public ApplicationObject ZoneToNative(Zone speckleZone)
+    {
+      var revitZone = GetExistingElementByApplicationId(speckleZone.applicationId) as DB.Zone;
+      var appObj = new ApplicationObject(speckleZone.id, speckleZone.speckle_type)
+      {
+        applicationId = speckleZone.applicationId
+      };
+
+      return appObj;
+    }
+
+    public BuiltElements.Revit.Zone ZoneToSpeckle(DB.Zone revitZone)
+    {
+      var speckleZone = new Zone
+      {
+        name = revitZone.Name,
+        area = GetParamValue<double>(revitZone, BuiltInParameter.ROOM_AREA),
+        volume = GetParamValue<double>(revitZone, BuiltInParameter.ROOM_VOLUME),
+        perimeter = GetParamValue<double>(revitZone, BuiltInParameter.ZONE_PERIMETER),
+        serviceType = revitZone.ServiceType.ToString()
+      };
+
+      // Optionally Zones and Spaces could be sent as a Parent Child, but edge cases abound.
+      // var spaces = new List<DB.Space>();
+      // var zoneSpaces = revitZone.Spaces.GetEnumerator();
+      //
+      // while (zoneSpaces.MoveNext())
+      // {
+      //   if (zoneSpaces.Current is DB.Space space)
+      //   {
+      //     spaces.Add(space);
+      //   }
+      // }
+      // speckleZone.spaces = spaces.Select(x => SpaceToSpeckle(x)).ToList();
+
+      GetAllRevitParamsAndIds(speckleZone, revitZone);
+
+      // No implicit displayValue
+      // speckleZone.displayValue = GetElementDisplayValue(revitSpace);
+
+      return speckleZone;
+    }
+
+    private Zone ConvertAndCacheZone(DB.Zone revitZone, Document doc)
+    {
+      if (revitZone == null)
+        return null;
+
+      if (!Zones.ContainsKey(revitZone.Name))
+        Zones[revitZone.Name] = ZoneToSpeckle(revitZone);
+
+      return Zones[revitZone.Name];
+    }
+  }
+}
--- Objects/Objects/BuiltElements/Zone.cs
+++ Objects/Objects/BuiltElements/Zone.cs
@@ -3,65 +3,36 @@ using Objects.Geometry;
 using Speckle.Core.Kits;
 using Speckle.Core.Models;
 
-namespace Objects.BuiltElements;
+namespace Objects.BuiltElements.Revit;
 
-public class Zone : Base, IHasArea, IHasVolume, IDisplayValue<List<Mesh>>
+public class Zone : Base, IHasArea, IHasVolume
 {
   public Zone() { }
 
-  [SchemaInfo("Space", "Creates a Speckle space", "BIM", "MEP")]
-  public Zone(string name, string number, [SchemaMainParam] Point basePoint, Level level)
-  {
-    this.name = name;
-    this.number = number;
-    this.basePoint = basePoint;
-    this.level = level;
-  }
-
   [SchemaInfo(
-    "Space with top level and offset parameters",
-    "Creates a Speckle space with the specified top level and offsets",
+    "A zone is a collection of one or more spaces.",
+    "Creates a Speckle zone to be used as a reference for spaces.",
     "BIM",
     "MEP"
   )]
-  public Zone(
-    string name,
-    string number,
-    [SchemaMainParam] Point basePoint,
-    Level level,
-    Level topLevel,
-    double topOffset,
-    double baseOffset
-  )
+  public Zone(string name)
   {
     this.name = name;
-    this.number = number;
-    this.basePoint = basePoint;
-    this.level = level;
-    this.topLevel = topLevel;
-    this.topOffset = topOffset;
-    this.baseOffset = baseOffset;
   }
 
   public string name { get; set; }
-  public string number { get; set; }
-  public Point basePoint { get; set; }
+
   public Level level { get; set; }
-  public double baseOffset { get; set; }
-  public Level topLevel { get; set; } // corresponds to UpperLimit property in Revit api
-  public double topOffset { get; set; } // corresponds to LimitOffset property in Revit api
-  public List<ICurve> voids { get; set; } = new();
-  public ICurve outline { get; set; }
-  public string spaceType { get; set; }
-  public string zoneName { get; set; }
+
+  public string serviceType { get; set; }
+
+  public bool isDefault { get; set; }
 
   public string units { get; set; }
 
-  // additional properties to add: also inclue space separation lines here? Phase? Associated Room? Zone object instead of id?
-
-  [DetachProperty]
-  public List<Mesh> displayValue { get; set; }
-
+  // implicit measurements
   public double area { get; set; }
   public double volume { get; set; }
+
+  public double perimeter { get; set; }
 }
