e078a2fd6c9418997cc09dc6a1ca9e46c44137a6
Jonathon Broughton <jonathon@stardotbmp.com>
2023-08-30 12:23:26 +0100

    Refactor ConvertZone.cs: improve zone conversion logic
    This commit refactors the `ZoneToNative` method in the `ConvertZone.cs` file to improve the logic for converting zones. The changes include:
    - Removing unnecessary using statements
    - Adding a new method, `ConvertZoneToRevit`, to handle the actual conversion process
    - Updating the `ZoneToNative` method to use the new conversion method and update the application object with relevant information
    - Implementing logic to check if a zone with the same name already exists in the document and updating it accordingly
    - Handling phase-related operations, including checking if a target phase exists and creating a default phase if not found
    - Setting instance parameters for existing zones or creating new ones as needed
    
    These changes aim to enhance the accuracy and efficiency of zone conversions in Revit.

--- Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertZone.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertZone.cs
@@ -1,6 +1,3 @@
-using Autodesk.Revit.DB;
-using Objects.BuiltElements;
-using Speckle.Core.Models;
 using System;
 using System.Linq;
 using Autodesk.Revit.DB;
@@ -12,15 +9,79 @@ namespace Objects.Converter.Revit
 {
   public partial class ConverterRevit
   {
-    public ApplicationObject ZoneToNative(Zone speckleZone)
+    public ApplicationObject ZoneToNative(RevitZone speckleZone)
     {
-      var revitZone = GetExistingElementByApplicationId(speckleZone.applicationId) as DB.Zone;
+      var revitZone = ConvertZoneToRevit(speckleZone, out ApplicationObject.State state);
       var appObj = new ApplicationObject(speckleZone.id, speckleZone.speckle_type)
       {
         applicationId = speckleZone.applicationId
       };
-
+      appObj.Update(status: state, createdId: revitZone.UniqueId, convertedItem: revitZone);
       return appObj;
+    }
+
+    public DB.Zone ConvertZoneToRevit(RevitZone speckleZone, out ApplicationObject.State state)
+    {
+      state = ApplicationObject.State.Unknown;
+
+      var docZones = new FilteredElementCollector(Doc).OfClass(typeof(DB.Zone)).ToElements().Cast<DB.Zone>();
+
+      if (speckleZone == null)
+        return null;
+
+      var hasZoneWithSameName = docZones.Any(x => x.Name == speckleZone.name);
+
+      DB.Zone existingZone = null;
+
+      // a zone that had been previously received
+      var revitZone = GetExistingElementByApplicationId(speckleZone.applicationId) as DB.Zone;
+
+      var revitZoneLevel = ConvertLevelToRevit(speckleZone.level, out _);
+
+      var activePhase = Doc.Phases
+        .Cast<Phase>()
+        .FirstOrDefault(x => x.Name == Doc.ActiveView.get_Parameter(BuiltInParameter.VIEW_PHASE).AsValueString());
+
+      // Does this zone phase exist on the document?
+      var targetPhase = Doc.Phases.Cast<Phase>().FirstOrDefault(x => x.Name == speckleZone["phaseName"]);
+
+      if (targetPhase == null)
+      {
+        // We're out of options, API doesn't allow us to create a Phase.
+        Report.LogConversionError(
+          new Exception($"Could not find phase `{speckleZone["phaseName"]}`, a default phase will be used.")
+        );
+        targetPhase = activePhase;
+      }
+
+      if (revitZone != null)
+      {
+        if (!hasZoneWithSameName)
+          revitZone.Name = speckleZone.name;
+
+        if (revitZone.Phase != null)
+        {
+          if (revitZone.Phase.Name != speckleZone["phaseName"])
+          {
+            // we'll have to delete the existing Zone and Make a new one.
+            revitZone = Doc.Create.NewZone(revitZoneLevel, targetPhase);
+            revitZone.Name = speckleZone.name;
+          }
+
+          SetInstanceParameters(revitZone, speckleZone);
+        }
+        else
+        {
+          revitZone = Doc.Create.NewZone(revitZoneLevel, targetPhase);
+          revitZone.Name = speckleZone.name;
+        }
+
+        state = ApplicationObject.State.Updated;
+      }
+
+      state = ApplicationObject.State.Created;
+
+      return revitZone;
     }
 
     public RevitZone ZoneToSpeckle(DB.Zone revitZone)
