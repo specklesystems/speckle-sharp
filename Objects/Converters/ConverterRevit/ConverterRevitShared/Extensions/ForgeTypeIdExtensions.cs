31993958fc3764e6d784f4098731895bc0de8810
Connor Ivy <connor@speckle.systems>
2023-08-31 15:39:31 -0500

    get rid of unused extension

--- Objects/Converters/ConverterRevit/ConverterRevitShared/Extensions/ForgeTypeIdExtensions.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Extensions/ForgeTypeIdExtensions.cs
@@ -1,6 +1,5 @@
 #nullable enable
 #if !REVIT2020
-using System.Collections.Generic;
 using System.Linq;
 using Autodesk.Revit.DB;
 using DB = Autodesk.Revit.DB;
@@ -26,23 +25,6 @@ namespace ConverterRevitShared.Extensions
     public static string ToUniqueString(this ForgeTypeId forgeTypeId)
     {
       return forgeTypeId.TypeId;
-    }
-    private readonly static HashSet<string> lengthTypes = new()
-    {
-      UnitTypeId.Millimeters.ToUniqueString(),
-      UnitTypeId.Centimeters.ToUniqueString(),
-      UnitTypeId.Meters.ToUniqueString(),
-      UnitTypeId.Inches.ToUniqueString(),
-      UnitTypeId.Feet.ToUniqueString(),
-      UnitTypeId.FeetFractionalInches.ToUniqueString(),
-    };
-    public static bool IsLengthType(this ForgeTypeId forgeTypeId)
-    {
-      if (lengthTypes.Contains(forgeTypeId.ToUniqueString()))
-      {
-        return true;
-      }
-      return false;
     }
   }
 }
