578f904c301d8e48d4ce0d3d7f87f2caa7c58c16
Jonathon Broughton <jonathon@stardotbmp.com>
2023-09-01 13:08:28 +0100

    refactor out handler methods from the single conversion method.
    This commit adds support for creating and updating Revit zones. Significant changes include:
    - Implemented logic to determine the target phase for a zone
    - Created a method to create a new Revit zone if it doesn't exist or update an existing one
    - Handled setting instance parameters for the zone
    - Updated the speckleZone object with the level information from the Revit zone

--- Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertZone.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertZone.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Linq;
 using Autodesk.Revit.DB;
+using Objects.BuiltElements;
 using Objects.BuiltElements.Revit;
 using Speckle.Core.Models;
 using DB = Autodesk.Revit.DB.Mechanical;
+using Level = Autodesk.Revit.DB.Level;
 
 namespace Objects.Converter.Revit
 {
@@ -24,62 +26,132 @@ namespace Objects.Converter.Revit
     {
       state = ApplicationObject.State.Unknown;
 
-      var docZones = new FilteredElementCollector(Doc).OfClass(typeof(DB.Zone)).ToElements().Cast<DB.Zone>();
-
       if (speckleZone == null)
         return null;
 
-      var hasZoneWithSameName = docZones.Any(x => x.Name == speckleZone.name);
+      var revitZone = GetExistingZone(speckleZone);
 
-      DB.Zone existingZone = null;
-
-      // a zone that had been previously received
-      var revitZone = GetExistingElementByApplicationId(speckleZone.applicationId) as DB.Zone;
+      var targetPhase = DetermineTargetPhase(speckleZone, revitZone);
 
       var revitZoneLevel = ConvertLevelToRevit(speckleZone.level, out _);
-
-      var activePhase = Doc.Phases
-        .Cast<Phase>()
-        .FirstOrDefault(x => x.Name == Doc.ActiveView.get_Parameter(BuiltInParameter.VIEW_PHASE).AsValueString());
-
-      // Does this zone phase exist on the document?
-      var targetPhase = Doc.Phases.Cast<Phase>().FirstOrDefault(x => x.Name == speckleZone["phaseName"]);
-
-      if (targetPhase == null)
+      if (revitZone == null)
       {
-        // We're out of options, API doesn't allow us to create a Phase.
-        Report.LogConversionError(
-          new Exception($"Could not find phase `{speckleZone["phaseName"]}`, a default phase will be used.")
-        );
-        targetPhase = activePhase;
-      }
+        DB.Zone sameNameZone = GetZoneWithSameName(speckleZone);
 
-      if (revitZone != null)
-      {
-        if (!hasZoneWithSameName)
-          revitZone.Name = speckleZone.name;
-
-        if (revitZone.Phase != null)
+        if (sameNameZone != null && sameNameZone.Phase.Name == speckleZone.phaseName)
         {
-          if (revitZone.Phase.Name != speckleZone["phaseName"])
-          {
-            // we'll have to delete the existing Zone and Make a new one.
-            revitZone = Doc.Create.NewZone(revitZoneLevel, targetPhase);
-            revitZone.Name = speckleZone.name;
-          }
-
-          SetInstanceParameters(revitZone, speckleZone);
+          revitZone = sameNameZone;
         }
         else
         {
-          revitZone = Doc.Create.NewZone(revitZoneLevel, targetPhase);
-          revitZone.Name = speckleZone.name;
-        }
+          if (sameNameZone != null)
+          {
+            Doc.Delete(sameNameZone.Id);
+            Doc.Regenerate();
+          }
 
-        state = ApplicationObject.State.Updated;
+          revitZone = CreateRevitZone(revitZoneLevel, targetPhase, speckleZone.name);
+        }
+      }
+      else
+      {
+        revitZone.Name = speckleZone.name;
       }
 
-      state = ApplicationObject.State.Created;
+      SetInstanceParameters(revitZone, speckleZone);
+
+      var getSameNameZone = GetZoneWithSameName(speckleZone);
+
+      state = ApplicationObject.State.Updated;
+
+      return revitZone;
+    }
+
+    private DB.Zone GetExistingZone(RevitZone speckleZone)
+    {
+      return GetExistingElementByApplicationId(speckleZone.applicationId) as DB.Zone;
+    }
+
+    private DB.Zone GetZoneWithSameName(RevitZone speckleZone)
+    {
+      var docZones = new FilteredElementCollector(Doc).OfClass(typeof(DB.Zone)).ToElements().Cast<DB.Zone>();
+
+      return docZones.FirstOrDefault(x => x.Name == speckleZone.name);
+    }
+
+    public static DB.Zone CreateRevitZone(Level revitZoneLevel, Phase targetPhase, string zoneName = null)
+    {
+      var newZone = Doc.Create.NewZone(revitZoneLevel, targetPhase);
+
+      if (zoneName != null)
+        newZone.Name = zoneName;
+      Doc.Regenerate();
+      return newZone;
+    }
+
+    /// <summary>
+    /// Determines the target phase for a space based on available information.
+    /// </summary>
+    /// <param name="speckleZone">The Space object from the Speckle system.</param>
+    /// <param name="revitSpace">The Space object from the Revit system.</param>
+    /// <returns>The determined target Phase object.</returns>
+    /// <remarks>
+    /// The method tries to determine the target phase based on the following priority:
+    /// 1. Phase from the Speckle space (if it exists).
+    /// 2. Phase from the existing Revit space (if it exists).
+    /// 3. Phase from the active view in Revit.
+    /// </remarks>
+    private static Phase DetermineTargetPhase(RevitZone speckleZone, DB.Zone revitZone)
+    {
+      // Get all phases
+      var phases = Doc.Phases.Cast<Phase>();
+
+      // Determine existing space phase, if any
+      Phase existingZonePhase = null;
+      if (revitZone != null)
+      {
+        string existingZonePhaseName = revitZone.Phase.ToString();
+        existingZonePhase = phases.FirstOrDefault(x => x.Name == existingZonePhaseName);
+      }
+
+      // Determine active view phase
+      string activeViewPhaseName = Doc.ActiveView.get_Parameter(BuiltInParameter.VIEW_PHASE).AsValueString();
+      var activeViewPhase = phases.FirstOrDefault(x => x.Name == activeViewPhaseName);
+
+      // Determine target phase
+      // Priority: speckleSpace phase > existing space phase > active view phase
+      string targetPhaseName = speckleZone.phaseName;
+      var targetPhase = phases.FirstOrDefault(x => x.Name == targetPhaseName) ?? existingZonePhase ?? activeViewPhase;
+
+      return targetPhase;
+    }
+
+    /// <summary>
+    /// Handles the Revit Zone based on the provided Speckle Space and target phase.
+    /// </summary>
+    /// <param name="speckleSpace">The Space object from the Speckle system.</param>
+    /// <param name="revitZone">The existing Revit Zone object. This may be modified by the method.</param>
+    /// <param name="targetPhase">The target Phase object.</param>
+    /// <param name="level">The Level object associated with the space.</param>
+    /// <returns>The modified or newly created Revit Zone object.</returns>
+    private DB.Zone CreateRevitZoneIfNeeded(Space speckleSpace, DB.Zone revitZone, Phase targetPhase, Level level)
+    {
+      var zoneName = speckleSpace.zone != null ? speckleSpace.zone.name : speckleSpace.zoneName; // zoneName is the previous property retained here for backwards compatibility.
+
+      if (revitZone == null && !string.IsNullOrEmpty(zoneName))
+      {
+        revitZone = ConvertZoneToRevit(speckleSpace.zone, out _);
+      }
+      else if (revitZone != null && revitZone.Phase.Name != targetPhase.Name)
+      {
+        Doc.Delete(revitZone.Id);
+
+        revitZone = string.IsNullOrEmpty(speckleSpace.zone.name)
+          ? ConvertZoneToRevit(speckleSpace.zone, out _)
+          : CreateRevitZone(level, targetPhase);
+
+        revitZone.Name = speckleSpace.zone.name;
+      }
 
       return revitZone;
     }
@@ -94,6 +166,11 @@ namespace Objects.Converter.Revit
         perimeter = GetParamValue<double>(revitZone, BuiltInParameter.ZONE_PERIMETER),
         serviceType = revitZone.ServiceType.ToString()
       };
+
+      var level = revitZone.Spaces.Cast<DB.Space>().Select(x => x.Level).First();
+
+      if (level != null)
+        speckleZone.level = LevelToSpeckle(level);
 
       GetAllRevitParamsAndIds(speckleZone, revitZone);
 
