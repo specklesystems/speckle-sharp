91074b82294fad6e0af8bab77547e222fd77a919
connorivy <43247197+connorivy@users.noreply.github.com>
2023-09-11 07:32:18 -0500

    Fix(Revit) : dont bake standalone mullions or panels (#2902)
    dont bake mullions
    
    Co-authored-by: Connor Ivy <connor@speckle.systems>

---
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Extensions/CategoryExtensions.cs
@@ -0,0 +1,16 @@
+using Autodesk.Revit.DB;
+
+namespace ConverterRevitShared.Extensions
+{
+  internal static class CategoryExtensions
+  {
+    public static bool EqualsBuiltInCategory(this Category category, BuiltInCategory builtInCategory)
+    {
+# if REVIT2020 || REVIT2021 || REVIT2022
+      return category.Id.IntegerValue == (int)builtInCategory;
+#else
+      return category.BuiltInCategory == builtInCategory;
+#endif
+    }
+  }
+}
--- Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertFamilyInstance.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Partial Classes/ConvertFamilyInstance.cs
@@ -4,6 +4,7 @@ using System.DoubleNumerics;
 using System.Linq;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Structure;
+using ConverterRevitShared.Extensions;
 using Objects.BuiltElements.Revit;
 using Objects.Organization;
 using RevitSharedResources.Helpers;
@@ -513,6 +514,13 @@ namespace Objects.Converter.Revit
       if (familySymbol == null)
       {
         appObj.Update(status: ApplicationObject.State.Failed);
+        return appObj;
+      }
+
+      if (familySymbol.Category.EqualsBuiltInCategory(BuiltInCategory.OST_CurtainWallMullions)
+        || familySymbol.Category.EqualsBuiltInCategory(BuiltInCategory.OST_CurtainWallPanels))
+      {
+        appObj.Update(logItem: "Revit cannot create standalone curtain panels or mullions", status: ApplicationObject.State.Skipped);
         return appObj;
       }
 
