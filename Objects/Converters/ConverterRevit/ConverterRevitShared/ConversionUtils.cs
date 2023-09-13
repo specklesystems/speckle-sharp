04ff91ac7e669f0f8564cc3bf72f960555818482
Claire Kuang <kuang.claire@gmail.com>
2023-09-13 10:45:36 +0100

    Update ConversionUtils.cs

--- Objects/Converters/ConverterRevit/ConverterRevitShared/ConversionUtils.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/ConversionUtils.cs
@@ -38,21 +38,21 @@ namespace Objects.Converter.Revit
       if (nestedElements == null)
         return;
 
-      bool toSkip = skipCategories is null;
       foreach (var obj in GraphTraversal.TraverseMember(nestedElements))
       {
+        bool toSkip = skipCategories is null;
         if (!toSkip)
         {
           var category = obj["category"] as string;
           if (!string.IsNullOrEmpty(category))
             toSkip = skipCategories.Contains(category);
         }
-        
+
         if (toSkip) // create the application object with status = skipped and log to reports
         {
           var nestedAppObj = Report.ReportObjects.TryGetValue(obj.id, out ApplicationObject value)
-           ? value
-           : new ApplicationObject(obj.id, obj.speckle_type);
+            ? value
+            : new ApplicationObject(obj.id, obj.speckle_type);
           nestedAppObj.Status = ApplicationObject.State.Skipped;
           Report.Log(nestedAppObj);
         }
@@ -320,29 +320,32 @@ namespace Objects.Converter.Revit
       // TODO : could add some generic getOrAdd overloads to avoid creating closures
       var paramData = revitDocumentAggregateCache
         .GetOrInitializeEmptyCacheOfType<ParameterToSpeckleData>(out _)
-        .GetOrAdd(paramInternalName, () =>
-        {
-          var definition = rp.Definition;
-          var newParamData = new ParameterToSpeckleData()
+        .GetOrAdd(
+          paramInternalName,
+          () =>
           {
-            Definition = definition,
-            InternalName = paramInternalName,
-            IsReadOnly = rp.IsReadOnly,
-            IsShared = rp.IsShared,
-            IsTypeParameter = isTypeParameter,
-            Name = definition.Name,
-            UnitType = definition.GetUnityTypeString(),
-          };
-          if (rp.StorageType == StorageType.Double)
-          {
-            unitTypeId = rp.GetUnitTypeId();
-            newParamData.UnitsSymbol = GetSymbolUnit(rp, definition, unitTypeId);
-            newParamData.ApplicationUnits = unitsOverride != null
-              ? UnitsToNative(unitsOverride).ToUniqueString()
-              : unitTypeId.ToUniqueString();
-          }
-          return newParamData;
-        }, out _);
+            var definition = rp.Definition;
+            var newParamData = new ParameterToSpeckleData()
+            {
+              Definition = definition,
+              InternalName = paramInternalName,
+              IsReadOnly = rp.IsReadOnly,
+              IsShared = rp.IsShared,
+              IsTypeParameter = isTypeParameter,
+              Name = definition.Name,
+              UnitType = definition.GetUnityTypeString(),
+            };
+            if (rp.StorageType == StorageType.Double)
+            {
+              unitTypeId = rp.GetUnitTypeId();
+              newParamData.UnitsSymbol = GetSymbolUnit(rp, definition, unitTypeId);
+              newParamData.ApplicationUnits =
+                unitsOverride != null ? UnitsToNative(unitsOverride).ToUniqueString() : unitTypeId.ToUniqueString();
+            }
+            return newParamData;
+          },
+          out _
+        );
 
       return paramData.GetParameterObjectWithValue(rp.GetValue(paramData.Definition, unitTypeId));
     }
@@ -357,9 +360,7 @@ namespace Objects.Converter.Revit
     /// <param name="cache"></param>
     /// <param name="forgeTypeId"></param>
     /// <returns></returns>
-    public string GetSymbolUnit(
-      DB.Parameter parameter,
-      DB.Definition definition,
+    public string GetSymbolUnit(DB.Parameter parameter, DB.Definition definition,
 #if REVIT2020
       DisplayUnitType unitTypeId
 #else
@@ -620,7 +621,7 @@ namespace Objects.Converter.Revit
 
       var cachedIds = PreviouslyReceivedObjectIds?.GetCreatedIdsFromConvertedId(applicationId);
       // TODO: we may not want just the first one
-      return  cachedIds == null ? null : Doc.GetElement(cachedIds.First());
+      return cachedIds == null ? null : Doc.GetElement(cachedIds.First());
     }
 
     public IEnumerable<DB.Element?> GetExistingElementsByApplicationId(string applicationId)
@@ -629,7 +630,8 @@ namespace Objects.Converter.Revit
         yield break;
 
       var cachedIds = PreviouslyReceivedObjectIds?.GetCreatedIdsFromConvertedId(applicationId);
-      if (cachedIds == null) yield break;
+      if (cachedIds == null)
+        yield break;
       foreach (var id in cachedIds)
         yield return Doc.GetElement(id);
     }
