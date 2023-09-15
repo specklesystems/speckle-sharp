8bf31bc3453f69fb3d44515dfe7aa9f5fc08eb44
oguzhankoral <oguzhankoral@gmail.com>
2023-09-15 11:18:05 +0300

    Notify UI after received

--- DesktopUI3/DUI3/Bindings/IReceiveBinding.cs
+++ DesktopUI3/DUI3/Bindings/IReceiveBinding.cs
@@ -22,9 +22,25 @@ namespace DUI3.Bindings
   {
     public static readonly string ReceiversExpired = "receiversExpired";
     public static readonly string ReceiverProgress = "receiverProgress";
+    public static readonly string Notify = "notify";
   }
 
   public class ReceiverModelCard : ModelCard
   {
+  }
+
+  public class ToastInfo
+  {
+    public string ModelCardId { get; set; }
+    public string Text { get; set; }
+    public string Level { get; set; }
+    public ToastAction Action { get; set; }
+    public int Timeout { get; set; }
+  }
+
+  public class ToastAction
+  {
+    public string Url { get; set; }
+    public string Name { get; set; }
   }
 }
