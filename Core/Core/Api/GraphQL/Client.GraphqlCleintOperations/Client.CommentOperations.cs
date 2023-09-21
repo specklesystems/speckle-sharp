aa9587cea443e0439b075440858fda4448c1dc34
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2023-09-21 11:52:57 +0100

    chore(core): Make client functions consistent with cancellation token position (#2925)
    * chore(core): Deprecated client functions that take cancellation token last
    
    * Changed usages to non-deprecated overload

--- Core/Core/Api/GraphQL/Client.GraphqlCleintOperations/Client.CommentOperations.cs
+++ Core/Core/Api/GraphQL/Client.GraphqlCleintOperations/Client.CommentOperations.cs
@@ -3,7 +3,6 @@
 using System.Threading;
 using System.Threading.Tasks;
 using GraphQL;
-using Speckle.Core.Logging;
 
 namespace Speckle.Core.Api;
 
@@ -15,26 +14,13 @@ public partial class Client
   /// <param name="streamId">Id of the stream to get the comments from</param>
   /// <param name="limit">The number of comments to get</param>
   /// <param name="cursor">Time to filter the comments with</param>
-  /// <returns></returns>
-  public Task<Comments> StreamGetComments(string streamId, int limit = 25, string? cursor = null)
-  {
-    return StreamGetComments(CancellationToken.None, streamId, limit, cursor);
-  }
-
-  /// <summary>
-  ///  Gets the comments on a Stream
-  /// </summary>
   /// <param name="cancellationToken"></param>
-  /// <param name="streamId">Id of the stream to get the comments from</param>
-  /// <param name="limit">The number of comments to get</param>
-  /// <param name="cursor">Time to filter the comments with</param>
   /// <returns></returns>
-  /// <exception cref="SpeckleException"></exception>
   public async Task<Comments> StreamGetComments(
-    CancellationToken cancellationToken,
     string streamId,
     int limit = 25,
-    string? cursor = null
+    string? cursor = null,
+    CancellationToken cancellationToken = default
   )
   {
     var request = new GraphQLRequest
@@ -88,25 +74,17 @@ public partial class Client
   }
 
   /// <summary>
-  ///  Gets the screenshot of a Comment
+  /// Gets the screenshot of a Comment
   /// </summary>
   /// <param name="id">Id of the comment</param>
   /// <param name="streamId">Id of the stream to get the comment from</param>
-  /// <returns></returns>
-  public Task<string> StreamGetCommentScreenshot(string id, string streamId)
-  {
-    return StreamGetCommentScreenshot(CancellationToken.None, id, streamId);
-  }
-
-  /// <summary>
-  ///  Gets the screenshot of a Comment
-  /// </summary>
   /// <param name="cancellationToken"></param>
-  /// <param name="id">Id of the comment</param>
-  /// <param name="streamId">Id of the stream to get the comment from</param>
   /// <returns></returns>
-  /// <exception cref="SpeckleException"></exception>
-  public async Task<string> StreamGetCommentScreenshot(CancellationToken cancellationToken, string id, string streamId)
+  public async Task<string> StreamGetCommentScreenshot(
+    string id,
+    string streamId,
+    CancellationToken cancellationToken = default
+  )
   {
     var request = new GraphQLRequest
     {
