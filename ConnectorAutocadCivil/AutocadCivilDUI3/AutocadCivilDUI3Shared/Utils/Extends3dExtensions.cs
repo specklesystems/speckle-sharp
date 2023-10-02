5a6af5d2df502d41e93085f05866ae49ce8ebe53
oguzhankoral <oguzhankoral@gmail.com>
2023-10-02 13:18:06 +0200

    Extension and utility functions for autocad editor operations

---
+++ ConnectorAutocadCivil/AutocadCivilDUI3/AutocadCivilDUI3Shared/Utils/Extends3dExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace AutocadCivilDUI3Shared.Utils;
+
+public static class Extends3dExtensions
+{
+  public static Extents3d FromObjectIds(Editor editor, IReadOnlyCollection<ObjectId> objectIds)
+  {
+    Transaction tr = editor.Document.Database.TransactionManager.StartTransaction();
+    
+    PromptSelectionResult selectionResult = editor.SelectImplied();
+    
+    if (selectionResult.Status == PromptStatus.OK)
+    {
+      SelectionSet selectionSet = selectionResult.Value;
+
+      if (selectionSet.Count > 0)
+      {
+        // Create a bounding box to include all selected objects
+        Extents3d selectedExtents = new();
+
+        foreach (ObjectId objectId in selectionSet.GetObjectIds())
+        {
+          Entity entity = tr.GetObject(objectId, OpenMode.ForRead) as Entity;
+          if (entity != null)
+          {
+            selectedExtents.AddExtents(entity.GeometricExtents);
+          }
+        }
+        tr.Commit();
+        return selectedExtents;
+      }
+    }
+    
+    tr.Commit();
+    return new Extents3d();
+  }
+}
