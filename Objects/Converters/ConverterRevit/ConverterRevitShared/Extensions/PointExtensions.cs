3b78a2cd98c6418c672c88f5f677e95edb49de4d
connorivy <43247197+connorivy@users.noreply.github.com>
2023-11-09 09:53:45 -0600

    Fix(Revit) : Send area outlines separate from openings (#2996)
    * working mostly with outline builder
    
    * only convert "largest" area
    
    * better code comments
    
    * remove unused usings
    
    * fixes per ian's comments
    
    * nest instead of continue
    
    * more adjustments per comments
    
    * create polyline extensions to enumerateAsLines
    
    ---------
    
    Co-authored-by: Connor Ivy <connor@speckle.systems>

---
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/Extensions/PointExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+using Objects.Geometry;
+
+namespace ConverterRevitShared.Extensions
+{
+  internal static class PointExtensions
+  {
+    public static bool IsOnLineBetweenPoints(this Point middle, Point p0, Point p1, double tolerance = .01)
+    {
+      Vector outerPointsDirection = new(p1 - p0);
+      Vector middleToStartPointsDirection = new(middle - p0);
+
+      Vector crossProduct = Vector.CrossProduct(outerPointsDirection, middleToStartPointsDirection);
+
+      // Check if the cross product vector is within tolerance
+      return Math.Abs(crossProduct.x) < tolerance
+        && Math.Abs(crossProduct.y) < tolerance
+        && Math.Abs(crossProduct.z) < tolerance;
+    }
+  }
+}
