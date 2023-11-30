939d26448d2ebb8c83cecb0dcd899fea22df61ef
Jonathon Broughton <jonathon@stardotbmp.com>
2023-11-30 13:02:56 +0000

    dang missed one

--- ConnectorNavisworks/ConnectorNavisworks/Entry/Ribbon.xaml.cs
+++ ConnectorNavisworks/ConnectorNavisworks/Entry/Ribbon.xaml.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Text;
 using System.Windows.Forms;
 using Autodesk.Navisworks.Api.Plugins;
 using Speckle.ConnectorNavisworks.Bindings;
