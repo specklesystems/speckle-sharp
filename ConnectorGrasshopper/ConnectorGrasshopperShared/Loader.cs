aad5bc836e8e45212fef0d7b6179e738ae09235c
Alan Rynne <alan@speckle.systems>
2024-01-09 18:58:11 +0100

    fix(rhino/gh): Disable `EnableWindowsTargeting` and removed unnecessary using

--- ConnectorGrasshopper/ConnectorGrasshopperShared/Loader.cs
+++ ConnectorGrasshopper/ConnectorGrasshopperShared/Loader.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ConnectorGrasshopper.Extras;
