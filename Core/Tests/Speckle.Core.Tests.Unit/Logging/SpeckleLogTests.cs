7dfdecc4312bbdbf7126c2db9bc86ba6ab79ebe6
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-01-18 12:45:56 +0000

    Logger unit tests

---
+++ Core/Tests/Speckle.Core.Tests.Unit/Logging/SpeckleLogTests.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
+using Speckle.Core.Logging;
+
+namespace Speckle.Core.Tests.Unit.Logging;
+
+[TestOf(typeof(SpeckleLog))]
+public class SpeckleLogTests : IDisposable
+{
+  private StringWriter _stdOut;
+  private Logger _sut;
+
+  [SetUp]
+  public virtual void Setup()
+  {
+    _sut = SpeckleLog.CreateConfiguredLogger(nameof(SpeckleLogTests), null, SetUp.TestLogConfig);
+    _stdOut = new StringWriter();
+    Console.SetOut(_stdOut);
+  }
+
+  [TearDown]
+  public void TearDown()
+  {
+    _stdOut?.Dispose();
+    _sut?.Dispose();
+  }
+
+  [Test]
+  [TestCase(LogEventLevel.Fatal, true)]
+  [TestCase(LogEventLevel.Error, true)]
+  [TestCase(LogEventLevel.Warning, true)]
+  [TestCase(LogEventLevel.Information, true)]
+  [TestCase(LogEventLevel.Debug, true)]
+  [TestCase(LogEventLevel.Verbose, false)]
+  public void LoggerWrites_WithLogEventLevel(LogEventLevel logLevel, bool expectLog)
+  {
+    const string TEMPLATE = "My log message";
+    _sut.Write(logLevel, TEMPLATE);
+
+    string result = _stdOut.ToString();
+
+    if (expectLog)
+    {
+      Assert.That(result, Contains.Substring(TEMPLATE));
+    }
+    else
+    {
+      Assert.That(result, Is.Empty);
+    }
+  }
+
+  [Test]
+  public void LoggerWrites_PositionalProperties()
+  {
+    const string PROP_NAME = "myProp";
+    const string TEMPLATE = $"My log message with positional prop {{{PROP_NAME}}}";
+    const string TARGET_VALUE = "my amazing value";
+    SpeckleLog.Logger.Warning(TEMPLATE, TARGET_VALUE);
+
+    string result = _stdOut.ToString();
+    Assert.That(result, Does.Contain(TARGET_VALUE));
+    Assert.That(result, Does.Not.Contain(PROP_NAME));
+  }
+
+  [Test]
+  public void LoggerWrites_ContextProperties()
+  {
+    const string PROP_NAME = "myProp";
+    const string TEMPLATE = $"My log message with context prop {{{PROP_NAME}}}";
+    const string TARGET_VALUE = "my amazing value";
+
+    SpeckleLog.Logger.ForContext(PROP_NAME, TARGET_VALUE).Warning(TEMPLATE);
+
+    string result = _stdOut.ToString();
+    Assert.That(result, Does.Contain(TARGET_VALUE));
+    Assert.That(result, Does.Not.Contain(PROP_NAME));
+  }
+
+  [Test]
+  public void LoggerWrites_ScopedProperties()
+  {
+    const string PROP_NAME = "myProp";
+    const string TEMPLATE = $"My log message with scoped prop {{{PROP_NAME}}}";
+    const string TARGET_VALUE = "my amazing value";
+
+    using var d1 = LogContext.PushProperty(PROP_NAME, TARGET_VALUE);
+
+    SpeckleLog.Logger.Warning(TEMPLATE);
+
+    string result = _stdOut.ToString();
+    Assert.That(result, Does.Contain(TARGET_VALUE));
+    Assert.That(result, Does.Not.Contain(PROP_NAME));
+  }
+
+  [Test]
+  [TestCase(true)]
+  [TestCase(false)]
+  public void CreateConfiguredLogger_WritesToConsole_ToConsole(bool shouldWrite)
+  {
+    const string TEST_MESSAGE = "This is my test message";
+
+    SpeckleLogConfiguration config =
+      new(logToConsole: shouldWrite, logToSeq: false, logToSentry: false, logToFile: false);
+    using var logger = SpeckleLog.CreateConfiguredLogger("My Test Host App!!", null, config);
+
+    logger.Fatal(TEST_MESSAGE);
+
+    string result = _stdOut.ToString();
+    if (shouldWrite)
+    {
+      Assert.That(result, Does.Contain(TEST_MESSAGE));
+    }
+    else
+    {
+      Assert.That(result, Is.Empty);
+    }
+  }
+
+  public void Dispose()
+  {
+    _stdOut?.Dispose();
+    _sut?.Dispose();
+  }
+}
