ddec609485f2981d1026abd4bfd4ba2200dc9d0c
Alan Rynne <alan@speckle.systems>
2024-03-21 11:38:16 +0100

    fix: Formatting

--- DUI3-DX/Sdk/Speckle.Converters.Common/NameAndRankValueAttribute.cs
+++ DUI3-DX/Sdk/Speckle.Converters.Common/NameAndRankValueAttribute.cs
@@ -6,9 +6,9 @@ namespace Speckle.Converters.Common;
 [AttributeUsage(AttributeTargets.Class)]
 public sealed class NameAndRankValueAttribute : Attribute
 {
-  // DO NOT CHANGE! This is the base, lowest rank for a conversion 
+  // DO NOT CHANGE! This is the base, lowest rank for a conversion
   public const int SPECKLE_DEFAULT_RANK = 0;
-  
+
   public string Name { get; private set; }
   public int Rank { get; private set; }
 
