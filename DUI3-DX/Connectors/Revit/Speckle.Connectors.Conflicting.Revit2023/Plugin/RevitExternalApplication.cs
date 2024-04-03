07107ab88138e1ab05523dc85a7880de0df90034
Connor Ivy <connor@speckle.systems>
2024-04-03 13:14:03 -0500

    add conflict project

---
+++ DUI3-DX/Connectors/Revit/Speckle.Connectors.Conflicting.Revit2023/Plugin/RevitExternalApplication.cs
@@ -0,0 +1,66 @@
+using System;
+using Autodesk.Revit.UI;
+using System.Reflection;
+using System.IO;
+using Serilog.Events;
+using System.Diagnostics;
+
+namespace Speckle.Connectors.Conflicting.Revit2023.Plugin;
+
+internal class RevitExternalApplication : IExternalApplication
+{
+  public RevitExternalApplication() { }
+
+  public Result OnStartup(UIControlledApplication application)
+  {
+    try
+    {
+      // POC: not sure what this is doing...  could be messing up our Aliasing????
+      AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+
+      var x = new Objects.BuiltElements.Revit.FamilyInstance();
+      var y = new Speckle.Core.Serialisation.BaseObjectSerializer();
+
+      var plugin = new RevitPlugin(application);
+      plugin.Initialise();
+    }
+    catch (Exception e)
+    {
+      return Result.Failed;
+    }
+
+    return Result.Succeeded;
+  }
+
+  public Result OnShutdown(UIControlledApplication application)
+  {
+    try { }
+    catch (Exception e)
+    {
+      // POC: feedback?
+      return Result.Failed;
+    }
+
+    return Result.Succeeded;
+  }
+
+  private Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+  {
+    // POC: tight binding to files
+    Assembly assembly = null;
+    string name = args.Name.Split(',')[0];
+    string path = Path.GetDirectoryName(typeof(RevitPlugin).Assembly.Location);
+
+    if (path != null)
+    {
+      string assemblyFile = Path.Combine(path, name + ".dll");
+
+      if (File.Exists(assemblyFile))
+      {
+        assembly = Assembly.LoadFrom(assemblyFile);
+      }
+    }
+
+    return assembly;
+  }
+}
---
+++ DUI3-DX/Connectors/Revit/Speckle.Connectors.Conflicting.Revit2023/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Speckle.Connectors.Conflicting.Revit2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
