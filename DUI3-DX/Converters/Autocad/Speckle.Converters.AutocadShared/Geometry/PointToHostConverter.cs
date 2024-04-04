8a007ec6c9d370c642345f9ef9c21be2565b92e7
Claire Kuang <kuang.claire@gmail.com>
2024-04-04 13:54:22 +0100

    Update PointToHostConverter.cs

--- DUI3-DX/Converters/Autocad/Speckle.Converters.AutocadShared/Geometry/PointToHostConverter.cs
+++ DUI3-DX/Converters/Autocad/Speckle.Converters.AutocadShared/Geometry/PointToHostConverter.cs
@@ -7,7 +7,27 @@ using Speckle.Core.Kits;
 namespace Speckle.Converters.Autocad.Geometry;
 
 [NameAndRankValue(nameof(SOG.Point), NameAndRankValueAttribute.SPECKLE_DEFAULT_RANK)]
-public class PointToHostConverter : ISpeckleObjectToHostConversion, IRawConversion<SOG.Point, AG.Point3d>
+public class DBPointToHostConverter : ISpeckleObjectToHostConversion, IRawConversion<SOG.Point, DBPoint>
+{
+  private readonly IConversionContextStack<Document, UnitsValue> _contextStack;
+
+  public DBPointToHostConverter(IConversionContextStack<Document, UnitsValue> contextStack)
+  {
+    _contextStack = contextStack;
+  }
+
+  public object Convert(Base target) => RawConvert((SOG.Point)target);
+
+  public DBPoint RawConvert(SOG.Point target)
+  {
+    double f = Units.GetConversionFactor(target.units, _contextStack.Current.SpeckleUnits);
+    AG.Point3d point = new(target.x * f, target.y * f, target.z * f);
+    return new(point);
+  }
+}
+
+/* POC: enable this when converter registration is resolved
+public class PointToHostConverter : IRawConversion<SOG.Point, AG.Point3d>
 {
   private readonly IConversionContextStack<Document, UnitsValue> _contextStack;
 
@@ -25,4 +45,4 @@ public class PointToHostConverter : ISpeckleObjectToHostConversion, IRawConversi
 
     return point;
   }
-}
+*/
