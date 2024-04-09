a08f714bbd891103e5051f05a02ea58d50aeb093
Alan Rynne <alan@speckle.systems>
2024-04-09 22:54:44 +0200

    chore: Removed unused unit converter

--- DUI3-DX/Converters/Rhino/Speckle.Converters.Rhino7/ToSpeckle/RhinoConverterToSpeckle.cs
+++ DUI3-DX/Converters/Rhino/Speckle.Converters.Rhino7/ToSpeckle/RhinoConverterToSpeckle.cs
@@ -1,24 +1,18 @@
-using Rhino;
 using Rhino.DocObjects;
 using Speckle.Autofac.DependencyInjection;
 using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
 using Speckle.Core.Models;
 
-namespace Speckle.Converters.Rhino7;
+namespace Speckle.Converters.Rhino7.ToSpeckle;
 
 public class RhinoConverterToSpeckle : ISpeckleConverterToSpeckle
 {
   private readonly IFactory<string, IHostObjectToSpeckleConversion> _toSpeckle;
-  private readonly IHostToSpeckleUnitConverter<UnitSystem> _unitConverter;
 
-  public RhinoConverterToSpeckle(
-    IFactory<string, IHostObjectToSpeckleConversion> toSpeckle,
-    IHostToSpeckleUnitConverter<UnitSystem> unitConverter
-  )
+  public RhinoConverterToSpeckle(IFactory<string, IHostObjectToSpeckleConversion> toSpeckle)
   {
     _toSpeckle = toSpeckle;
-    _unitConverter = unitConverter;
   }
 
   public Base Convert(object target)
