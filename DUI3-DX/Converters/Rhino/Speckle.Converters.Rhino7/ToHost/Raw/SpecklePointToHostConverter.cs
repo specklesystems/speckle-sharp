98ad13e7005252e1d6cb4bd9ba82824936440188
Alan Rynne <alan@speckle.systems>
2024-04-09 13:35:03 +0200

    feat: Initial simple raw converters

---
+++ DUI3-DX/Converters/Rhino/Speckle.Converters.Rhino7/ToHost/Raw/SpecklePointToHostConverter.cs
@@ -0,0 +1,14 @@
+﻿using Speckle.Converters.Common.Objects;
+
+namespace Speckle.Converters.Rhino7.ToHost.Raw;
+
+public class SpecklePointToHostConverter : IRawConversion<SOG.Point, RG.Point3d>
+{
+  public SpecklePointToHostConverter() { }
+
+  public RG.Point3d RawConvert(SOG.Point target)
+  {
+    // POC: NO unit conversion
+    return new RG.Point3d(target.x, target.y, target.x);
+  }
+}
---
+++ DUI3-DX/Converters/Rhino/Speckle.Converters.Rhino7/ToHost/SpecklePointToPointObjectConverter.cs
@@ -0,0 +1,18 @@
+﻿using Speckle.Converters.Common;
+using Speckle.Converters.Common.Objects;
+using Speckle.Core.Models;
+
+namespace Speckle.Converters.Rhino7.ToHost;
+
+[NameAndRankValue(nameof(SOG.Point), NameAndRankValueAttribute.SPECKLE_DEFAULT_RANK)]
+public class SpecklePointToPointObjectConverter : ISpeckleObjectToHostConversion
+{
+  private readonly IRawConversion<SOG.Point, RG.Point3d> _pointConverter;
+
+  public SpecklePointToPointObjectConverter(IRawConversion<SOG.Point, RG.Point3d> pointConverter)
+  {
+    _pointConverter = pointConverter;
+  }
+
+  public object Convert(Base target) => throw new NotImplementedException();
+}
