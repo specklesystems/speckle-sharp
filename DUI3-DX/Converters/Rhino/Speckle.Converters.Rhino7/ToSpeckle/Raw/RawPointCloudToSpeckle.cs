a02831f062c2c875ae48b2fe260e3b7a10dbeeaa
Alan Rynne <alan@speckle.systems>
2024-04-15 12:43:39 +0200

    feat: Added PointCloud to speckle conversion

---
+++ DUI3-DX/Converters/Rhino/Speckle.Converters.Rhino7/ToSpeckle/Raw/RawPointCloudToSpeckle.cs
@@ -0,0 +1,29 @@
+﻿using Rhino;
+using Speckle.Converters.Common;
+using Speckle.Converters.Common.Objects;
+
+namespace Speckle.Converters.Rhino7.ToSpeckle.Raw;
+
+public class RawPointCloudToSpeckle : IRawConversion<RG.PointCloud, SOG.Pointcloud>
+{
+  private readonly IConversionContextStack<RhinoDoc, UnitSystem> _contextStack;
+  private readonly IRawConversion<RG.Box, SOG.Box> _boxConverter;
+
+  public RawPointCloudToSpeckle(
+    IConversionContextStack<RhinoDoc, UnitSystem> contextStack,
+    IRawConversion<RG.Box, SOG.Box> boxConverter
+  )
+  {
+    _contextStack = contextStack;
+    _boxConverter = boxConverter;
+  }
+
+  public SOG.Pointcloud RawConvert(RG.PointCloud target) =>
+    new()
+    {
+      points = target.GetPoints().SelectMany(pt => new[] { pt.X, pt.Y, pt.Z }).ToList(),
+      colors = target.GetColors().Select(o => o.ToArgb()).ToList(),
+      bbox = _boxConverter.RawConvert(new RG.Box(target.GetBoundingBox(true))),
+      units = _contextStack.Current.SpeckleUnits
+    };
+}
