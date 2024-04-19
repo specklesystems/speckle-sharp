02d9cf1fb4a4ee7e10c7831c83e2c0214c5a719b
oguzhankoral <oguzhankoral@gmail.com>
2024-04-19 10:14:27 +0300

    Use Location for Assembly

--- DUI3-DX/Connectors/Autocad/Speckle.Connectors.AutocadShared/HostApp/AutocadSettings.cs
+++ DUI3-DX/Connectors/Autocad/Speckle.Connectors.AutocadShared/HostApp/AutocadSettings.cs
@@ -9,7 +9,7 @@ public class AutocadSettings
   {
     HostAppInfo = hostAppInfo;
     HostAppVersion = hostAppVersion;
-    Modules = new[] { new DirectoryInfo(typeof(AutocadSettings).Assembly.ToString()).Parent.FullName };
+    Modules = new[] { new DirectoryInfo(typeof(AutocadSettings).Assembly.Location).Parent.FullName };
   }
 
   public HostApplication HostAppInfo { get; private set; }
