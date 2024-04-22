cfe1e9d863db1b935b18f5ca53acfd1af0da7ef1
oguzhankoral <oguzhankoral@gmail.com>
2024-04-23 02:25:53 +0300

    Fix null errors

--- DUI3-DX/Converters/Autocad/Speckle.Converters.AutocadShared/ToSpeckle/Geometry/Polyline2dToSpeckleConverter.cs
+++ DUI3-DX/Converters/Autocad/Speckle.Converters.AutocadShared/ToSpeckle/Geometry/Polyline2dToSpeckleConverter.cs
@@ -1,47 +1,42 @@
 using System.Collections.Generic;
-using Autodesk.AutoCAD.DatabaseServices;
 using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
 using Speckle.Core.Models;
 using Speckle.Converters.Autocad.Extensions;
 using System.Linq;
-using Autodesk.AutoCAD.Geometry;
 
 namespace Speckle.Converters.Autocad.Geometry;
 
 /// <summary>
-/// The <see cref="Polyline2d"/> class converter. Converts to <see cref="SOG.Autocad.AutocadPolycurve"/>.
-/// <see cref="Polyline2d"/> of type <see cref="Poly2dType.SimplePoly"/> will be converted as <see cref="Polyline"/>.
+/// The <see cref="ADB.Polyline2d"/> class converter. Converts to <see cref="SOG.Autocad.AutocadPolycurve"/>.
+/// <see cref="ADB.Polyline2d"/> of type <see cref="ADB.Poly2dType.SimplePoly"/> will be converted as <see cref="ADB.Polyline"/>.
 /// </summary>
 /// <remarks>
-/// <see cref="Polyline2d"/> of type <see cref="Poly2dType.SimplePoly"/> will have only <see cref="SOG.Line"/>s and <see cref="SOG.Arc"/>s in <see cref="SOG.Polycurve.segments"/>.
-/// <see cref="Polyline2d"/> of type <see cref="Poly2dType.FitCurvePoly"/>, <see cref="Poly2dType.CubicSplinePoly"/> and <see cref="Poly2dType.QuadSplinePoly"/> will have only one <see cref="SOG.Curve"/> in <see cref="SOG.Polycurve.segments"/>.
-/// The IHostObjectToSpeckleConversion inheritance should only expect database-resident <see cref="Polyline2d"/> objects. IRawConversion inheritance can expect non database-resident objects, when generated from other converters.
+/// <see cref="ADB.Polyline2d"/> of type <see cref="ADB.Poly2dType.SimplePoly"/> will have only <see cref="SOG.Line"/>s and <see cref="SOG.Arc"/>s in <see cref="SOG.Polycurve.segments"/>.
+/// <see cref="ADB.Polyline2d"/> of type <see cref="ADB.Poly2dType.FitCurvePoly"/>, <see cref="ADB.Poly2dType.CubicSplinePoly"/> and <see cref="ADB.Poly2dType.QuadSplinePoly"/> will have only one <see cref="SOG.Curve"/> in <see cref="SOG.Polycurve.segments"/>.
+/// The IHostObjectToSpeckleConversion inheritance should only expect database-resident <see cref="ADB.Polyline2d"/> objects. IRawConversion inheritance can expect non database-resident objects, when generated from other converters.
 /// </remarks>
 [NameAndRankValue(nameof(ADB.Polyline2d), NameAndRankValueAttribute.SPECKLE_DEFAULT_RANK)]
 public class Polyline2dToSpeckleConverter : IHostObjectToSpeckleConversion
 {
   private readonly IRawConversion<ADB.Arc, SOG.Arc> _arcConverter;
   private readonly IRawConversion<ADB.Line, SOG.Line> _lineConverter;
-  private readonly IRawConversion<ADB.Polyline, SOG.Autocad.AutocadPolycurve> _polylineConverter;
   private readonly IRawConversion<ADB.Spline, SOG.Curve> _splineConverter;
   private readonly IRawConversion<AG.Plane, SOG.Plane> _planeConverter;
-  private readonly IRawConversion<Extents3d, SOG.Box> _boxConverter;
-  private readonly IConversionContextStack<Document, UnitsValue> _contextStack;
+  private readonly IRawConversion<ADB.Extents3d, SOG.Box> _boxConverter;
+  private readonly IConversionContextStack<Document, ADB.UnitsValue> _contextStack;
 
   public Polyline2dToSpeckleConverter(
     IRawConversion<ADB.Arc, SOG.Arc> arcConverter,
     IRawConversion<ADB.Line, SOG.Line> lineConverter,
-    IRawConversion<ADB.Polyline, SOG.Autocad.AutocadPolycurve> polylineConverter,
     IRawConversion<ADB.Spline, SOG.Curve> splineConverter,
     IRawConversion<AG.Plane, SOG.Plane> planeConverter,
-    IRawConversion<Extents3d, SOG.Box> boxConverter,
-    IConversionContextStack<Document, UnitsValue> contextStack
+    IRawConversion<ADB.Extents3d, SOG.Box> boxConverter,
+    IConversionContextStack<Document, ADB.UnitsValue> contextStack
   )
   {
     _arcConverter = arcConverter;
     _lineConverter = lineConverter;
-    _polylineConverter = polylineConverter;
     _splineConverter = splineConverter;
     _planeConverter = planeConverter;
     _boxConverter = boxConverter;
@@ -67,16 +62,16 @@ public class Polyline2dToSpeckleConverter : IHostObjectToSpeckleConversion
     var polyType = SOG.Autocad.AutocadPolyType.Unknown;
     switch (target.PolyType)
     {
-      case Poly2dType.SimplePoly:
+      case ADB.Poly2dType.SimplePoly:
         polyType = SOG.Autocad.AutocadPolyType.Simple2d;
         break;
-      case Poly2dType.FitCurvePoly:
+      case ADB.Poly2dType.FitCurvePoly:
         polyType = SOG.Autocad.AutocadPolyType.FitCurve2d;
         break;
-      case Poly2dType.CubicSplinePoly:
+      case ADB.Poly2dType.CubicSplinePoly:
         polyType = SOG.Autocad.AutocadPolyType.CubicSpline2d;
         break;
-      case Poly2dType.QuadSplinePoly:
+      case ADB.Poly2dType.QuadSplinePoly:
         polyType = SOG.Autocad.AutocadPolyType.QuadSpline2d;
         break;
     }
@@ -85,13 +80,16 @@ public class Polyline2dToSpeckleConverter : IHostObjectToSpeckleConversion
     List<double> value = new();
     List<double> bulges = new();
     List<double> tangents = new();
-    List<Vertex2d> vertices = target
-      .GetSubEntities<Vertex2d>(OpenMode.ForRead, _contextStack.Current.Document.TransactionManager.TopTransaction)
+    List<ADB.Vertex2d> vertices = target
+      .GetSubEntities<ADB.Vertex2d>(
+        ADB.OpenMode.ForRead,
+        _contextStack.Current.Document.TransactionManager.TopTransaction
+      )
       .ToList();
 
     for (int i = 0; i < vertices.Count; i++)
     {
-      Vertex2d vertex = vertices[i];
+      ADB.Vertex2d vertex = vertices[i];
 
       // get vertex value in the Global Coordinate System (GCS).
       value.AddRange(vertex.Position.ToArray());
@@ -116,27 +114,35 @@ public class Polyline2dToSpeckleConverter : IHostObjectToSpeckleConversion
     List<Objects.ICurve> segments = new();
 
     // POC: retrieve spline display value here for database-resident polylines by connecting all vertex points
-    SOG.Polyline displayValue;
     if (target.Database is not null)
     {
-      var exploded = new DBObjectCollection();
+      var exploded = new ADB.DBObjectCollection();
       target.Explode(exploded);
-      Point3d previousPoint = new();
+      AG.Point3d previousPoint = new();
       for (int i = 0; i < exploded.Count; i++)
       {
         var segment = exploded[i] as ADB.Curve;
+
+        if (segment == null)
+        {
+          continue;
+        }
 
         if (i == 0 && exploded.Count > 1)
         {
           // get the connection point to the next segment - this is necessary since imported polycurves might have segments in different directions
           var nextSegment = exploded[i + 1] as ADB.Curve;
-          Point3d connectionPoint =
+          if (nextSegment == null)
+          {
+            continue;
+          }
+          AG.Point3d connectionPoint =
             nextSegment.StartPoint.IsEqualTo(segment.StartPoint) || nextSegment.StartPoint.IsEqualTo(segment.EndPoint)
               ? nextSegment.StartPoint
               : nextSegment.EndPoint;
 
           previousPoint = connectionPoint;
-          segment = GetCorrectSegmentDirection(segment, connectionPoint, true, out Point3d otherPoint);
+          segment = GetCorrectSegmentDirection(segment, connectionPoint, true, out AG.Point3d _);
         }
         else
         {
@@ -187,9 +193,9 @@ public class Polyline2dToSpeckleConverter : IHostObjectToSpeckleConversion
 
   private ADB.Curve GetCorrectSegmentDirection(
     ADB.Curve segment,
-    Point3d connectionPoint,
+    AG.Point3d connectionPoint,
     bool isFirstSegment,
-    out Point3d nextPoint
+    out AG.Point3d nextPoint
   ) // note sometimes curve3d may not have endpoints
   {
     nextPoint = segment.EndPoint;
@@ -199,7 +205,7 @@ public class Polyline2dToSpeckleConverter : IHostObjectToSpeckleConversion
       return segment;
     }
 
-    bool reverseDirection = false;
+    bool reverseDirection;
     if (isFirstSegment)
     {
       reverseDirection = segment.StartPoint.IsEqualTo(connectionPoint);
