755ab82acc4649b912c7ad0d4fd06892e9b2a6dd
Alan Rynne <alan@speckle.systems>
2024-04-23 13:41:29 +0200

    feat(rvt): Adds DirectShape conversion to Speckle

---
+++ DUI3-DX/Converters/Revit/Speckle.Converters.RevitShared/Raw/Geometry/SingleMeshConversionToSpeckle.cs
@@ -0,0 +1,84 @@
+﻿using Objects.Other;
+using Speckle.Converters.Common.Objects;
+using Speckle.Converters.RevitShared.Helpers;
+
+namespace Speckle.Converters.RevitShared.ToSpeckle;
+
+public class SingleMeshConversionToSpeckle : IRawConversion<DB.Mesh, SOG.Mesh>
+{
+  private readonly IRawConversion<DB.XYZ, SOG.Point> _xyzToPointConverter;
+  private readonly IRawConversion<DB.Material, RenderMaterial> _materialConverter;
+  private readonly RevitConversionContextStack _contextStack;
+
+  public SingleMeshConversionToSpeckle(
+    RevitConversionContextStack contextStack,
+    IRawConversion<DB.XYZ, SOG.Point> xyzToPointConverter,
+    IRawConversion<DB.Material, RenderMaterial> materialConverter
+  )
+  {
+    _contextStack = contextStack;
+    _xyzToPointConverter = xyzToPointConverter;
+    _materialConverter = materialConverter;
+  }
+
+  public SOG.Mesh RawConvert(DB.Mesh target)
+  {
+    var doc = _contextStack.Current.Document.Document;
+
+    List<double> vertices = GetSpeckleMeshVertexData(target);
+    List<int> faces = GetSpeckleMeshFaceData(target);
+
+    RenderMaterial? speckleMaterial = null;
+    if (doc.GetElement(target.MaterialElementId) is DB.Material revitMaterial)
+    {
+      speckleMaterial = _materialConverter.RawConvert(revitMaterial);
+    }
+
+    return new SOG.Mesh(vertices, faces, units: _contextStack.Current.SpeckleUnits)
+    {
+      ["renderMaterial"] = speckleMaterial
+    };
+  }
+
+  private List<double> GetSpeckleMeshVertexData(DB.Mesh target)
+  {
+    var vertices = new List<double>();
+
+    foreach (var vert in target.Vertices)
+    {
+      vertices.AddRange(_xyzToPointConverter.RawConvert(vert).ToList());
+    }
+
+    return vertices;
+  }
+
+  private List<int> GetSpeckleMeshFaceData(DB.Mesh target)
+  {
+    var faces = new List<int>();
+    for (int i = 0; i < target.NumTriangles; i++)
+    {
+      var triangle = target.get_Triangle(i);
+      faces.AddRange(GetMeshTriangleData(triangle));
+    }
+
+    return faces;
+  }
+
+  /// <summary>
+  /// Retrieves the triangle data of a mesh to be stored in a Speckle Mesh faces property.
+  /// </summary>
+  /// <param name="triangle">The mesh triangle object.</param>
+  /// <returns>A list of integers representing the triangle data.</returns>
+  /// <remarks>
+  /// Output format is a 4 item list with format [3, v1, v2, v3]; where the first item is the triangle flag (for speckle)
+  /// and the 3 following numbers are the indices of each vertex in the vertex list.
+  /// </remarks>
+  private IReadOnlyList<int> GetMeshTriangleData(DB.MeshTriangle triangle) =>
+    new[]
+    {
+      3, // The TRIANGLE flag in speckle
+      (int)triangle.get_Index(0),
+      (int)triangle.get_Index(1),
+      (int)triangle.get_Index(2)
+    };
+}
---
+++ DUI3-DX/Converters/Revit/Speckle.Converters.RevitShared/ToSpeckle/DirectShapeConversionToSpeckle.cs
@@ -0,0 +1,74 @@
+﻿using Objects.Other;
+using Speckle.Converters.Common;
+using Speckle.Converters.Common.Objects;
+using Speckle.Converters.RevitShared.Extensions;
+using Speckle.Converters.RevitShared.Helpers;
+using Speckle.Converters.RevitShared.ToSpeckle;
+using Speckle.Core.Models;
+
+namespace Speckle.Converters.Revit2023.ToSpeckle;
+
+[NameAndRankValue(nameof(DB.DirectShape), NameAndRankValueAttribute.SPECKLE_DEFAULT_RANK)]
+public class DirectShapeConversionToSpeckle : BaseConversionToSpeckle<DB.DirectShape, SOBR.DirectShape>
+{
+  private readonly RevitConversionContextStack _contextStack;
+  private readonly IRawConversion<DB.Mesh, SOG.Mesh> _meshConverter;
+  private readonly IRawConversion<DB.Material, RenderMaterial> _materialConverter;
+  private readonly ParameterObjectAssigner _parameterObjectAssigner;
+
+  public DirectShapeConversionToSpeckle(
+    RevitConversionContextStack contextStack,
+    IRawConversion<DB.Mesh, SOG.Mesh> meshConverter,
+    IRawConversion<DB.Material, RenderMaterial> materialConverter,
+    ParameterObjectAssigner parameterObjectAssigner
+  )
+  {
+    _contextStack = contextStack;
+    _meshConverter = meshConverter;
+    _materialConverter = materialConverter;
+    _parameterObjectAssigner = parameterObjectAssigner;
+  }
+
+  public override SOBR.DirectShape RawConvert(DB.DirectShape target)
+  {
+    var category = target.Category.GetBuiltInCategory().GetSchemaBuilderCategoryFromBuiltIn();
+    var element = target.get_Geometry(new DB.Options());
+
+    var geometries = new List<Base>(); // POC: This could be an list of meshes.
+    foreach (var geometryObject in element)
+    {
+      switch (geometryObject)
+      {
+        case DB.Mesh mesh:
+          geometries.Add(_meshConverter.RawConvert(mesh));
+          break;
+        case DB.Solid solid:
+          var doc = _contextStack.Current.Document.Document;
+          foreach (DB.Face face in solid.Faces)
+          {
+            DB.Mesh revitMesh = face.Triangulate();
+            SOG.Mesh speckleMesh = _meshConverter.RawConvert(revitMesh);
+
+            // Override mesh material (which in this case should be null) with face material if it exists.
+            if (doc.GetElement(face.MaterialElementId) is DB.Material faceMaterial)
+            {
+              var speckleMaterial = _materialConverter.RawConvert(faceMaterial);
+              speckleMesh["renderMaterial"] = speckleMaterial; // POC: Mesh not having a typed renderMaterial is madness
+            }
+
+            geometries.Add(speckleMesh);
+          }
+          break;
+      }
+    }
+
+    SOBR.DirectShape result = new(target.Name, category, geometries) { displayValue = geometries };
+
+    // POC: Parameter extractor exlodes with DS, returns a null elementType somewhere.
+    _parameterObjectAssigner.AssignParametersToBase(target, result);
+
+    result["type"] = target.Name;
+
+    return result;
+  }
+}
