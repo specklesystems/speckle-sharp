9736d5679735fce6ed78478536dbdc6165dd03f6
Connor Ivy <connor@speckle.systems>
2024-04-26 15:12:17 -0500

    fix build

--- ConnectorCore/DllConflictManagement/Serialization/SpeckleNewtonsoftSerializer.cs
+++ ConnectorCore/DllConflictManagement/Serialization/SpeckleNewtonsoftSerializer.cs
@@ -6,5 +6,9 @@ public class SpeckleNewtonsoftSerializer : ISerializer
 {
   public string Serialize<T>(T obj) => JsonConvert.SerializeObject(obj);
 
-  public T Deserialize<T>(string serialized) => JsonConvert.DeserializeObject<T>(serialized);
+  public T Deserialize<T>(string serialized) =>
+    JsonConvert.DeserializeObject<T>(serialized)
+    ?? throw new InvalidOperationException(
+      $"Unable to deserialize the provided JSON into an object of type {typeof(T)}"
+    );
 }
