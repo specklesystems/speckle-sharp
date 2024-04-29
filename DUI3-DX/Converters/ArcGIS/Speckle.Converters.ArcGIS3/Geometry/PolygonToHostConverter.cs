2e32a3e2d1df0e18eb08295f03548bb21655fa2e
KatKatKateryna <kateryna@speckle.systems>
2024-04-30 00:58:36 +0100

    add loops to polygons on receive

--- DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Geometry/PolygonToHostConverter.cs
+++ DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Geometry/PolygonToHostConverter.cs
@@ -14,16 +14,25 @@ public class PolygonToHostConverter : IRawConversion<List<SGIS.GisPolygonGeometr
 
   public ACG.Polygon RawConvert(List<SGIS.GisPolygonGeometry> target)
   {
+    if (target.Count == 0)
+    {
+      throw new SpeckleConversionException("Feature contains no geometry");
+    }
+
     List<ACG.Polygon> polyList = new();
     foreach (SGIS.GisPolygonGeometry poly in target)
     {
-      // POC: add voids
       ACG.Polyline boundary = _polylineConverter.RawConvert(poly.boundary);
-      polyList.Add(new ACG.PolygonBuilderEx(boundary).ToGeometry());
-    }
-    if (polyList.Count == 0)
-    {
-      throw new SpeckleConversionException("Feature contains no geometry");
+      ACG.PolygonBuilderEx polyOuterRing = new(boundary);
+
+      foreach (SOG.Polyline loop in poly.voids)
+      {
+        // adding inner loops: https://github.com/esri/arcgis-pro-sdk/wiki/ProSnippets-Geometry#build-a-donut-polygon
+        ACG.Polyline loop_native = _polylineConverter.RawConvert(loop);
+        polyOuterRing.AddPart(loop_native.Copy3DCoordinatesToList());
+      }
+      ACG.Polygon polygon = polyOuterRing.ToGeometry();
+      polyList.Add(polygon);
     }
     return new ACG.PolygonBuilderEx(polyList, ACG.AttributeFlags.HasZ).ToGeometry();
   }
