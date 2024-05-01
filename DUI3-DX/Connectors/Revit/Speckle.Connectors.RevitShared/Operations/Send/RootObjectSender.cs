77806b62bf35c1916c9ecd7257fc3a85134efa8b
Oğuzhan Koral <45078678+oguzhankoral@users.noreply.github.com>
2024-05-01 22:42:49 +0300

    CNX-9378 Pass source app from RevitSettings (#3334)
    Pass source app from RevitSettings

--- DUI3-DX/Connectors/Revit/Speckle.Connectors.RevitShared/Operations/Send/RootObjectSender.cs
+++ DUI3-DX/Connectors/Revit/Speckle.Connectors.RevitShared/Operations/Send/RootObjectSender.cs
@@ -3,6 +3,7 @@ using Speckle.Connectors.Utils.Operations;
 using Speckle.Core.Api;
 using System.Threading.Tasks;
 using System.Threading;
+using Speckle.Connectors.Revit.Plugin;
 using Speckle.Core.Credentials;
 using Speckle.Core.Models;
 using Speckle.Core.Transports;
@@ -16,10 +17,12 @@ namespace Speckle.Connectors.Revit.Operations.Send;
 internal class RootObjectSender : IRootObjectSender
 {
   private readonly Func<Account, string, ITransport> _transportFactory;
+  private readonly RevitSettings _revitSettings;
 
-  public RootObjectSender(Func<Account, string, ITransport> transportFactory)
+  public RootObjectSender(Func<Account, string, ITransport> transportFactory, RevitSettings revitSettings)
   {
     _transportFactory = transportFactory;
+    _revitSettings = revitSettings;
   }
 
   public async Task<string> Send(
@@ -51,7 +54,7 @@ internal class RootObjectSender : IRootObjectSender
         {
           streamId = projectId,
           branchName = modelId,
-          sourceApplication = "Rhino",
+          sourceApplication = _revitSettings.HostSlug, // POC: These naming is a bit?
           objectId = sendResult.rootObjId
         },
         ct
