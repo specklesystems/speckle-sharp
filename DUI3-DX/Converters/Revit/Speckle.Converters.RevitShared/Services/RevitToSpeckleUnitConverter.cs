b4d232f9ee791b1e0c3a294443da8e0ec4eb1431
Connor Ivy <connor@speckle.systems>
2024-05-01 17:45:15 -0500

    Add alternate feet and meters

--- DUI3-DX/Converters/Revit/Speckle.Converters.RevitShared/Services/RevitToSpeckleUnitConverter.cs
+++ DUI3-DX/Converters/Revit/Speckle.Converters.RevitShared/Services/RevitToSpeckleUnitConverter.cs
@@ -13,8 +13,10 @@ public sealed class RevitToSpeckleUnitConverter : IHostToSpeckleUnitConverter<DB
     _unitMapping[DB.UnitTypeId.Millimeters] = Units.Millimeters;
     _unitMapping[DB.UnitTypeId.Centimeters] = Units.Centimeters;
     _unitMapping[DB.UnitTypeId.Meters] = Units.Meters;
+    _unitMapping[DB.UnitTypeId.MetersCentimeters] = Units.Meters;
     _unitMapping[DB.UnitTypeId.Inches] = Units.Inches;
     _unitMapping[DB.UnitTypeId.Feet] = Units.Feet;
+    _unitMapping[DB.UnitTypeId.FeetFractionalInches] = Units.Feet;
   }
 
   // POC: maybe just convert, it's not a Try method
