7f7d3af4c95392b5950184a3c036e216c0835338
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-05-06 14:01:43 +0100

    Added units scaling to displayValue fallback Rhino

--- DUI3-DX/Converters/Rhino/Speckle.Converters.Rhino7/ToHost/TopLevel/SpeckleFallbackToHostConversion.cs
+++ DUI3-DX/Converters/Rhino/Speckle.Converters.Rhino7/ToHost/TopLevel/SpeckleFallbackToHostConversion.cs
@@ -1,5 +1,7 @@
-﻿using Speckle.Converters.Common;
+﻿using Rhino;
+using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
+using Speckle.Core.Kits;
 using Speckle.Core.Models;
 
 namespace Speckle.Converters.Rhino7.ToHost.TopLevel;
@@ -12,16 +14,19 @@ public class SpeckleFallbackToHostConversion
   private readonly IRawConversion<SOG.Line, RG.LineCurve> _lineConverter;
   private readonly IRawConversion<SOG.Polyline, RG.PolylineCurve> _polylineConverter;
   private readonly IRawConversion<SOG.Mesh, RG.Mesh> _meshConverter;
+  private readonly IConversionContextStack<RhinoDoc, UnitSystem> _contextStack;
 
   public SpeckleFallbackToHostConversion(
     IRawConversion<SOG.Line, RG.LineCurve> lineConverter,
     IRawConversion<SOG.Polyline, RG.PolylineCurve> polylineConverter,
-    IRawConversion<SOG.Mesh, RG.Mesh> meshConverter
+    IRawConversion<SOG.Mesh, RG.Mesh> meshConverter,
+    IConversionContextStack<RhinoDoc, UnitSystem> contextStack
   )
   {
     _lineConverter = lineConverter;
     _polylineConverter = polylineConverter;
     _meshConverter = meshConverter;
+    _contextStack = contextStack;
   }
 
   public object Convert(Base target) => RawConvert((DisplayableObject)target);
@@ -38,9 +43,27 @@ public class SpeckleFallbackToHostConversion
         SOG.Mesh mesh => _meshConverter.RawConvert(mesh),
         _ => throw new NotSupportedException($"Found unsupported fallback geometry: {item.GetType()}")
       };
+      x.Transform(GetUnitsTransform(item));
       result.Add(x);
     }
 
     return result;
+  }
+
+  private RG.Transform GetUnitsTransform(Base speckleObject)
+  {
+    /*
+     * POC: CNX-9270 Looking at a simpler, more performant way of doing unit scaling on `ToNative`
+     * by fully relying on the transform capabilities of the HostApp, and only transforming top-level stuff.
+     * This may not hold when adding more complex conversions, but it works for now!
+     */
+    if (speckleObject["units"] is string units)
+    {
+      var scaleFactor = Units.GetConversionFactor(units, _contextStack.Current.SpeckleUnits);
+      var scale = RG.Transform.Scale(RG.Point3d.Origin, scaleFactor);
+      return scale;
+    }
+
+    return RG.Transform.Identity;
   }
 }
