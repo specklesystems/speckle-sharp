f600e7543ff6cf7a363445fb100f9031e533c671
KatKatKateryna <kateryna@speckle.systems>
2024-05-13 12:52:42 +0100

    add scaling factor on receive

--- DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Geometry/PointSingleToHostConverter.cs
+++ DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Geometry/PointSingleToHostConverter.cs
@@ -1,14 +1,32 @@
+using ArcGIS.Desktop.Mapping;
+using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
+using Speckle.Core.Kits;
 using Speckle.Core.Models;
 
 namespace Speckle.Converters.ArcGIS3.Geometry;
 
 public class PointToHostConverter : IRawConversion<SOG.Point, ACG.MapPoint>
 {
+  private readonly IConversionContextStack<Map, ACG.Unit> _contextStack;
+
+  public PointToHostConverter(IConversionContextStack<Map, ACG.Unit> contextStack)
+  {
+    _contextStack = contextStack;
+  }
+
   public object Convert(Base target) => RawConvert((SOG.Point)target);
 
   public ACG.MapPoint RawConvert(SOG.Point target)
   {
-    return new ACG.MapPointBuilderEx(target.x, target.y, target.z).ToGeometry();
+    double scaleFactor = Units.GetConversionFactor(
+      _contextStack.Current.SpeckleUnits,
+      _contextStack.Current.Document.SpatialReference.Unit.ToString()
+    );
+    return new ACG.MapPointBuilderEx(
+      target.x * scaleFactor,
+      target.y * scaleFactor,
+      target.z * scaleFactor
+    ).ToGeometry();
   }
 }
