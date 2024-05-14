4801bc83a72a74097c9c6e761849521b9eb6732a
Adam Hathcock <adam@speckle.systems>
2024-05-14 09:44:20 +0100

    fix autocad

--- Build/Consts.cs
+++ Build/Consts.cs
@@ -6,7 +6,7 @@ public static class Consts
   public static readonly (string, string)[] Projects =
   {
     ("DUI3-DX\\Connectors\\ArcGIS\\Speckle.Connectors.ArcGIS3", "net6.0-windows"),
-    ("DUI3-DX\\Connectors\\Autocad\\Speckle.Connectors.Autocad", "net48"),
+    ("DUI3-DX\\Connectors\\Autocad\\Speckle.Connectors.Autocad2023", "net48"),
     ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.Revit2023", "net48"),
     ("DUI3-DX\\Connectors\\Rhino\\Speckle.Connectors.Rhino7", "net48")
   };
