a5cf5847ca92babac7e060ff6cd978e489b13b2c
Adam Hathcock <adam@speckle.systems>
2024-05-14 09:10:15 +0100

    Add more connectors to zip

--- Build/Consts.cs
+++ Build/Consts.cs
@@ -3,12 +3,11 @@
 public static class Consts
 {
   public static readonly string[] Solutions = { "DUI3-DX.slnf" };
-  public static readonly string[] Frameworks = { "net48" };
   public static readonly (string, string)[] Projects =
   {
+    ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.ArcGIS3", "net6.0-windows"),
+    ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.Autocad", "net48"),
     ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.Revit2023", "net48"),
-    ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.ArcGIS3", "net48")
+    ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.Rhino7", "net48")
   };
-
-  public static readonly string Root = "C:\\Users\\adam\\Git\\speckle-sharp";
 }
