328322a5639a969505caee7d7b7a79624c695312
Adam Hathcock <adam@speckle.systems>
2024-05-14 09:18:42 +0100

    fix connectors :(

--- Build/Consts.cs
+++ Build/Consts.cs
@@ -5,9 +5,9 @@ public static class Consts
   public static readonly string[] Solutions = { "DUI3-DX.slnf" };
   public static readonly (string, string)[] Projects =
   {
-    ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.ArcGIS3", "net6.0-windows"),
-    ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.Autocad", "net48"),
+    ("DUI3-DX\\Connectors\\ArcGIS\\Speckle.Connectors.ArcGIS3", "net6.0-windows"),
+    ("DUI3-DX\\Connectors\\Autocad\\Speckle.Connectors.Autocad", "net48"),
     ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.Revit2023", "net48"),
-    ("DUI3-DX\\Connectors\\Revit\\Speckle.Connectors.Rhino7", "net48")
+    ("DUI3-DX\\Connectors\\Rhino\\Speckle.Connectors.Rhino7", "net48")
   };
 }
