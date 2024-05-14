45f69c29f7863a3ca595056c19d5f589d88a6123
Adam Hathcock <adam@speckle.systems>
2024-05-14 14:52:51 +0100

    docs say GH token is bearer

--- Build/Github.cs
+++ Build/Github.cs
@@ -23,7 +23,7 @@ public static class Github
       RequestUri = new Uri("https://api.github.com/repos/specklesystems/connector-installers/dispatches"),
       Headers =
       {
-        Authorization = new AuthenticationHeaderValue("Token", token),
+        Authorization = new AuthenticationHeaderValue("Bearer", token),
         UserAgent = { new ProductInfoHeaderValue("Speckle.build", "3.0.0") }
       },
       Content = content
