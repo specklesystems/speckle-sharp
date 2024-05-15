e13cb5697bcf4879933123b71430c1e2cde211f0
oguzhankoral <oguzhankoral@gmail.com>
2024-05-15 15:03:58 +0300

    Clear WpfHost.Child on panel close

--- DUI3-DX/Connectors/Rhino/Speckle.Connectors.Rhino7/HostApp/SpeckleRhinoPanelHost.cs
+++ DUI3-DX/Connectors/Rhino/Speckle.Connectors.Rhino7/HostApp/SpeckleRhinoPanelHost.cs
@@ -1,4 +1,7 @@
 using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using Rhino.UI;
 using Speckle.Connectors.DUI.WebView;
 using Speckle.Connectors.Rhino7.Plugin;
 
@@ -8,10 +11,29 @@ namespace Speckle.Connectors.Rhino7.HostApp;
 public class SpeckleRhinoPanelHost : RhinoWindows.Controls.WpfElementHost
 {
   private readonly uint _docSn;
+  private readonly DUI3ControlWebView? _webView;
 
   public SpeckleRhinoPanelHost(uint docSn)
     : base(SpeckleConnectorsRhino7Plugin.Instance.Container?.Resolve<DUI3ControlWebView>(), null)
   {
     _docSn = docSn;
+    _webView = SpeckleConnectorsRhino7Plugin.Instance.Container?.Resolve<DUI3ControlWebView>();
+    Panels.Closed += PanelsOnClosed;
+  }
+
+  private void PanelsOnClosed(object sender, PanelEventArgs e)
+  {
+    if (e.PanelId == typeof(SpeckleRhinoPanelHost).GUID)
+    {
+      // Disconnect UIElement from WpfElementHost. Otherwise, we can't reinit panel with same DUI3ControlWebView
+      if (_webView != null)
+      {
+        // Since WpfHost inherited from Border, find the parent as border and set null it's Child.
+        if (LogicalTreeHelper.GetParent(_webView) is Border border)
+        {
+          border.Child = null;
+        }
+      }
+    }
   }
 }
