29b3ab0957173f75d3bb090ea1d69081ee4cc5f5
KatKatKateryna <kateryna@speckle.systems>
2024-05-18 02:32:32 +0100

    send densified curved segments using Map CRS precision

--- DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Features/PolylineFeatureToSpeckleConverter.cs
+++ DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Features/PolylineFeatureToSpeckleConverter.cs
@@ -1,3 +1,5 @@
+using ArcGIS.Desktop.Mapping;
+using Speckle.Converters.Common;
 using Speckle.Converters.Common.Objects;
 
 namespace Speckle.Converters.ArcGIS3.Features;
@@ -5,17 +7,38 @@ namespace Speckle.Converters.ArcGIS3.Features;
 public class PolyineFeatureToSpeckleConverter : IRawConversion<ACG.Polyline, IReadOnlyList<SOG.Polyline>>
 {
   private readonly IRawConversion<ACG.ReadOnlySegmentCollection, SOG.Polyline> _segmentConverter;
+  private readonly IConversionContextStack<Map, ACG.Unit> _contextStack;
 
-  public PolyineFeatureToSpeckleConverter(IRawConversion<ACG.ReadOnlySegmentCollection, SOG.Polyline> segmentConverter)
+  public PolyineFeatureToSpeckleConverter(
+    IRawConversion<ACG.ReadOnlySegmentCollection, SOG.Polyline> segmentConverter,
+    IConversionContextStack<Map, ACG.Unit> contextStack
+  )
   {
     _segmentConverter = segmentConverter;
+    _contextStack = contextStack;
   }
 
   public IReadOnlyList<SOG.Polyline> RawConvert(ACG.Polyline target)
   {
     // https://pro.arcgis.com/en/pro-app/latest/sdk/api-reference/topic8480.html
     List<SOG.Polyline> polylineList = new();
-    foreach (var segmentCollection in target.Parts)
+    ACG.Polyline polylineToConvert = target;
+
+    // segmentize the polylines with curves using precision value of the Map's Spatial Reference
+    if (target.HasCurves is true)
+    {
+      var densifiedPolyline = ACG.GeometryEngine.Instance.DensifyByDeviation(
+        target,
+        _contextStack.Current.Document.SpatialReference.XYTolerance
+      );
+      polylineToConvert = (ACG.Polyline)densifiedPolyline;
+      if (densifiedPolyline == null)
+      {
+        throw new ArgumentException("Polyline densification failed");
+      }
+    }
+
+    foreach (var segmentCollection in polylineToConvert.Parts)
     {
       polylineList.Add(_segmentConverter.RawConvert(segmentCollection));
     }
