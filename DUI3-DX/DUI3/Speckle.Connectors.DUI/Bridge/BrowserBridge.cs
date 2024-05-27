1f59f01c465bf3b036a62ecf6d460dfb187cfd66
Oğuzhan Koral <45078678+oguzhankoral@users.noreply.github.com>
2024-05-27 16:42:26 +0300

    Chore (dui3): error message from inner exception on sync functions (#3441)
    * Pass inner exception message if it is thrown on SYNC function
    
    * Extend error details

--- DUI3-DX/DUI3/Speckle.Connectors.DUI/Bridge/BrowserBridge.cs
+++ DUI3-DX/DUI3/Speckle.Connectors.DUI/Bridge/BrowserBridge.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks.Dataflow;
 using System.Diagnostics;
 using Microsoft.Extensions.Logging;
 using Speckle.Connectors.Utils;
+using Speckle.Core.Models.Extensions;
 
 namespace Speckle.Connectors.DUI.Bridge;
 
@@ -255,7 +256,17 @@ public class BrowserBridge : IBridge
   /// </summary>
   private void ReportUnhandledError(string requestId, Exception e)
   {
-    var errorDetails = new { e.Message, Error = e.ToString() };
+    var message = e.Message;
+    if (e is TargetInvocationException tie) // Exception on SYNC function calls. Message should be passed from inner exception since it is wrapped.
+    {
+      message = tie.InnerException?.Message;
+    }
+    var errorDetails = new
+    {
+      Message = message, // Topmost message
+      Error = e.ToFormattedString(), // All messages from exceptions
+      StackTrace = e.ToString()
+    };
 
     var serializedError = JsonConvert.SerializeObject(errorDetails, _serializerOptions);
     NotifyUIMethodCallResultReady(requestId, serializedError);
