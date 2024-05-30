92dcfc4e8c2d4fb03004c126bc096c4cb5e2c637
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-05-30 13:01:55 +0100

    Beam

--- Objects/Objects/BuiltElements/Beam.cs
+++ Objects/Objects/BuiltElements/Beam.cs
@@ -5,22 +5,33 @@ using Speckle.Core.Models;
 
 namespace Objects.BuiltElements;
 
-public class Beam : Base, IDisplayValue<List<Mesh>>
+public class Beam : Base, IDisplayValue<IReadOnlyList<Base>>
 {
   public Beam() { }
 
-  [SchemaInfo("Beam", "Creates a Speckle beam", "BIM", "Structure")]
-  public Beam([SchemaMainParam] ICurve baseLine)
+  public Beam(ICurve baseLine, Level? level, string? units, IReadOnlyList<Mesh>? displayValue = null)
   {
     this.baseLine = baseLine;
+    this.level = level;
+    this.units = units;
+    this.displayValue = ((IReadOnlyList<Base>?)displayValue) ?? new[] { (Base)baseLine };
   }
 
   public ICurve baseLine { get; set; }
 
   public virtual Level? level { get; internal set; }
 
-  public string units { get; set; }
+  public string? units { get; set; }
 
   [DetachProperty]
-  public List<Mesh> displayValue { get; set; }
+  public IReadOnlyList<Base> displayValue { get; set; }
+
+  #region Schema Info Constructors
+  [SchemaInfo("Beam", "Creates a Speckle beam", "BIM", "Structure")]
+  public Beam([SchemaMainParam] ICurve baseLine)
+  {
+    this.baseLine = baseLine;
+  }
+
+  #endregion
 }
