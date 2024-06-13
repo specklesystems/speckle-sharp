f6a23ebd7dcc7652f4992cc5d2f7656a39b1d98d
KatKatKateryna <89912278+KatKatKateryna@users.noreply.github.com>
2024-06-13 16:48:05 +0800

    DUI3-295 track non native received objects for the full report (#3497)
    * introduce ConverionTracker; simplify variables
    
    * track conversions up to writing groups into dataset
    
    * throw exceptions for failed datasets. TODO: write to results
    
    * correct results
    
    * correct result order
    
    * properly updating Trackers
    
    * more comments
    
    * remove fake Exception from GIS layers
    
    * Move BUILD function to the top

--- DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/INonNativeFeaturesUtils.cs
+++ DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/INonNativeFeaturesUtils.cs
@@ -4,7 +4,5 @@ namespace Speckle.Converters.ArcGIS3.Utils;
 
 public interface INonNativeFeaturesUtils
 {
-  public List<(string parentPath, string converted)> WriteGeometriesToDatasets(
-    Dictionary<TraversalContext, (string parentPath, ACG.Geometry geom)> convertedObjs
-  );
+  public void WriteGeometriesToDatasets(Dictionary<TraversalContext, ObjectConversionTracker> conversionTracker);
 }
