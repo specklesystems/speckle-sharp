09d1a43cc308267c8c3c533b181cd5de31bc0aa5
Adam Hathcock <adam@speckle.systems>
2024-06-13 13:37:46 +0100

    proxy map is also proxy aware

--- DUI3-DX/Connectors/Revit/Speckle.Connectors.RevitShared/DependencyInjection/ProxyMap.cs
+++ DUI3-DX/Connectors/Revit/Speckle.Connectors.RevitShared/DependencyInjection/ProxyMap.cs
@@ -8,57 +8,70 @@ namespace Speckle.Connectors.Revit2023.Converters;
 [SuppressMessage("Performance", "CA1810:Initialize reference type static fields inline")]
 public class ProxyMap : IProxyMap
 {
-  private static readonly ConcurrentDictionary<Type, Type> s_typeMaps = new();
-  private static readonly ConcurrentDictionary<Type, Type> s_reverseMaps = new();
+  private static readonly ConcurrentDictionary<Type, Type> s_revitToInterfaceMap = new();
+  private static readonly ConcurrentDictionary<Type, Type> s_proxyToInterfaceMap = new();
+  private static readonly ConcurrentDictionary<Type, Type> s_interfaceToRevit = new();
   private static readonly ConcurrentDictionary<Type, Func<object, object>> s_proxyFactory = new();
 
   [SuppressMessage("Maintainability", "CA1506:Avoid excessive class coupling")]
   static ProxyMap()
   {
-    Add<DB.Element, IRevitElement>(x => new ElementProxy(x));
-    Add<DB.FamilyInstance, IRevitFamilyInstance>(x => new FamilyInstanceProxy(x));
-    Add<DB.Curve, IRevitCurve>(x => new CurveProxy(x));
-    Add<DB.BoundarySegment, IRevitBoundarySegment>(x => new BoundarySegmentProxy(x));
-    Add<DB.Level, IRevitLevel>(x => new LevelProxy(x));
-    Add<DB.Location, IRevitLocation>(x => new LocationProxy(x));
-    Add<DB.Material, IRevitMaterial>(x => new MaterialProxy(x));
-    Add<DB.ModelCurveArray, IRevitModelCurveArray>(x => new ModelCurveArrayProxy(x));
-    Add<DB.ModelCurveArrArray, IRevitModelCurveArrArray>(x => new ModelCurveArrArrayProxy(x));
-    Add<DB.Parameter, IRevitParameter>(x => new ParameterProxy(x));
-    Add<DB.BasePoint, IRevitBasePoint>(x => new BasePointProxy(x));
-    Add<DB.Wall, IRevitWall>(x => new WallProxy(x));
-    Add<DB.Panel, IRevitPanel>(x => new PanelProxy(x));
-    Add<DB.Floor, IRevitFloor>(x => new FloorProxy(x));
-    Add<DB.Ceiling, IRevitCeiling>(x => new CeilingProxy(x));
-    Add<DB.FootPrintRoof, IRevitFootPrintRoof>(x => new FootPrintRoofProxy(x));
-    Add<DB.ModelLine, IRevitModelLine>(x => new ModelLineProxy(x));
-    Add<DB.RoofBase, IRevitRoofBase>(x => new RoofBaseProxy(x));
+    Add<DB.Element, IRevitElement, ElementProxy>(x => new ElementProxy(x));
+    Add<DB.FamilyInstance, IRevitFamilyInstance, FamilyInstanceProxy>(x => new FamilyInstanceProxy(x));
+    Add<DB.Curve, IRevitCurve, CurveProxy>(x => new CurveProxy(x));
+    Add<DB.BoundarySegment, IRevitBoundarySegment, BoundarySegmentProxy>(x => new BoundarySegmentProxy(x));
+    Add<DB.Level, IRevitLevel, LevelProxy>(x => new LevelProxy(x));
+    Add<DB.Location, IRevitLocation, LocationProxy>(x => new LocationProxy(x));
+    Add<DB.Material, IRevitMaterial, MaterialProxy>(x => new MaterialProxy(x));
+    Add<DB.ModelCurveArray, IRevitModelCurveArray, ModelCurveArrayProxy>(x => new ModelCurveArrayProxy(x));
+    Add<DB.ModelCurveArrArray, IRevitModelCurveArrArray, ModelCurveArrArrayProxy>(x => new ModelCurveArrArrayProxy(x));
+    Add<DB.Parameter, IRevitParameter, ParameterProxy>(x => new ParameterProxy(x));
+    Add<DB.BasePoint, IRevitBasePoint, BasePointProxy>(x => new BasePointProxy(x));
+    Add<DB.Wall, IRevitWall, WallProxy>(x => new WallProxy(x));
+    Add<DB.Panel, IRevitPanel, PanelProxy>(x => new PanelProxy(x));
+    Add<DB.Floor, IRevitFloor, FloorProxy>(x => new FloorProxy(x));
+    Add<DB.Ceiling, IRevitCeiling, CeilingProxy>(x => new CeilingProxy(x));
+    Add<DB.FootPrintRoof, IRevitFootPrintRoof, FootPrintRoofProxy>(x => new FootPrintRoofProxy(x));
+    Add<DB.ModelLine, IRevitModelLine, ModelLineProxy>(x => new ModelLineProxy(x));
+    Add<DB.RoofBase, IRevitRoofBase, RoofBaseProxy>(x => new RoofBaseProxy(x));
   }
 
-  private static void Add<T, TProxy>(Func<T, TProxy> f)
+  private static void Add<T, TInterface, TProxy>(Func<T, TProxy> f)
     where T : class
-    where TProxy : notnull
+    where TInterface : notnull
+    where TProxy : TInterface
   {
-    s_typeMaps.TryAdd(typeof(T), typeof(TProxy));
-    s_proxyFactory.TryAdd(typeof(TProxy), w => f((T)w));
-    s_reverseMaps.TryAdd(typeof(TProxy), typeof(T));
+    s_revitToInterfaceMap.TryAdd(typeof(T), typeof(TInterface));
+    s_proxyToInterfaceMap.TryAdd(typeof(TProxy), typeof(TInterface));
+    s_proxyFactory.TryAdd(typeof(TInterface), w => f((T)w));
+    s_interfaceToRevit.TryAdd(typeof(TInterface), typeof(T));
   }
 
-  public Type? GetMappedType(Type type)
+  public Type? GetMappedTypeFromHostType(Type type)
   {
-    if (s_typeMaps.TryGetValue(type, out var t))
+    if (s_revitToInterfaceMap.TryGetValue(type, out var t))
     {
       return t;
     }
     return null;
-  }
-
-  public Type? UnmapType(Type type)
+  } 
+  public Type? GetMappedTypeFromProxyType(Type type)
   {
-    if (s_reverseMaps.TryGetValue(type, out var t))
+    if (s_proxyToInterfaceMap.TryGetValue(type, out var t))
     {
       return t;
     }
+
+    return null;
+  }
+
+  public Type? GetHostTypeFromMappedType(Type type)
+  {
+    if (s_interfaceToRevit.TryGetValue(type, out var t))
+    {
+      return t;
+    }
+
     return null;
   }
 
