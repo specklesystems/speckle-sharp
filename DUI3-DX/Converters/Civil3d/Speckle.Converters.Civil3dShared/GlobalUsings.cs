0d8f5c7dbbcc8aefc2afee73b78ed05a3c7799ab
Claire Kuang <kuang.claire@gmail.com>
2024-06-24 10:07:32 +0100

    removes converter

--- DUI3-DX/Converters/Civil3d/Speckle.Converters.Civil3dShared/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using ADB = Autodesk.AutoCAD.DatabaseServices;
-global using AG = Autodesk.AutoCAD.Geometry;
-global using SOG = Objects.Geometry;
-global using SOP = Objects.Primitive;
-global using Document = Autodesk.AutoCAD.ApplicationServices.Document;
-global using Application = Autodesk.AutoCAD.ApplicationServices.Core.Application;
