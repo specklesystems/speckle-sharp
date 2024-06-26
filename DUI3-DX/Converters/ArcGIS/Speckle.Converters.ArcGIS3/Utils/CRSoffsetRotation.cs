2d72914fe868a04c51983153c37c01e330bcbf42
KatKatKateryna <kateryna@speckle.systems>
2024-06-27 00:25:25 +0100

    more complete constructor

--- DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/CRSoffsetRotation.cs
+++ DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/CRSoffsetRotation.cs
@@ -1,22 +1,44 @@
+using ArcGIS.Core.Geometry;
+
 namespace Speckle.Converters.ArcGIS3.Utils;
 
 /// <summary>
-/// Container with origin offsets and rotation angle
+/// Container with origin offsets and rotation angle for the specified SpatialReference
 /// </summary>
-public readonly struct CRSoffsetRotation
+public struct CRSoffsetRotation
 {
-  public double LatOffset { get; }
-  public double LonOffset { get; }
-  public double TrueNorthRadians { get; }
+  public SpatialReference SpatialReference { get; }
+  public double LatOffset { get; set; }
+  public double LonOffset { get; set; }
+  public double TrueNorthRadians { get; set; }
 
   /// <summary>
   /// Initializes a new instance of <see cref="CRSoffsetRotation"/>.
   /// </summary>
+  /// <param name="spatialReference">Latitude (Y) ofsset in the current SpatialReference units.</param>
+  public CRSoffsetRotation(SpatialReference spatialReference)
+  {
+    SpatialReference = spatialReference;
+    LatOffset = 0;
+    LonOffset = 0;
+    TrueNorthRadians = 0;
+  }
+
+  /// <summary>
+  /// Initializes a new instance of <see cref="CRSoffsetRotation"/>.
+  /// </summary>
+  /// <param name="spatialReference">SpatialReference to apply offsets and rotation to.</param>
   /// <param name="latOffset">Latitude (Y) ofsset in the current SpatialReference units.</param>
   /// <param name="lonOffset">Longitude (X) ofsset in the current SpatialReference units.</param>
   /// <param name="trueNorthRadians">Angle to True North in radians.</param>
-  public CRSoffsetRotation(double latOffset, double lonOffset, double trueNorthRadians)
+  public CRSoffsetRotation(
+    SpatialReference spatialReference,
+    double latOffset,
+    double lonOffset,
+    double trueNorthRadians
+  )
   {
+    SpatialReference = spatialReference;
     LatOffset = latOffset;
     LonOffset = lonOffset;
     TrueNorthRadians = trueNorthRadians;
