c31aba1e389e837cfacd4ed95e317f690374385f
Kilian Speiser <kilsp@dtu.dk>
2024-06-27 11:25:41 +0200

    params null

--- Objects/Converters/ConverterRevit/ConverterRevitShared/ConversionUtils.cs
+++ Objects/Converters/ConverterRevit/ConverterRevitShared/ConversionUtils.cs
@@ -152,11 +152,15 @@ public partial class ConverterRevit
 #else
     double area_transformed = UnitUtils.ConvertFromInternalUnits(area, UnitTypeId.SquareMeters);
 #endif
-            Base parameters = (Base)obj["parameters"];
-            if (parameters != null)
+            var paramObject = obj["parameters"];
+            if( paramObject != null )
             {
-              Objects.BuiltElements.Revit.Parameter hostedAreaParameter = new Parameter("Cutout Area", area_transformed, "m²");
-              parameters["Cutout Area"] = hostedAreaParameter;
+              Base parameters = (Base)paramObject;
+              if (parameters != null)
+              {
+                Objects.BuiltElements.Revit.Parameter hostedAreaParameter = new Parameter("Cutout Area", area_transformed, "m²");
+                parameters["Cutout Area"] = hostedAreaParameter;
+              }
             }
             convertedHostedElements.Add(obj);
             ConvertedObjects.Add(obj.applicationId);
