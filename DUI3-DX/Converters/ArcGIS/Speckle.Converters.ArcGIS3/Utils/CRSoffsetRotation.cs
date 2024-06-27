13a405fd592c0c8c873f71e533c29c246cf62916
KatKatKateryna <kateryna@speckle.systems>
2024-06-27 01:10:02 +0100

    add Send-Receive transforms

--- DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/CRSoffsetRotation.cs
+++ DUI3-DX/Converters/ArcGIS/Speckle.Converters.ArcGIS3/Utils/CRSoffsetRotation.cs
@@ -12,10 +12,41 @@ public struct CRSoffsetRotation
   public double LonOffset { get; set; }
   public double TrueNorthRadians { get; set; }
 
+  private void NormalizeAngle()
+  {
+    if (TrueNorthRadians < -2 * Math.PI || TrueNorthRadians > 2 * Math.PI)
+    {
+      // do something
+      TrueNorthRadians += 0;
+    }
+  }
+
+  public SOG.Point OffsetRotateOnReceive(SOG.Point point)
+  {
+    // ?? scale PT first, to match CRS?
+    NormalizeAngle();
+    double x2 = point.x * Math.Cos(TrueNorthRadians) - point.y * Math.Sin(TrueNorthRadians);
+    double y2 = point.x * Math.Sin(TrueNorthRadians) + point.y * Math.Cos(TrueNorthRadians);
+    x2 += LonOffset;
+    y2 += LatOffset;
+    return new SOG.Point(x2, y2, point.z, point.units);
+  }
+
+  public SOG.Point OffsetRotateOnSend(SOG.Point point)
+  {
+    // ?? scale PT first, to match CRS?
+    NormalizeAngle();
+    double x2 = point.x - LonOffset;
+    double y2 = point.y - LatOffset;
+    x2 = x2 * Math.Cos(TrueNorthRadians) + y2 * Math.Sin(TrueNorthRadians);
+    y2 = -x2 * Math.Sin(TrueNorthRadians) + y2 * Math.Cos(TrueNorthRadians);
+    return new SOG.Point(x2, y2, point.z, point.units);
+  }
+
   /// <summary>
   /// Initializes a new instance of <see cref="CRSoffsetRotation"/>.
   /// </summary>
-  /// <param name="spatialReference">Latitude (Y) ofsset in the current SpatialReference units.</param>
+  /// <param name="spatialReference">SpatialReference to apply offsets and rotation to.</param>
   public CRSoffsetRotation(SpatialReference spatialReference)
   {
     SpatialReference = spatialReference;
