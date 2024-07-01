7d10c7fc89414290a7d5e36780d024f5b7ec1d82
KatKatKateryna <89912278+KatKatKateryna@users.noreply.github.com>
2024-07-01 18:44:48 +0800

    DUI3-305 agis implement highlight for individual features e.g. from receive report (#3529)
    * introduce ObjectID constructor
    
    * adapt BasicBindings highlight to use ObjectId struct
    
    * select individual features
    
    * update missing tracker Items
    
    * query features by ID for selection
    
    * separate struct for MapMembers
    
    * add comments
    
    * formatting
    
    * formatting
    
    ---------
    
    Co-authored-by: Oğuzhan Koral <45078678+oguzhankoral@users.noreply.github.com>

---
+++ DUI3-DX/Connectors/ArcGIS/Speckle.Connectors.ArcGIS3/Utils/MapMemberFeature.cs
@@ -0,0 +1,16 @@
+using ArcGIS.Desktop.Mapping;
+
+namespace Speckle.Connectors.ArcGIS.Utils;
+
+// bind together a layer object on the map, and auto-assigned ID if the specific feature
+public readonly struct MapMemberFeature
+{
+  public int? FeatureId { get; } // unique feature id (start from 0) of a feature in the layer
+  public MapMember MapMember { get; } // layer object on the Map
+
+  public MapMemberFeature(MapMember mapMember, int? featureId)
+  {
+    MapMember = mapMember;
+    FeatureId = featureId;
+  }
+}
