8675966ee69a55a25c4f2def633543d9f8adc713
Adam Hathcock <adam@speckle.systems>
2024-07-02 16:29:28 +0100

    Rhino 7 test

---
+++ DUI3-DX/Converters/Rhino/Speckle.Converters.Rhino7.Tests/EllipseToSpeckleConverterTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NUnit.Framework;
+using Rhino;
+using Speckle.Converters.Common;
+using Speckle.Converters.Common.Objects;
+
+namespace Speckle.Converters.Rhino7.Tests;
+
+public class EllipseToSpeckleConverterTests
+{
+  private MockRepository _repository;
+
+  private Mock<IConversionContextStack<RhinoDoc, UnitSystem>> _conversionContextStack;
+
+  private Mock<ITypedConverter<RG.Plane, SOG.Plane>> _planeConverter;
+  private Mock<ITypedConverter<RG.Box, SOG.Box>> _boxConverter;
+
+  [SetUp]
+  public void Setup()
+  {
+    _repository = new(MockBehavior.Strict);
+    _conversionContextStack = _repository.Create<IConversionContextStack<RhinoDoc, UnitSystem>>();
+    _planeConverter = _repository.Create<ITypedConverter<RG.Plane, SOG.Plane>>();
+    _boxConverter = _repository.Create<ITypedConverter<RG.Box, SOG.Box>>();
+  }
+
+  [TearDown]
+  public void Verify() => _repository.VerifyAll();
+}
---
+++ DUI3-DX/Converters/Rhino/Speckle.Converters.Rhino7.Tests/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using RG = Rhino.Geometry;
+global using SOG = Objects.Geometry;
+global using SOP = Objects.Primitive;
