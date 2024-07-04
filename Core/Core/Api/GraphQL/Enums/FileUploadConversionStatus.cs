82f0cc9155d77ad77ca81b7904ddc887c9fcf380
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-07-04 22:32:55 +0100

    int enum as int

--- Core/Core/Api/GraphQL/Enums/FileUploadConversionStatus.cs
+++ Core/Core/Api/GraphQL/Enums/FileUploadConversionStatus.cs
@@ -3,8 +3,8 @@
 //This enum isn't explicitly defined in the schema, instead its usages are int typed (But represent an enum)
 public enum FileUploadConversionStatus
 {
-  Queued,
-  Processing,
-  Success,
-  Error,
+  Queued = 0,
+  Processing = 1,
+  Success = 2,
+  Error = 3,
 }
