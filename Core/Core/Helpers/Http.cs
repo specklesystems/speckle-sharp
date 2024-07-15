3d144b39aad11956c3b57bed07aa689746c1e89b
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2024-07-15 17:55:30 +0100

    fix(core/http): log message should not duplicate path separator - the Uri.PathAndQuery will return the leading path separator so the formatting context should not add it. - https://learn.microsoft.com/en-us/dotnet/api/system.uri?view=net-8.0#examples

--- Core/Core/Helpers/Http.cs
+++ Core/Core/Helpers/Http.cs
@@ -225,7 +225,7 @@ public sealed class SpeckleHttpClientHandler : HttpClientHandler
       SpeckleLog.Logger
         .ForContext("ExceptionType", policyResult.FinalException?.GetType())
         .Information(
-          "Execution of http request to {httpScheme}://{hostUrl}/{relativeUrl} {resultStatus} with {httpStatusCode} after {elapsed} seconds and {retryCount} retries",
+          "Execution of http request to {httpScheme}://{hostUrl}{relativeUrl} {resultStatus} with {httpStatusCode} after {elapsed} seconds and {retryCount} retries",
           request.RequestUri.Scheme,
           request.RequestUri.Host,
           request.RequestUri.PathAndQuery,
