25f77f7dbc5bf2b1bd6b75d34e321c119620c260
Adam Hathcock <adam@speckle.systems>
2024-07-25 11:27:36 +0100

    remove even more usings

--- ConnectorDynamo/ConnectorDynamo/ViewNode/View.cs
+++ ConnectorDynamo/ConnectorDynamo/ViewNode/View.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
 using Dynamo.Engine;
 using Dynamo.Graph.Nodes;
--- ConnectorRevit/ConnectorRevit/Entry/HelpCommand.cs
+++ ConnectorRevit/ConnectorRevit/Entry/HelpCommand.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics;
 using System.IO;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
