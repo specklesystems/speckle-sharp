94f1a66ca51a227214064a6c5f27ae8052a86e3c
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-08-19 17:25:10 +0100

    Added Long Send Test

---
+++ Objects/Tests/Objects.Tests.Unit/LogSendTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Speckle.Core.Api;
+using Speckle.Core.Credentials;
+using Speckle.Core.Models;
+using Speckle.Core.Transports;
+using Speckle.Sdk.Logging;
+using SpecklePathProvider = Speckle.Core.Helpers.SpecklePathProvider;
+
+namespace Objects.Tests.Unit;
+
+public class LogSendTest
+{
+  [Test]
+  [TestCase("5cbf84a0061172102ef8a66ae914f232", "https://testing1.speckle.dev/projects/1e9cb95ada")]
+  public async Task SendTest(string objectId, string destination)
+  {
+    var testData = await GetSampleData(objectId);
+
+    SpeckleLog.Logger.Information("Starting Long Send Test Send");
+
+    var destinationTransport = await GetDestination(destination);
+
+    var res = await Operations.Send(testData, new[] { destinationTransport });
+
+    SpeckleLog.Logger.Information($"Starting Send was successful: {objectId}", res);
+  }
+
+  private async Task<ITransport> GetDestination(string destination)
+  {
+    StreamWrapper sw = new(destination);
+    var acc = await sw.GetAccount();
+    return new ServerTransport(acc, sw.StreamId);
+  }
+
+  private async Task<Base> GetSampleData(string objectId)
+  {
+    using SQLiteTransport source = new(SpecklePathProvider.UserApplicationDataPath(), "longsendtest");
+
+    return await Operations.Receive(objectId, source);
+  }
+}
