25483ada366248af5a3dee8cf0dd898a33928a92
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-09-13 15:13:12 +0100

    Rhino8 Net7 -> Net4.8 (#3625)
    * Net7 -> Net4.8
    
    * Removed serilog enriching for netframework compat
    
    * fix: docker-compose.yml updates
    
    ---------
    
    Co-authored-by: Alan Rynne <alan@speckle.systems>

--- Core/Core/Logging/SpeckleLog.cs
+++ Core/Core/Logging/SpeckleLog.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
-using Serilog.Exceptions;
 using Speckle.Core.Credentials;
 using Speckle.Core.Helpers;
 
@@ -180,10 +179,10 @@ public static class SpeckleLog
       .Enrich.WithProperty("runtime", RuntimeInformation.FrameworkDescription)
       .Enrich.WithProperty("hostApplication", $"{hostApplicationName}{hostApplicationVersion ?? ""}");
 
-    if (logConfiguration.EnhancedLogContext)
-    {
-      serilogLogConfiguration = serilogLogConfiguration.Enrich.WithExceptionDetails();
-    }
+    // if (logConfiguration.EnhancedLogContext)
+    // {
+    //   serilogLogConfiguration = serilogLogConfiguration.Enrich.WithExceptionDetails();
+    // }
 
     if (logConfiguration.LogToFile && canLogToFile)
     {
