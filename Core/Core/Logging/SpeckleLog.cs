5e61741ad704106e59ce744b4cfc83f057b78d86
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-10-30 12:18:52 +0000

    File Version Info

--- Core/Core/Logging/SpeckleLog.cs
+++ Core/Core/Logging/SpeckleLog.cs
@@ -177,8 +177,8 @@ public static class SpeckleLog
     var serilogLogConfiguration = new LoggerConfiguration().MinimumLevel
       .Is(logConfiguration.MinimumLevel)
       .Enrich.FromLogContext()
-      .Enrich.WithProperty("version", fileVersionInfo.FileVersion)
-      .Enrich.WithProperty("productVersion", fileVersionInfo.ProductVersion)
+      .Enrich.WithProperty("version", fileVersionInfo?.FileVersion ?? "unknown")
+      .Enrich.WithProperty("productVersion", fileVersionInfo?.ProductVersion ?? "unknown")
       .Enrich.WithProperty("hostOs", DetermineHostOsSlug())
       .Enrich.WithProperty("hostOsVersion", Environment.OSVersion)
       .Enrich.WithProperty("hostOsArchitecture", RuntimeInformation.ProcessArchitecture.ToString())
@@ -264,9 +264,18 @@ public static class SpeckleLog
     return id;
   }
 
-  private static FileVersionInfo GetFileVersionInfo()
+  private static FileVersionInfo? GetFileVersionInfo()
   {
-    var assembly = Assembly.GetExecutingAssembly().Location;
+    // For some reason, Location will return null inside GoDot runtimes,
+    // Despite `Location` being decorated as non-nullable
+    string? assembly = Assembly.GetExecutingAssembly().Location;
+
+    // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+    if (assembly == null)
+    {
+      return null;
+    }
+
     return FileVersionInfo.GetVersionInfo(assembly);
   }
 
