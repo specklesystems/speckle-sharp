b1601f71588fb32ac03de49f84ee7d45fe5426d3
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2024-11-05 15:48:00 +0000

    jrm/fe2 corrections (#3649)
    API corrections
    
    Co-authored-by: Alan Rynne <alan@speckle.systems>

--- Core/Core/Api/GraphQL/Models/ProjectCollaborator.cs
+++ Core/Core/Api/GraphQL/Models/ProjectCollaborator.cs
@@ -4,6 +4,7 @@ namespace Speckle.Core.Api.GraphQL.Models;
 
 public sealed class ProjectCollaborator
 {
+  public string id { get; init; }
   public string role { get; init; }
   public LimitedUser user { get; init; }
 }
