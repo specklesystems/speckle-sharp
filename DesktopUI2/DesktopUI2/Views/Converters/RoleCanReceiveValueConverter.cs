cb28b909388e932a9b475f776346254b625b7eca
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-04-15 16:08:00 +0100

    Better set of greyed out parts

---
+++ DesktopUI2/DesktopUI2/Views/Converters/RoleCanReceiveValueConverter.cs
@@ -0,0 +1,64 @@
+﻿#nullable enable
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Speckle.Core.Api.GraphQL;
+
+namespace DesktopUI2.Views.Converters;
+
+public class RoleCanReceiveValueConverter : IValueConverter
+{
+  public bool Convert(object? value)
+  {
+    return value switch
+    {
+      StreamRoles.STREAM_OWNER => true,
+      StreamRoles.STREAM_CONTRIBUTOR => true,
+      _ => false
+    };
+  }
+
+  public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) => Convert(value);
+
+  public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    throw new NotSupportedException();
+  }
+}
+
+public sealed class RoleCanNotReceiveValueConverter : IValueConverter
+{
+  private readonly RoleCanReceiveValueConverter _inner = new();
+
+  public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    return !_inner.Convert(value);
+  }
+
+  public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    throw new NotSupportedException();
+  }
+}
+
+public sealed class RoleReceiveErrorMessageConverter : IValueConverter
+{
+  private readonly RoleCanReceiveValueConverter _inner = new();
+
+  public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    if (!_inner.Convert(value))
+    {
+      return "You do not have access to send or receive models on this project";
+    }
+    else
+    {
+      return null;
+    }
+  }
+
+  public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+  {
+    throw new NotSupportedException();
+  }
+}
